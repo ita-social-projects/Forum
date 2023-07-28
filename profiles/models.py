cf07a989529d0f54ebde334faf75715b6fa7fe02
Sofia Bilous <EgikSonya@gmail.com>
2023-07-28 17:06:53 +0300

    +models.py, serializers.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from .serializers import ProfileSerializer, ActivitySerializer, CategotySerializer
+from PIL import Image
 
-# Create your models here.
+REGIONS = ["east", "west", "north", "south"]
+
+
+class Profile(models.Model):
+
+    profile_id = models.IntegerField(primary_key=True)
+    comp_category = models.ManyToManyField("Category")
+    comp_activity = models.ManyToManyField("Activity")
+    comp_name = models.CharField(max_length=50)
+    comp_official_name = models.CharField(max_length=255, unique=True)
+    comp_region = models.CharField(choices=REGIONS)
+    comp_common_info = models.CharField(max_length=255)
+    comp_phone_number = models.CharField(
+        max_length=12,
+        validators=[
+            ProfileSerializer.validate_phone_number,
+            #RegexValidator(r"^\d+$", message="Phone number must contain only numbers."),
+        ]
+    )
+    comp_EDRPOU = models.IntegerField(
+        unique=True,
+        validators=[
+            ProfileSerializer.validate_edrpou,
+            #RegexValidator(r"^\d{8}$", message="EDRPOU must contain exactly 8 digits."),
+        ]
+
+    )
+    comp_year_of_foundation = models.SmallIntegerField(max_length=4, validators=[ProfileSerializer.validate_foundation_year])
+    comp_service_info = models.TextField()
+    comp_product_info = models.TextField()
+    comp_address = models.TextField()
+    comp_registered = models.BinaryField(max_length=1)
+    comp_is_startup = models.BinaryField(max_length=1)
+    comp_banner_image = models.ImageField(validators=[ProfileSerializer.validate_image])
+
+    person_name = models.CharField(max_length=50)
+    person_surname = models.CharField(max_length=50)
+    #person_company = models.CharField(max_length=50)
+    person_position = models.CharField(max_length=50)
+    person_email = models.EmailField(
+        max_length=50,
+        unique=True,
+        validators=[ProfileSerializer.validate_email]
+    )
+    person_password = models.CharField(max_length=128, validators=[ProfileSerializer.validate_password])
+
+    startup_idea = models.TextField()
+
+
+class Activity(models.Model):
+    activity_id = models.IntegerField(primary_key=True)
+    name = models.CharField()
+
+
+class Category(models.Model):
+    category_id = models.IntegerField(primary_key=True)
+    name = models.CharField()
+
---
+++ profiles/serializers.py
@@ -0,0 +1,80 @@
+from django.db import models
+from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from PIL import Image
+from .models import Profile, Activity, Category
+
+REGIONS = ["east", "west", "north", "south"]
+
+class ActivitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Activity
+        fields = "__all__"
+
+class CategotySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class ProfileSerializer(serializers.ModelSerializer):
+    activity = ActivitySerializer(many=True, read_only=True)
+    category = ActivitySerializer(many=True, read_only=True)
+
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+    def validate_email(email_value: str):
+        if "@" not in email_value:
+            raise ValidationError("Email address must include the '@' symbol.")
+
+
+    def validate_password(password_value: str):
+        if len(password_value) < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
+        if not any(char.isupper() for char in password_value):
+            raise ValidationError("Password must include at least one uppercase letter (A-Z).")
+        if not any(char.islower() for char in password_value):
+            raise ValidationError("Password must include at least one lowercase letter (a-z).")
+        if not any(char.isdigit() for char in password_value):
+            raise ValidationError("Password must include at least one digit (0-9).")
+
+
+    def validate_phone_number(phone_number_value: str):
+        # validate number length
+        if len(phone_number_value) != 12:
+            raise ValidationError("Phone number must be exactly 12 characters long.")
+        # validate number contain only digits
+        if not phone_number_value.isdigit():
+            raise ValidationError("Phone number must contain only numbers.")
+
+
+    def validate_edrpou(edrpou_value: int):
+        if len(str(edrpou_value)) != 8:
+            raise ValidationError("EDRPOU must be exactly 8 digits long.")
+
+
+    def validate_image(image: Image):
+        # validate image format
+        valid_formats = ["png", "jpeg", "jpg"]
+        img = Image.open(image)
+        format_ = img.format.lower()
+        if format_ not in valid_formats:
+            raise ValidationError("Unsupported image format. Only PNG and JPEG are allowed.")
+
+        # validate image size
+        max_size = 50 * 1024 * 1024  # 50MB in bytes
+        if image.size > max_size:
+            raise ValidationError("Image size exceeds the maximum allowed (50MB).")
+
+
+    def validate_foundation_year(foundation_year_value: int):
+        if len(str(foundation_year_value)) != 4:
+            raise ValidationError("Foundation year must be exactly 4 digits long.")
+
+
+
