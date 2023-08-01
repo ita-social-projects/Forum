56283f56932a50fbaa281beb0044fa670cc029ec
DianaUzun <dianauzun2006@gmail.com>
2023-08-01 15:58:01 +0300

    improve validate_foundation_year

--- validation/validate_foundation_year.py
+++ validation/validate_foundation_year.py
@@ -1,6 +1,8 @@
+import datetime
 from django.core.exceptions import ValidationError
 
 
 def validate_foundation_year(foundation_year_value: int):
-    if len(str(foundation_year_value)) != 4:
+    current_year = datetime.datetime.now().year
+    if len(str(foundation_year_value)) != 4 or foundation_year_value > current_year:
         raise ValidationError("Foundation year must be exactly 4 digits long.")
