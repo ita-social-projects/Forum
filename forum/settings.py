58d4b7538a54f8302d23b42decddb1a7bbd8735b
Sofia Bilous <EgikSonya@gmail.com>
2023-08-01 14:05:38 +0300

    add pillow

--- forum/settings.py
+++ forum/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = os.environ.get('DEBUG')
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
 
 
 # Application definition
