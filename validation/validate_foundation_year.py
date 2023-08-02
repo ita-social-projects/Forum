7166eba23ead1637880e2f59debd4053f789b242
Sofia Bilous <EgikSonya@gmail.com>
2023-08-02 10:35:39 +0300

    change validation of foundation year long->range

--- validation/validate_foundation_year.py
+++ validation/validate_foundation_year.py
@@ -2,11 +2,12 @@ import datetime
 from django.core.exceptions import ValidationError
 
 
-def validate_foundation_year_long(foundation_year_value: int) -> None:
-    if len(str(foundation_year_value)) != 4:
-        raise ValidationError("Foundation year must be exactly 4 digits long.")
+def validate_foundation_year_range(foundation_year_value: int) -> None:
+    if not (foundation_year_value in range(1800, (datetime.datetime.now().year+1))):
+        raise ValidationError("Foundation year must be exactly in range 1800-current year.")
 
 
 def validate_foundation_year_is_valid(foundation_year_value: int) -> None:
     if foundation_year_value > datetime.datetime.now().year:
         raise ValidationError("Foundation year cannot be more than current year.")
+
