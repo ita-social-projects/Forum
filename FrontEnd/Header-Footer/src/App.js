60761b845f975a85d0bcad475cbe7657414da6ce
RedSSD <salatcaesarrr1@gmail.com>
2023-08-03 15:33:35 +0200

    Moved header and footer to UI folder

--- FrontEnd/Header-Footer/src/App.js
+++ FrontEnd/Header-Footer/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
+import Footer from "./components/UI/footer/Footer";
+import Header from "./components/UI/header/Header";
 
 
 function App() {
