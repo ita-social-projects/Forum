bf9d55b7a16aeba46c17388ed73b1bec6e0e8c31
whoaam1 <yaroslav.shchudlyk.knm.2020@lpnu.ua>
2023-08-07 16:32:06 +0300

    remove module os

--- forum/settings.py
+++ forum/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
