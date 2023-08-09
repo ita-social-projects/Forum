b8ac008de905d615d5606445ea1407663cccbbf0
Sofia Bilous <EgikSonya@gmail.com>
2023-08-09 18:31:56 +0300

    connect pages

---
+++ FrontEnd/ConnectedPages/src/App.js
@@ -0,0 +1,71 @@
+//For authorization Page
+//import React from "react";
+//import MainPageRectangle from "./components/main-page-rectangle/MainPageRectangle";
+//
+//function App() {
+//  return (
+//      <MainPageRectangle />
+//  );
+//}
+//
+//export default App;
+
+//For main page
+//import "./AppMain.css"
+//import MainMenu from "./components/landing-page/menu/Menu";
+//import MainBanner from "./components/landing-page/banner/Banner";
+//import MainPartners from "./components/landing-page/partners/Partners";
+//import MainCompanies from "./components/landing-page/companies/Companies";
+//import MainLoginBanner from "./components/landing-page/login-banner/LoginBanner";
+//import MainAboutSection from "./components/landing-page/about-section/About";
+//
+//function App() {
+//  return (
+//    <div className="main-app">
+//      <div className="main-app-header">
+//        <MainMenu/>
+//        <MainBanner/>
+//        <div className="main-app-body">
+//            <MainPartners/>
+//            <MainCompanies/>
+//            <MainLoginBanner/>
+//            <MainAboutSection/>
+//        </div>
+//      </div>
+//    </div>
+//  );
+//}
+//
+//export default App;
+
+//For Profile SignUp page
+//import logo from './logo.svg'; -Do we really need this string of code?
+//import './AppSignUpPage.css';
+//import SignUpPageComponent from '.components/SignUp/SignUpPage';
+//
+//function App() {
+//  return (
+//    <SignUpPageComponent />
+//  );
+//}
+//
+//export default App;
+
+//For Header-Footer
+//import './AppHeaderFooter.css';
+//import Footer from "./components/UI/footer/Footer";
+//import Header from "./components/UI/header/Header";
+//
+//
+//function App() {
+//  return (
+//    <div className="App">
+//
+//        <Header isAuthorized={true}></Header>
+//        <Footer></Footer>
+//    </div>
+//  );
+//}
+//
+//export default App;
+
