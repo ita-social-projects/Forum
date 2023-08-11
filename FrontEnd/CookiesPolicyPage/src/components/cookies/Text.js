5a4586d19c523077fa9dcb31b3b9be51c2b51344
RedSSD <salatcaesarrr1@gmail.com>
2023-08-11 14:45:00 +0200

    Changed policy text and text output

--- FrontEnd/CookiesPolicyPage/src/components/cookies/Text.js
+++ FrontEnd/CookiesPolicyPage/src/components/cookies/Text.js
@@ -1,44 +1,69 @@
 export const cookiesPolicy = {
     updated: "Aug 9, 2023",
-    intro: "Welcome to Forum (\"we,\" \"us,\" or \"our\"). This Cookies Policy is designed to help you understand how we use cookies and similar technologies on our website www.website.com (\"Website\"). By using our Website, you consent to the use of cookies in accordance with this policy.",
+    intro: "<p>This Cookies Policy explains what Cookies are and how We use them. You should read this policy so You can understand what type of cookies We use, or the information We collect using Cookies and how that information is used. This Cookies Policy has been created with the help of the <a href=\"https://www.termsfeed.com/cookies-policy-generator/\" target=\"_blank\">Cookies Policy Generator</a>.</p>\n" +
+        "<p>Cookies do not typically contain any information that personally identifies a user, but personal information that we store about You may be linked to the information stored in and obtained from Cookies. For further information on how We use, store and keep your personal data secure, see our Privacy Policy.</p>\n" +
+        "<p>We do not store sensitive personal information, such as mailing addresses, account passwords, etc. in the Cookies We use.</p>",
     sections: [
         {
             id: 1,
-            title: "What are Cookies?",
-            content: "Cookies are small text files that are placed on your device (computer, tablet, smartphone, etc.) when you visit a website. They are widely used to make websites work efficiently and provide valuable information to website owners."
+            title: "Interpretation and Definitions",
+            content: "<h2>Interpretation</h2>\n" +
+                "<p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>\n" +
+                "<h2>Definitions</h2>\n" +
+                "<p>For the purposes of this Cookies Policy:</p>\n" +
+                "<ul>\n" +
+                "<li><strong>Company</strong> (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Cookies Policy) refers to SoftServe, 2D Sadova Street, Lviv 79021.</li>\n" +
+                "<li><strong>Cookies</strong> means small files that are placed on Your computer, mobile device or any other device by a website, containing details of your browsing history on that website among its many uses.</li>\n" +
+                "<li><strong>Website</strong> refers to Forum, accessible from <a href=\"https://forumproject.ua\" rel=\"external nofollow noopener\" target=\"_blank\">https://forumproject.ua</a></li>\n" +
+                "<li><strong>You</strong> means the individual accessing or using the Website, or a company, or any legal entity on behalf of which such individual is accessing or using the Website, as applicable.</li>\n" +
+                "</ul>"
         },
         {
             id: 2,
-            title: "How We Use Cookies",
-            content: "Essential Cookies: These cookies are necessary for the functioning of our Website. They enable you to navigate the site and use its features, such as accessing secure areas or making use of the shopping cart." +
-
-                "Analytics and Performance Cookies: These cookies help us gather information about how visitors use our Website, which pages they visit most often, and if they encounter any errors. This information helps us improve the performance and design of the Website.\n" +
-
-                "Functionality Cookies: These cookies allow our Website to remember choices you make (such as your username, language, or region) and provide enhanced, personalized features.\n" +
-
-                "Advertising and Targeting Cookies: We may work with third-party advertising networks to display targeted ads on our Website or other websites you visit. These cookies track your browsing habits and enable us to show you ads that are more relevant to your interests."
+            title: "The use of the Cookies",
+            content: "<h2>Type of Cookies We Use</h2>\n" +
+                "<p>Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies. Persistent Cookies remain on your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close your web browser.</p>\n" +
+                "<p>We use both session and persistent Cookies for the purposes set out below:</p>\n" +
+                "<ul>\n" +
+                "<li>\n" +
+                "<p><strong>Necessary / Essential Cookies</strong></p>\n" +
+                "<p>Type: Session Cookies</p>\n" +
+                "<p>Administered by: Us</p>\n" +
+                "<p>Purpose: These Cookies are essential to provide You with services available through the Website and to enable You to use some of its features. They help to authenticate users and prevent fraudulent use of user accounts. Without these Cookies, the services that You have asked for cannot be provided, and We only use these Cookies to provide You with those services.</p>\n" +
+                "</li>\n" +
+                "<li>\n" +
+                "<p><strong>Functionality Cookies</strong></p>\n" +
+                "<p>Type: Persistent Cookies</p>\n" +
+                "<p>Administered by: Us</p>\n" +
+                "<p>Purpose: These Cookies allow us to remember choices You make when You use the Website, such as remembering your login details or language preference. The purpose of these Cookies is to provide You with a more personal experience and to avoid You having to re-enter your preferences every time You use the Website.</p>\n" +
+                "</li>\n" +
+                "</ul>\n" +
+                "<h2>Your Choices Regarding Cookies</h2>\n" +
+                "<p>If You prefer to avoid the use of Cookies on the Website, first You must disable the use of Cookies in your browser and then delete the Cookies saved in your browser associated with this website. You may use this option for preventing the use of Cookies at any time.</p>\n" +
+                "<p>If You do not accept Our Cookies, You may experience some inconvenience in your use of the Website and some features may not function properly.</p>\n" +
+                "<p>If You'd like to delete Cookies or instruct your web browser to delete or refuse Cookies, please visit the help pages of your web browser.</p>\n" +
+                "<ul>\n" +
+                "<li>\n" +
+                "<p>For the Chrome web browser, please visit this page from Google: <a href=\"https://support.google.com/accounts/answer/32050\" rel=\"external nofollow noopener\" target=\"_blank\">https://support.google.com/accounts/answer/32050</a></p>\n" +
+                "</li>\n" +
+                "<li>\n" +
+                "<p>For the Internet Explorer web browser, please visit this page from Microsoft: <a href=\"http://support.microsoft.com/kb/278835\" rel=\"external nofollow noopener\" target=\"_blank\">http://support.microsoft.com/kb/278835</a></p>\n" +
+                "</li>\n" +
+                "<li>\n" +
+                "<p>For the Firefox web browser, please visit this page from Mozilla: <a href=\"https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored\" rel=\"external nofollow noopener\" target=\"_blank\">https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored</a></p>\n" +
+                "</li>\n" +
+                "<li>\n" +
+                "<p>For the Safari web browser, please visit this page from Apple: <a href=\"https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac\" rel=\"external nofollow noopener\" target=\"_blank\">https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac</a></p>\n" +
+                "</li>\n" +
+                "</ul>\n" +
+                "<p>For any other web browser, please visit your web browser's official web pages.</p>\n" +
+                "<h2>More Information about Cookies</h2>\n" +
+                "<p>You can learn more about cookies here: <a href=\"https://www.termsfeed.com/blog/cookies/\" target=\"_blank\">All About Cookies by TermsFeed</a>.</p>\n" +
+                "<h2>Contact Us</h2>\n" +
+                "<p>If you have any questions about this Cookies Policy, You can contact us:</p>\n" +
+                "<ul>\n" +
+                "<li>By visiting this page on our website: <a href=\"https://forumproject.ua\" rel=\"external nofollow noopener\" target=\"_blank\">https://forumproject.ua</a></li>\n" +
+                "</ul>"
         },
-        {
-            id: 3,
-            title: "Third-Party Cookies",
-            content: "We may also allow third-party cookies on our Website. These cookies are placed by third-party services and may serve different purposes, such as analytics, advertising, or social media integration. We do not have direct control over the information collected by these cookies."
-        },
-        {
-            id: 4,
-            title: "Managing Cookies",
-            content: "Most web browsers allow you to manage your cookie preferences. You can usually set your browser to refuse cookies, delete cookies, or alert you when cookies are being sent. Please note that if you choose to disable cookies, some parts of our Website may not function properly."
-        },
-        {
-            id: 5,
-            title: "Changes to this Policy",
-            content: "We may update this Cookies Policy from time to time to reflect changes in technology, data protection laws, or our practices. We encourage you to review this policy periodically to stay informed about how we use cookies."
-        },
-        {
-            id: 6,
-            title: "Contact Us",
-            content: "If you have any questions or concerns about our Cookies Policy, please contact us at contact@forumwebsite.com.\n" +
-                "\n" +
-                "By continuing to use our Website, you agree to the use of cookies as described in this policy."
-        }
     ]
 };
