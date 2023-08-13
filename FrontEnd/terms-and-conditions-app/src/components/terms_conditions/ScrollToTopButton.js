4f0abbd4a9da72247dae5d927401632b71fc1b1c
whoaam1 <yaroslav.shchudlyk.knm.2020@lpnu.ua>
2023-08-14 01:05:07 +0300

    implement terms and conditions page

---
+++ FrontEnd/terms-and-conditions-app/src/components/terms_conditions/ScrollToTopButton.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import './ScrollToTopButton.css';
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className={`scroll-to-top-button ${isVisible ? 'visible' : ''}`} onClick={scrollToTop}>
+      &#8679;
+    </div>
+  );
+};
+
+export default ScrollToTopButton;
