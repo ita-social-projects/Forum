9418d93264581ef28438df9d4361f60353b3ae81
Sofia Bilous <EgikSonya@gmail.com>
2023-08-17 14:05:13 +0300

    fix imports

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,11 +1,16 @@
 //For authorization Page
-//Don't forget about npm install validator, npm i react-router-dom@6.4.1, npm install react-cookie, npm install react-hook-form
+//Don't forget about
+//npm install validator
+//npm i react-router-dom@6.4.1
+//npm install react-cookie
+//npm install react-hook-form
 import React from 'react';
-import MainPageRectangle from './components/main-page-rectangle/MainPageRectangle';
 import { useState } from 'react';
 import { Cookies } from 'react-cookie';
 import { useCookies } from 'react-cookie';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './AppMain.css';
+import MainPageRectangle from './components/main-page-rectangle/MainPageRectangle';
 import MainMenu from './components/landing-page/menu/Menu';
 import MainBanner from './components/landing-page/banner/Banner';
 import MainPartners from './components/landing-page/partners/Partners';
@@ -17,12 +22,8 @@ import { SignUpPage } from './components/SignUp/pages/SignUpPage';
 import './AppHeaderFooter.css';
 import Footer from './components/HeaderFooter/footer/Footer';
 import Header from './components/HeaderFooter/header/Header';
-// import './AppPrivacyPolicy.css';
-// import Footer from './components/PrivacyPolicyPage/UI/footer/Footer';
-// import Header from './components/PrivacyPolicyPage/UI/header/Header';
 import { PrivacyPolicy } from './components/PrivacyPolicyPage/privacy/PrivacyPolicyComponent';
 import ScrollToTopButton from './components/PrivacyPolicyPage/privacy/ScrollToTopButton';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TermsAndConditions from './components/terms-and-conditions-app/terms_conditions/TermsAndConditionsComponent';
 
 function App() {
