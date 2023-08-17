06e20db99c1fada1a673eb48d975ea42042e0585
Oleksandra <mremarka314@gmail.com>
2023-08-17 16:16:42 +0300

    Add retrieve profile detail

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,7 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -7,35 +11,17 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 
-class ProfileList(ListAPIView):
-    """
-    List all profiles depending on query parameter include_deleted.
-    """
-    serializer_class = ProfileSerializer
-
-    def get_queryset(self):
-        include_deleted = self.request.query_params.get("include_deleted", False)
-        if include_deleted:
-            return Profile.objects.all()
-        return Profile.objects.filter(is_deleted=False)
-
-
 class ProfileDetail(RetrieveUpdateDestroyAPIView):
     """
     Retrieve or delete a profile instance.
     """
     serializer_class = ProfileSerializer
     queryset = Profile.objects.filter(is_deleted=False)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def retrieve(self, request, pk=None):
-        # queryset = Profile.objects.all()
-        profile = get_object_or_404(self.queryset, pk=pk)
+        user_id = Token.objects.get(key=request.auth.key).user_id
+        profile = get_object_or_404(self.queryset, pk=pk, person_id=user_id)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
-
-    def destroy(self, request, pk=None):
-        profile = get_object_or_404(self.queryset, pk=pk)
-        profile.is_deleted = True
-        profile.save()
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
