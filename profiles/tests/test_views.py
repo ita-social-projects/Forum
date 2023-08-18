d10b3d6d81ab18919e3646c5415008230a9df2fd
Oleksandra <mremarka314@gmail.com>
2023-08-18 14:52:26 +0300

    tests adjusted

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase, APIClient
 from authentication.models import CustomUser
-from profiles.models import Profile
+from profiles.models import Profile, Category, Activity
 
 
 class TestProfileListDetailAPIView(APITestCase):
@@ -26,6 +26,16 @@ class TestProfileListDetailAPIView(APITestCase):
             comp_is_startup=False
         )
 
+        test_person3 = CustomUser.objects.create_user(
+            person_email="test3@test.com",
+            password="Testing01",
+            person_name="test",
+            person_surname="test",
+            comp_name="test",
+            comp_registered=True,
+            comp_is_startup=False
+        )
+
         test_profile = Profile.objects.create(
             person=test_person,
             is_deleted=False
@@ -34,6 +44,8 @@ class TestProfileListDetailAPIView(APITestCase):
             person=test_person2,
             is_deleted=False
         )
+
+
 
     def setUp(self) -> None:
         self.client = APIClient()
@@ -48,7 +60,7 @@ class TestProfileListDetailAPIView(APITestCase):
             }).data["auth_token"]
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token1}")
         self.user1_id = self.client.get(
-            path="http://127.0.0.1:8000/api/auth/users/me/").data["id"]
+            path="/api/auth/users/me/").data["id"]
         self.token2 = self.client.post(
             path="/api/auth/token/login/",
             data={
@@ -57,9 +69,14 @@ class TestProfileListDetailAPIView(APITestCase):
             }).data["auth_token"]
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token2}")
         self.user2_id = self.client.get(
-            path="http://127.0.0.1:8000/api/auth/users/me/").data["id"]
+            path="/api/auth/users/me/").data["id"]
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token1}")
-
+        self.token3 = self.client.post(
+            path="/api/auth/token/login/",
+            data={
+                "person_email": "test3@test.com",
+                "password": "Testing01"
+            }).data["auth_token"]
     def tearDown(self) -> None:
         self.client.logout()
 
@@ -79,13 +96,11 @@ class TestProfileListDetailAPIView(APITestCase):
 
     def test_get_profile_of_other_user(self):
         response = self.client.get("".join([self.base_url, str(self.user2_id)]))
-        self.assertEqual(404, response.status_code)
+        self.assertEqual(200, response.status_code)
 
     def test_delete_profile_authorized(self):
         response = self.client.delete("".join([self.base_url, str(self.user1_id)]))
         self.assertEqual(204, response.status_code)
-        self.assertEqual(True, response.data["is_deleted"])
-
         response = self.client.get(self.base_url)
         self.assertEqual(0, len(response.data))
 
