b8cd252d286aa25682277dafba6dd65e500ff554
RedSSD <salatcaesarrr1@gmail.com>
2023-08-18 14:56:03 +0200

    removed profile from usuitable_pages, added Footer.module.css

--- FrontEnd/src/components/HeaderFooter/footer/Footer.jsx
+++ FrontEnd/src/components/HeaderFooter/footer/Footer.jsx
@@ -1,12 +1,13 @@
 import FooterTop from "./top/FooterTop";
 import FooterBottom from "./bottom/FooterBottom";
 import ShortFooter from "./short/ShortFooter";
+import css from "./Footer.module.css"
 
-const UNSUITABLE_PAGES = ["login", "register", "profile"]
+const UNSUITABLE_PAGES = ["login", "register"]
 
 function Footer(props) {
     return (
-        <footer>
+        <footer className={css["footer-content"]}>
             {UNSUITABLE_PAGES.includes(props.page) ? (<ShortFooter/>) :
                 (<>
                     <FooterTop></FooterTop>
