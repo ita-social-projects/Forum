87b253a6e636b84c51816511a14a9e80efeeb25b
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-08-20 16:56:40 +0200

    authentication tests added

---
+++ authentication/tests/test_user_logout.py
@@ -0,0 +1,38 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from authentication.models import CustomUser
+
+
+class UserLoginAPITests(APITestCase):
+
+    def setUp(self):
+        self.test_user = CustomUser.objects.create_user(
+            person_email="test@test.com",
+            password="Test1234",
+            person_name="Test",
+            person_surname="Test",
+        )
+        self.test_user.is_active = True
+        self.test_user.save()
+
+    def test_logout_successful(self):
+        self.test_user_token = self.client.post(
+            "/api/auth/token/login/", data={
+                "person_email": "test@test.com",
+                "password": "Test1234",
+            }).data["auth_token"]
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Token {self.test_user_token}")
+        url = "/api/auth/token/logout/"
+        response = self.client.post(url)
+        self.assertEqual(response.status_code,
+                         status.HTTP_204_NO_CONTENT)
+
+    def test_logout_not_loged_in(self):
+        url = "/api/auth/token/logout/"
+        response = self.client.post(url)
+        self.assertEqual(response.status_code,
+                         status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(
+            {"detail": "Authentication credentials were not provided."}, response.json())
