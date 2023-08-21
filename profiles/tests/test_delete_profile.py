d48cb46a324f307b2d6cdfb4415db180f55dbcd9
Oleksandra <mremarka314@gmail.com>
2023-08-21 16:05:37 +0300

    tests adjusted for delete

--- profiles/tests/test_delete_profile.py
+++ profiles/tests/test_delete_profile.py
@@ -1,9 +1,9 @@
 from rest_framework.test import APITestCase, APIClient
 from authentication.models import CustomUser
-from profiles.models import Profile, Category, Activity
+from profiles.models import Profile
 
 
-class TestProfileListDetailAPIView(APITestCase):
+class TestProfileDetailAPIView(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
@@ -45,8 +45,6 @@ class TestProfileListDetailAPIView(APITestCase):
             is_deleted=False
         )
 
-
-
     def setUp(self) -> None:
         self.client = APIClient()
         self.base_url = "/api/profiles/"
@@ -77,17 +75,19 @@ class TestProfileListDetailAPIView(APITestCase):
                 "person_email": "test3@test.com",
                 "password": "Testing01"
             }).data["auth_token"]
+
     def tearDown(self) -> None:
         self.client.logout()
 
-    def test_get_all_profiles_authorized_no_queryparam(self):
-        response = self.client.get(self.base_url)
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(1, len(response.data))
+    def test_get_profile_unauthorized(self):
+        self.client.logout()
+        response = self.client.get("".join([self.base_url, str(self.user1_id)]))
+        self.assertEqual(401, response.status_code)
 
-    def test_get_all_profiles_authorized_queryparam_include_all(self):
-        response = self.client.get(f"{self.base_url}?include_all=True")
-        self.assertEqual(2, len(response.data))
+    def test_delete_profile_unauthorized(self):
+        self.client.logout()
+        response = self.client.delete("".join([self.base_url, str(self.user1_id)]))
+        self.assertEqual(401, response.status_code)
 
     def test_get_profile_authorized(self):
         response = self.client.get("".join([self.base_url, str(self.user1_id)]))
@@ -99,34 +99,21 @@ class TestProfileListDetailAPIView(APITestCase):
         self.assertEqual(200, response.status_code)
 
     def test_delete_profile_authorized(self):
+        # get all before delete
+        response = self.client.get(self.base_url)
+        profiles_len_before_del = len(response.data)
+        # del profile
         response = self.client.delete("".join([self.base_url, str(self.user1_id)]))
         self.assertEqual(204, response.status_code)
+        # check the profile is deleted
         response = self.client.get(self.base_url)
-        self.assertEqual(0, len(response.data))
+        persons = [response.data[i]["person"] for i in range(len(response.data))]
+        self.assertEqual(profiles_len_before_del-1, len(response.data))
+        self.assertTrue(self.user1_id not in persons)
+        # try access deleted profile
+        response = self.client.get("".join([self.base_url, str(self.user1_id)]))
+        self.assertEqual(404, response.status_code)
 
     def test_delete_profile_of_other_user_authorized(self):
         response = self.client.delete("".join([self.base_url, str(self.user2_id)]))
         self.assertEqual(404, response.status_code)
-
-    def test_get_user_profiles_authorized_after_delete_no_queryparam(self):
-        response = self.client.get({self.base_url})
-        self.assertEqual(404, response.status_code)
-
-    def test_get_user_profiles_authorized_after_delete_queryparam_include_deleted(self):
-        response = self.client.get(f"{self.base_url}?include_deleted=True")
-        self.assertEqual(1, len(response.data))
-
-    def test_get_all_profiles_unauthorized(self):
-        self.client.logout()
-        response = self.client.get(self.base_url)
-        self.assertEqual(401, response.status_code)
-
-    def test_get_profile_unauthorized(self):
-        self.client.logout()
-        response = self.client.get("".join([self.base_url, str(self.user1_id)]))
-        self.assertEqual(401, response.status_code)
-
-    def test_delete_profile_unauthorized(self):
-        self.client.logout()
-        response = self.client.delete("".join([self.base_url, str(self.user1_id)]))
-        self.assertEqual(401, response.status_code)
