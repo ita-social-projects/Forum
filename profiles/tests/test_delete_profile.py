01d4f6d1a55bab7fd176c17338491c08d220d876
Oleksandra <mremarka314@gmail.com>
2023-08-22 15:01:21 +0300

    adjust tests to model changes

--- profiles/tests/test_delete_profile.py
+++ profiles/tests/test_delete_profile.py
@@ -13,25 +13,25 @@ class TestProfileDetailAPIView(APITestCase):
             password="Testing01",
             person_name="test",
             person_surname="test",
-            comp_name="test",
-            comp_registered=True,
-            comp_is_startup=False
+            is_active=True
         )
         test_person2 = CustomUser.objects.create_user(
             person_email="test2@test.com",
             password="Testing01",
             person_name="test",
             person_surname="test",
-            comp_name="test",
-            comp_registered=True,
-            comp_is_startup=False
+            is_active=True
         )
 
         test_profile = Profile.objects.create(
-            person=test_person
+            person=test_person,
+            comp_is_startup=True,
+            comp_registered=False
         )
         test_profile = Profile.objects.create(
-            person=test_person2
+            person=test_person2,
+            comp_registered=True,
+            comp_is_startup=False
         )
 
     def setUp(self) -> None:
@@ -45,39 +45,33 @@ class TestProfileDetailAPIView(APITestCase):
                 "password": "Testing01"
             }).data["auth_token"]
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token1}")
-        self.user1_id = self.client.get(
-            path="/api/auth/users/me/").data["id"]
         self.token2 = self.client.post(
             path="/api/auth/token/login/",
             data={
                 "person_email": "test2@test.com",
                 "password": "Testing01"
             }).data["auth_token"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token2}")
-        self.user2_id = self.client.get(
-            path="/api/auth/users/me/").data["id"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token1}")
 
     def tearDown(self) -> None:
         self.client.logout()
 
     def test_get_profile_unauthorized(self):
         self.client.logout()
-        response = self.client.get(f"/api/profiles/{self.user1_id}")
+        response = self.client.get("/api/profiles/{user1_id}".format(user1_id=1))
         self.assertEqual(401, response.status_code)
 
     def test_delete_profile_unauthorized(self):
         self.client.logout()
-        response = self.client.delete(f"/api/profiles/{self.user1_id}")
+        response = self.client.delete("/api/profiles/{user1_id}".format(user1_id=1))
         self.assertEqual(401, response.status_code)
 
     def test_get_profile_authorized(self):
-        response = self.client.get(f"/api/profiles/{self.user1_id}")
+        response = self.client.get("/api/profiles/{user1_id}".format(user1_id=1))
         self.assertEqual(200, response.status_code)
         self.assertEqual(False, response.data["is_deleted"])
 
     def test_get_profile_of_other_user(self):
-        response = self.client.get(f"/api/profiles/{self.user2_id}")
+        response = self.client.get("/api/profiles/{user2_id}".format(user2_id=1))
         self.assertEqual(200, response.status_code)
 
     def test_delete_profile_authorized(self):
@@ -85,17 +79,17 @@ class TestProfileDetailAPIView(APITestCase):
         response = self.client.get("/api/profiles/")
         profiles_len_before_del = len(response.data)
         # del profile
-        response = self.client.delete(f"/api/profiles/{self.user1_id}")
+        response = self.client.delete("/api/profiles/{user1_id}".format(user1_id=1))
         self.assertEqual(204, response.status_code)
         # check the profile is deleted
         response = self.client.get("/api/profiles/")
         persons = [response.data[i]["person"] for i in range(len(response.data))]
         self.assertEqual(profiles_len_before_del-1, len(response.data))
-        self.assertTrue(self.user1_id not in persons)
+        self.assertTrue(1 not in persons)
         # try access deleted profile
-        response = self.client.get(f"/api/profiles/{self.user1_id}")
+        response = self.client.get("/api/profiles/{user1_id}".format(user1_id=1))
         self.assertEqual(404, response.status_code)
 
     def test_delete_profile_of_other_user_authorized(self):
-        response = self.client.delete(f"/api/profiles/{self.user2_id}")
+        response = self.client.delete("/api/profiles/{user2_id}".format(user2_id=2))
         self.assertEqual(404, response.status_code)
