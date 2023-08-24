bbfaadfd6f1bda23c234e78efd5b94d7d71bbc42
Oleksandra <mremarka314@gmail.com>
2023-08-24 17:47:40 +0300

    APIView name adjusted

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import SavedCompaniesListCreate, SavedCompaniesDestroy
-from .views import ProfileList, ProfileDetail, ViewCompanyList
+from .views import ProfileList, ProfileDetail, ViewedCompanyList
 
 app_name = "profiles"
 
@@ -9,5 +9,5 @@ urlpatterns = [
     path(r'profiles/<pk>', ProfileDetail.as_view(), name='profile-detail'),
     path('saved-list/', SavedCompaniesListCreate.as_view(), name='saved_companies_list_create'),
     path('saved-list/<pk>/', SavedCompaniesDestroy.as_view(), name='saved_companies_destroy'),
-    path('viewed-list/', ViewCompanyList.as_view(), name='viewed_company_list'),
+    path('viewed-list/', ViewedCompanyList.as_view(), name='viewed_company_list'),
 ]
