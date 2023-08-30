ebc7c8ee89f2816c35d7f36fac124e1b98c7ca33
whooaami <yarkos03@gmail.com>
2023-08-30 11:07:51 +0300

    renamed app

--- administration/apps.py
+++ administration/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdminConfig(AppConfig):
+class AdministrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin'
+    name = 'administration'
