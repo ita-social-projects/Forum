f319a8b4bbaab3ccc4d272a2183a53c054e25d53
whooaami <yarkos03@gmail.com>
2023-08-31 16:09:13 +0300

    Add login for admin panel

--- administration/urls.py
+++ administration/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from .views import ProfileList, ProfileDetail
 
 app_name = "administration"
 
 urlpatterns = [
+    path("auth/", include('djoser.urls')),
+    re_path(r"^auth/", include('djoser.urls.authtoken')),
     path('profiles/', ProfileList.as_view(), name='profile-list'),
     path('profiles/<pk>', ProfileDetail.as_view(), name='profile-detail'),
 ]
