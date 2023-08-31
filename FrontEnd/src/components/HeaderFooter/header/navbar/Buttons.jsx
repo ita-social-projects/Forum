712079cbe491ac35ef91766ef8dcbf89a83accc1
Oleksandra <mremarka314@gmail.com>
2023-08-31 16:36:14 +0300

    fix navigation to signup

--- FrontEnd/src/components/HeaderFooter/header/navbar/Buttons.jsx
+++ FrontEnd/src/components/HeaderFooter/header/navbar/Buttons.jsx
@@ -5,7 +5,7 @@ function Buttons() {
     return (
         <div className={css["header-buttons-section"]}>
             <Link className={css["header-login__button"]} to="/authorization">Увійти</Link>
-            <Link className={css["header-register__button"]} to="/SignUpPage">Зареєструватися</Link>
+            <Link className={css["header-register__button"]} to="/sign-up">Зареєструватися</Link>
         </div>
     );
 };
