26e474f1742a30d589be3d4534a2f9b2cfb5f744
whooaami <yarkos03@gmail.com>
2023-09-07 13:03:58 +0300

    Add routes for pages

--- FrontEnd/src/components/adminPage/AdminPage.jsx
+++ FrontEnd/src/components/adminPage/AdminPage.jsx
@@ -3,21 +3,24 @@ import Header from "./header/Header";
 import Menu from "./menu/Menu";
 import UserDetail from "./detail-view/UserDetail";
 import UserTable from "./table/UserTable";
+import CompanyTable from "./table/CompanyTable";
+import CompanyDetail from "./detail-view/CompanyDetail";
 import css from "./AdminPage.module.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function AdminPage() {
     return (
         <div>
-            <Header></Header>
+            <Header />
                 <div className={css["content"]}>
-                        <Menu></Menu>
+                        <Menu />
                         <Routes>
                             <Route path="/users" element={<UserTable />} />
                             <Route path="/users/:id" element={<UserDetail />} />
+                            <Route path="/companies" element={<CompanyTable />} />
+                            <Route path="/company/:id" element={<CompanyDetail />} />
                         </Routes>
                 </div>
-            <Footer></Footer>
         </div>
 
     );
