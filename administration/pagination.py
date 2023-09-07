0603917941b68c8a2b9afc2f0b9bfe34dcb50915
RedSSD <salatcaesarrr1@gmail.com>
2023-09-07 13:40:34 +0200

    added current_page and total_pages to pagination class

--- administration/pagination.py
+++ administration/pagination.py
@@ -3,3 +3,9 @@ from rest_framework.pagination import PageNumberPagination
 
 class UserListPagination(PageNumberPagination):
     page_size = 3
+
+    def get_paginated_response(self, data):
+        response = super().get_paginated_response(data)
+        response.data['current_page'] = self.page.number
+        response.data['total_pages'] = self.page.paginator.num_pages
+        return response
