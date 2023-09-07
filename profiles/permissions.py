a3306be801ad3aaf5f0d52118c7fd4834ba45ef5
Oleksandra <mremarka314@gmail.com>
2023-09-07 12:17:19 +0300

    adjust permission

--- profiles/permissions.py
+++ profiles/permissions.py
@@ -2,15 +2,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class UserIsProfileOwnerOrReadOnly(BasePermission):
-
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
+            if request.query_params.get("with_contacts"):
+                return True if request.user.is_authenticated else False
             return True
 
         return obj.person == request.user
-
-
-class IsAllowedToViewContacts(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.method in SAFE_METHODS
