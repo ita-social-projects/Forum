277084c58b66ea7d653817a326cd64e3c335f225
Oleksandra <mremarka314@gmail.com>
2023-09-09 02:24:23 +0300

    remove setUpTestData & replace via factories

--- profiles/tests/test_filter_profiles.py
+++ profiles/tests/test_filter_profiles.py
@@ -1,79 +1,90 @@
 from rest_framework.test import APITestCase
-from authentication.models import CustomUser
-from profiles.models import Profile, Category, Activity
+
 from utils.dump_response import dump  # noqa
+from rest_framework import status
+from profiles.factories import ProfileStartupFactory, ProfileCompanyFactory, ActivityFactory
+from authentication.factories import UserFactory
 
 
 class TestProfileListAPIView(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        from random import choice, sample
-        test_persons = []
-        test_activities = []
-        test_categories = []
-
-        # setup activities
-        HEADER_ACTIVITIES = ["producer", "importer", "retail", "horeca"]
-        for i in range(len(HEADER_ACTIVITIES)):
-            test_activity = Activity.objects.create(
-                name=HEADER_ACTIVITIES[i]
-            )
-            test_activities.append(test_activity)
-        test_activity = Activity.objects.create(name="test")
-        test_activities.append(test_activity)
-
-        # setup categories
-        categories = ["cheese", "milk", "honey", "wood"]
-        for i in range(len(categories)):
-            test_category = Category.objects.create(
-                name=categories[i]
-            )
-            test_categories.append(test_category)
-
-        # setup persons
-        for i in range(12):
-            if i % 2 == 0:
-                test_person = CustomUser.objects.create_user(
-                    person_email=f"test{i + 1}@test.com",
-                    password="Testing01",
-                    person_name="test",
-                    person_surname="test",
-                    is_active=True
-                )
-                test_profile = Profile.objects.create(
-                    person=test_person,
-                    comp_official_name=f"Startup {i}",
-                    comp_is_startup=True,
-                    comp_registered=False
-                )
-
-            else:
-                test_person = CustomUser.objects.create_user(
-                    person_email=f"test{i + 1}@test.com",
-                    password="Testing01",
-                    person_name="test",
-                    person_surname="test",
-                    is_active=True
-                )
-                test_profile = Profile.objects.create(
-                    person=test_person,
-                    comp_official_name=f"Company {i}",
-                    comp_is_startup=False,
-                    comp_registered=True
-                )
-            test_profile.comp_activity.set(sample(test_activities, 2))
-            test_profile.comp_category.add(choice(test_categories))
-            test_persons.append(test_person)
+    # @classmethod
+    # def setUpTestData(cls):
+    #     from random import choice, sample
+    #     test_persons = []
+    #     test_activities = []
+    #     test_categories = []
+    #
+    #     # setup activities
+    #     HEADER_ACTIVITIES = ["producer", "importer", "retail", "horeca"]
+    #     for i in range(len(HEADER_ACTIVITIES)):
+    #         test_activity = Activity.objects.create(
+    #             name=HEADER_ACTIVITIES[i]
+    #         )
+    #         test_activities.append(test_activity)
+    #     test_activity = Activity.objects.create(name="test")
+    #     test_activities.append(test_activity)
+    #
+    #     # setup categories
+    #     categories = ["cheese", "milk", "honey", "wood"]
+    #     for i in range(len(categories)):
+    #         test_category = Category.objects.create(
+    #             name=categories[i]
+    #         )
+    #         test_categories.append(test_category)
+    #
+    #     # setup persons
+    #     for i in range(12):
+    #         if i % 2 == 0:
+    #             test_person = CustomUser.objects.create_user(
+    #                 person_email=f"test{i + 1}@test.com",
+    #                 password="Testing01",
+    #                 person_name="test",
+    #                 person_surname="test",
+    #                 is_active=True
+    #             )
+    #             test_profile = Profile.objects.create(
+    #                 person=test_person,
+    #                 comp_official_name=f"Startup {i}",
+    #                 comp_is_startup=True,
+    #                 comp_registered=False
+    #             )
+    #
+    #         else:
+    #             test_person = CustomUser.objects.create_user(
+    #                 person_email=f"test{i + 1}@test.com",
+    #                 password="Testing01",
+    #                 person_name="test",
+    #                 person_surname="test",
+    #                 is_active=True
+    #             )
+    #             test_profile = Profile.objects.create(
+    #                 person=test_person,
+    #                 comp_official_name=f"Company {i}",
+    #                 comp_is_startup=False,
+    #                 comp_registered=True
+    #             )
+    #         test_profile.comp_activity.set(sample(test_activities, 2))
+    #         test_profile.comp_category.add(choice(test_categories))
+    #         test_persons.append(test_person)
 
     def setUp(self) -> None:
-        # login user & get token
-        self.token1 = self.client.post(
-            path="/api/auth/token/login/",
-            data={
-                "person_email": "test1@test.com",
-                "password": "Testing01"
-            }).data["auth_token"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token1}")
+        self.user = UserFactory()
+
+        self.producer_activity = ActivityFactory(name="producer")
+        self.importer_activity = ActivityFactory(name="importer")
+        self.retail_activity = ActivityFactory(name="retail")
+        self.horeca_activity = ActivityFactory(name="horeca")
+
+        self.company = ProfileCompanyFactory(
+            person=self.user,
+            comp_activity=(self.importer_activity, self.retail_activity, self.horeca_activity))
+        self.startups = ProfileStartupFactory.create_batch(2,
+                                                           comp_activity=(self.producer_activity, self.horeca_activity))
+
+        self.importers = ProfileCompanyFactory.create_batch(3, comp_activity=(self.importer_activity,))
+        self.producers = ProfileStartupFactory.create_batch(2, comp_activity=(self.producer_activity,))
+        self.retailers = ProfileCompanyFactory.create_batch(2, comp_activity=(self.retail_activity,))
+        self.horecas = ProfileStartupFactory.create_batch(1, comp_activity=(self.horeca_activity,))
 
     def test_get_all_profiles_authorized_no_filters(self):
         response = self.client.get("/api/profiles/?page=1&page_size=12")
