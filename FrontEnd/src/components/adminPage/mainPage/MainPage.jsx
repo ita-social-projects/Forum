470fe711d4d9e6f18a668183cf168cfbe4a52bb4
RedSSD <salatcaesarrr1@gmail.com>
2023-09-10 00:58:58 +0200

    add Text.js to the main page

--- FrontEnd/src/components/adminPage/mainPage/MainPage.jsx
+++ FrontEnd/src/components/adminPage/mainPage/MainPage.jsx
@@ -1,18 +1,18 @@
 import css from "./mainPage.module.css"
+import {MAIN_PAGE_TEXT} from "./Text";
 
 function MainPage() {
     return (
         <div className={css["main-page-section"]}>
             <h1 align="center">Welcome to the admin panel</h1>
             <p>Here, you can:</p>
-            <br />
-            <p><b>Check Profiles:</b> Review user and company profiles.</p>
-            <br />
-            <p><b>Edit Profiles:</b> Make necessary updates to profiles for accuracy.</p>
-            <br />
-            <p><b>Approve Profiles:</b> Give the green light to new profiles.</p>
-            <br />
-            <p><b>Delete Profiles:</b> In case of policy violations or other issues.</p>
+            <br/>
+            {MAIN_PAGE_TEXT.map((element) =>(
+                <div>
+                    <p><b>{element.title}</b> {element.text}</p>
+                    <br />
+                </div>
+            ))}
         </div>
     );
 };
