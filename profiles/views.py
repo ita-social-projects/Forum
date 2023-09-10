ab01a4abe4d8bacf068472182abe27a1333c3995
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-09-10 17:06:46 +0200

    saved companies list and tests upd

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.generics import ListCreateAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -11,7 +11,7 @@ from .serializers import (SavedCompanySerializer, ProfileSerializer, ViewedCompa
 from .permissions import UserIsProfileOwnerOrReadOnly, SavedCompaniesListPermission
 
 
-class SavedCompaniesListCreate(ListCreateAPIView):
+class SavedCompaniesCreate(CreateAPIView):
     """
     List of saved companies.
     Add a company to the saved list.
@@ -20,11 +20,6 @@ class SavedCompaniesListCreate(ListCreateAPIView):
     serializer_class = SavedCompanySerializer
     pagination_class = ForumPagination
     
-    def get_queryset(self):
-        user = self.request.user
-        saved_companies = SavedCompany.objects.filter(user=user).order_by("company_id")
-        return saved_companies
-
     def post(self, request):
         user = request.user
         pk = request.data.get("company_pk")
