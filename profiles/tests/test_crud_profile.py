eab9f866ce91d4bb344e1258aa5c841ab330c3d5
Oleksandra <mremarka314@gmail.com>
2023-09-12 22:22:01 +0300

    adjust test after merge

--- profiles/tests/test_crud_profile.py
+++ profiles/tests/test_crud_profile.py
@@ -1,12 +1,10 @@
-import io
+from io import BytesIO
 import os
 from PIL import Image
 from rest_framework.test import APITestCase
 from rest_framework import status
 
-from profiles.models import Profile
-
-from profiles.factories import ProfileStartupFactory, ProfileCompanyFactory, CategoryFactory, ActivityFactory
+from profiles.factories import ProfileStartupFactory, CategoryFactory, ActivityFactory
 from authentication.factories import UserFactory
 
 from utils.dump_response import dump  # noqa
@@ -17,8 +15,8 @@ class TestProfileDetailAPIView(APITestCase):
 
     @staticmethod
     def _generate_image(ext, size=(100, 100)):
-        '''for mocking png and jpeg files'''
-        file = io.BytesIO()
+        """for mocking png and jpeg files"""
+        file = BytesIO()
         image = Image.new("RGB", size=size)
         formatext = ext.upper()
         image.save(file, formatext)
@@ -32,6 +30,7 @@ class TestProfileDetailAPIView(APITestCase):
         self.user = UserFactory(person_email="test1@test.com")
         self.profile = ProfileStartupFactory.create(
             person=self.user,
+            comp_official_name="Test Official Startup",
             comp_phone_number='380100102034',
             comp_EDRPOU=99999999
         )
@@ -66,43 +65,43 @@ class TestProfileDetailAPIView(APITestCase):
             "comp_service_info": "test service info",
             "comp_product_info": "test product info",
             "comp_banner_image": None
-        }
-            , response.data
+        }, response.data
         )
 
     def test_get_profile_authorized_not_owner(self):
-        profile2 = ProfileCompanyFactory()
-
+        profile2 = ProfileStartupFactory()
         self.client.force_authenticate(self.user)
+
         response = self.client.get(path="/api/profiles/{profile_id}".format(profile_id=profile2.profile_id))
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertIsNone(response.data.get("comp_phone_number"))
         self.assertIsNone(response.data.get("email"))
 
     def test_get_profile_authorized_owner(self):
-        self.client.force_authenticate(self.test_person_with_profile)
-        response = self.client.get("/api/profiles/{profile_id}".format(profile_id=self.test_profile.profile_id))
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get("/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id))
         self.assertEqual(200, response.status_code, response.content)
         self.assertEqual(
             {
-                "profile_id": self.test_profile.profile_id,
-                "person": self.test_person_with_profile.id,
+                "profile_id": self.profile.profile_id,
+                "person": self.user.id,
                 "is_saved": False,
-                "comp_name": "Test 1",
-                "comp_registered": True,
-                "comp_is_startup": False,
-                "comp_official_name": "Test 1 official name",
+                "comp_name": "Test Comp name",
+                "comp_registered": False,
+                "comp_is_startup": True,
+                "comp_official_name": "Test Official Startup",
                 "comp_region": "E",
-                "comp_common_info": "Test 1 common info",
-                "comp_phone_number": "380990102034",
-                "comp_EDRPOU": 10000001,
-                "comp_year_of_foundation": 2020,
-                "comp_service_info": "Test 1 service info",
-                "comp_product_info": "Test 1 product info",
-                "comp_address": "Lviv",
+                "comp_common_info": "test common info",
+                "comp_phone_number": "380100102034",
+                "comp_EDRPOU": 99999999,
+                "comp_year_of_foundation": 2022,
+                "comp_service_info": "test service info",
+                "comp_product_info": "test product info",
+                "comp_address": "Test Country, Test City, St. Test, 1",
                 "comp_banner_image": None,
-                "person_position": None,
-                "startup_idea": "Test 1 start up idea",
+                "person_position": "Test",
+                "startup_idea": "Test startup idea",
                 "is_deleted": False,
                 "comp_category": [],
                 "comp_activity": []
@@ -148,7 +147,7 @@ class TestProfileDetailAPIView(APITestCase):
 
         # check the profile is deleted
         response = self.client.get(path="/api/profiles/")
-        self.assertEqual(0, len(response.data["results"]))
+        self.assertEqual(0, response.data["total_items"])
         self.profile.refresh_from_db()
         self.assertTrue(self.profile.is_deleted)
 
@@ -157,14 +156,16 @@ class TestProfileDetailAPIView(APITestCase):
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
 
     def test_delete_profile_of_other_user_authorized(self):
-        profile2 = ProfileCompanyFactory()
+        profile2 = ProfileStartupFactory()
         self.client.force_authenticate(self.user)
+
         response = self.client.delete(path="/api/profiles/{profile_id}".format(profile_id=profile2.profile_id))
         self.assertEqual(403, response.status_code)
 
     # PATCH requests section
     def test_partial_update_profile_authorized(self):
         self.client.force_authenticate(self.user)
+
         response = self.client.patch(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
@@ -176,8 +177,9 @@ class TestProfileDetailAPIView(APITestCase):
         self.assertEqual("Test_company", response.data.get("comp_official_name"))
 
     def test_partial_update_authorized_not_owner(self):
-        profile2 = ProfileCompanyFactory()
+        profile2 = ProfileStartupFactory()
         self.client.force_authenticate(self.user)
+
         response = self.client.patch(
             path="/api/profiles/{profile_id}".format(profile_id=profile2.profile_id),
             data={
@@ -188,6 +190,7 @@ class TestProfileDetailAPIView(APITestCase):
 
     def test_partial_update_profile_with_wrong_image(self):
         self.client.force_authenticate(self.user)
+
         response = self.client.patch(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
@@ -207,8 +210,9 @@ class TestProfileDetailAPIView(APITestCase):
 
     def test_partial_update_profile_not_exist(self):
         self.client.force_authenticate(self.user)
+
         response = self.client.patch(
-            path="/api/profiles/{profile_id}".format(profile_id=1000000000000),
+            path="/api/profiles/{profile_id}".format(profile_id=0),
             data={
                 "comp_official_name": "Test_company",
                 "comp_year_of_foundation": 2005
@@ -217,6 +221,7 @@ class TestProfileDetailAPIView(APITestCase):
 
     def test_partial_update_profile_wrong_data(self):
         self.client.force_authenticate(self.user)
+
         response = self.client.patch(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
@@ -228,6 +233,7 @@ class TestProfileDetailAPIView(APITestCase):
     def test_partial_update_profile_category(self):
         category = CategoryFactory()
         self.client.force_authenticate(self.user)
+
         response = self.client.patch(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
@@ -240,6 +246,7 @@ class TestProfileDetailAPIView(APITestCase):
     def test_partial_update_profile_activity(self):
         activity = ActivityFactory()
         self.client.force_authenticate(self.user)
+
         response = self.client.patch(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
@@ -252,11 +259,12 @@ class TestProfileDetailAPIView(APITestCase):
     # PUT requests section
     def test_full_update_profile_authorized_with_partial_data(self):
         self.client.force_authenticate(self.user)
+
         response = self.client.put(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
-                "comp_region": 'Kyiv',
-                "comp_phone_number": 167300044419991
+                "comp_region": 'W',
+                "comp_phone_number": '380100109934'
             })
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
@@ -264,11 +272,12 @@ class TestProfileDetailAPIView(APITestCase):
         category = CategoryFactory()
         activity = ActivityFactory()
         self.client.force_authenticate(self.user)
+
         response = self.client.put(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
                 "person": self.user.id,
-                "comp_official_name": "Jannifer",
+                "comp_official_name": "Jennifer",
                 "comp_region": 'E',
                 "comp_common_info": "Good Very",
                 "comp_phone_number": 123456789012,
@@ -293,12 +302,13 @@ class TestProfileDetailAPIView(APITestCase):
     def test_full_update_profile_unauthorized(self):
         category = CategoryFactory()
         activity = ActivityFactory()
+
         response = self.client.put(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
                 "profile_id": self.profile.profile_id,
                 "person": self.user.id,
-                "comp_official_name": "Jannifer",
+                "comp_official_name": "Jennifer",
                 "comp_region": 'E',
                 "comp_common_info": "Good Very",
                 "comp_phone_number": 123456789012,
@@ -322,12 +332,13 @@ class TestProfileDetailAPIView(APITestCase):
 
     def test_full_update_profile_not_exist(self):
         self.client.force_authenticate(self.user)
+
         response = self.client.put(
-            path="/api/profiles/{profile_id}".format(profile_id=10000000000),
+            path="/api/profiles/{profile_id}".format(profile_id=0),
             data={
                 "profile_id": self.profile.profile_id,
                 "person": self.user.id,
-                "comp_official_name": "Jannifer",
+                "comp_official_name": "Jennifer",
                 "comp_region": 'E',
                 "comp_common_info": "Good Very",
                 "comp_phone_number": 123456789012,
@@ -351,12 +362,13 @@ class TestProfileDetailAPIView(APITestCase):
 
     def test_full_update_profile_wrong_data(self):
         self.client.force_authenticate(self.user)
+
         response = self.client.put(
             path="/api/profiles/{profile_id}".format(profile_id=self.profile.profile_id),
             data={
                 "profile_id": self.profile.profile_id,
                 "person": self.user.id,
-                "comp_official_name": "Jannifer",
+                "comp_official_name": "Jennifer",
                 "comp_region": 'E',
                 "comp_common_info": "Good Very",
                 "comp_phone_number": 123456789012,
@@ -386,7 +398,7 @@ class TestProfileDetailAPIView(APITestCase):
             data={
                 "profile_id": self.profile.profile_id,
                 "person": self.user.id,
-                "comp_official_name": "Jannifer",
+                "comp_official_name": "Jennifer",
                 "comp_region": 'E',
                 "comp_common_info": "Good Very",
                 "comp_phone_number": 123456789012,
@@ -414,6 +426,7 @@ class TestProfileDetailAPIView(APITestCase):
         category = CategoryFactory()
         activity = ActivityFactory()
         self.client.force_authenticate(user2)
+
         response = self.client.post(
             path="/api/profiles/",
             data={
@@ -428,6 +441,7 @@ class TestProfileDetailAPIView(APITestCase):
         category = CategoryFactory()
         activity = ActivityFactory()
         self.client.force_authenticate(user2)
+
         response = self.client.post(
             path="/api/profiles/",
             data={
@@ -474,6 +488,7 @@ class TestProfileDetailAPIView(APITestCase):
             "person_position": "director",
             "startup_idea": "StartUp idea from test case",
             "is_deleted": False,
+            "is_saved": False,
             "comp_category": [
                 category.category_id
             ],
@@ -486,6 +501,7 @@ class TestProfileDetailAPIView(APITestCase):
         user2 = UserFactory()
         category = CategoryFactory()
         activity = ActivityFactory()
+
         response = self.client.post(
             path="/api/profiles/",
             data={
@@ -496,14 +512,14 @@ class TestProfileDetailAPIView(APITestCase):
         self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_create_profile_already_exists(self):
-        self.client.force_authenticate(self.user)
-        user2 = UserFactory()
         category = CategoryFactory()
         activity = ActivityFactory()
+        self.client.force_authenticate(self.user)
+
         response = self.client.post(
             path="/api/profiles/",
             data={
-                "person": user2.id,
+                "person": self.user.id,
                 "comp_category": [category.category_id],
                 "comp_activity": [activity.activity_id]
             })
@@ -512,6 +528,7 @@ class TestProfileDetailAPIView(APITestCase):
     def test_create_profile_wrong_data(self):
         user2 = UserFactory()
         self.client.force_authenticate(user2)
+
         response = self.client.post(
             path="/api/profiles/",
             data={
