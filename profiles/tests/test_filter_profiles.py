b075e2df22b70014a176b3529509d497b31ae78d
Oleksandra <mremarka314@gmail.com>
2023-09-13 01:04:19 +0300

    add saved filter tests to company type test class

--- profiles/tests/test_filter_profiles.py
+++ profiles/tests/test_filter_profiles.py
@@ -1,40 +1,162 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from authentication.factories import UserFactory
 from profiles.factories import ProfileStartupFactory, ProfileCompanyFactory, ActivityFactory
+from profiles.factories import SavedCompanyFactory, SavedStartupFactory
 from profiles.models import Profile
 from utils.dump_response import dump  # noqa
 
 
 class TestProfileFilterCompanyType(APITestCase):
     def setUp(self) -> None:
-        self.company = ProfileCompanyFactory.create_batch(3)
-        self.startups = ProfileStartupFactory.create_batch(4)
+        self.user = UserFactory()
+        self.company = ProfileCompanyFactory()
+        self.startup = ProfileStartupFactory()
 
-    def test_get_all_profiles_any_user_no_filters(self):
-        response = self.client.get(path="/api/profiles/?page=1&page_size=12")
-        self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(7, response.data["total_items"])
+    def test_get_saved_companies_unauthorized(self):
+        response = self.client.get(path="/api/profiles/?filters=is_saved")
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
-    def test_get_all_profiles_any_user_filter_companies(self):
+    # TODO: add get all profiles w/o filters test
+
+    def test_get_profiles_filter_companies_unauthorized_count(self):
         response = self.client.get(
             path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="company"))
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(3, response.data["total_items"])
-        self.assertTrue(all(
-            response.data["results"][i]["comp_is_startup"] is False and response.data["results"][i][
-                "comp_registered"] is True
-            for i in range(len(response.data["results"]))))
+        # self.assertEqual(len(self.company), response.data["total_items"])
+        self.assertEqual(1, response.data["total_items"])
 
-    def test_get_all_profiles_any_user_filter_startups(self):
+    def test_get_profiles_filter_companies_unauthorized_content(self):
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="company"))
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(self.company.profile_id, response.data["results"][0]["profile_id"])
+        # self.assertTrue(all(
+        #     [self.companies[i].profile_id == response.data["results"][i]["profile_id"]
+        #      for i in range(len(self.companies))]
+        # ))
+
+    def test_get_profiles_filter_startups_unauthorized_count(self):
         response = self.client.get(
             path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="startup"))
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(4, response.data["total_items"])
-        self.assertTrue(all(
-            response.data["results"][i]["comp_is_startup"] is True and response.data["results"][i][
-                "comp_registered"] is False
-            for i in range(len(response.data["results"]))))
+        # self.assertEqual(len(self.startup), response.data["total_items"])
+        self.assertEqual(1, response.data["total_items"])
+
+    def test_get_profiles_filter_startups_unauthorized_content(self):
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="startup"))
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(self.startup.profile_id, response.data["results"][0]["profile_id"])
+
+    def test_get_profiles_filter_companies_authorized_count(self):
+        SavedCompanyFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="company"))
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(2, response.data["total_items"], msg="Total result amount doesn't match.")
+        self.assertEqual(
+            1, len([company["is_saved"] for company in response.data["results"] if company["is_saved"]]),
+            msg="Saved companies count doesn't match."
+        )
+
+    def test_get_profiles_filter_companies_authorized_content(self):
+        saved_company = SavedCompanyFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="company"))
+
+        saved_from_response = [company for company in response.data["results"] if company["is_saved"]]
+
+        all_companies_ids_from_db = [saved_company.company.profile_id, self.company.profile_id]
+        all_companies_ids_from_response = [company["profile_id"] for company in response.data["results"]]
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(saved_company.company.profile_id, saved_from_response[0]["profile_id"],
+                         msg="Saved companies do not match")
+        self.assertCountEqual(all_companies_ids_from_db, all_companies_ids_from_response,
+                              msg="Companies do not match")
+
+    def test_get_profiles_filter_companies_saved_authorized_count(self):
+        SavedCompanyFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&filters={filter}&page=1&page_size=12".format(
+                company_type="company", filter="is_saved"))
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(1, response.data["total_items"])
+
+    def test_get_profiles_filter_companies_saved_authorized_content(self):
+        saved_company = SavedCompanyFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&filters={filter}&page=1&page_size=12".format(
+                company_type="company", filter="is_saved"))
+        saved_from_response = [company for company in response.data["results"] if company["is_saved"]]
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(saved_company.company.profile_id, saved_from_response[0]["profile_id"],
+                         msg="Saved companies do not match")
+
+    def test_get_profiles_filter_startups_authorized_count(self):
+        SavedStartupFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="startup"))
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(2, response.data["total_items"])
+        self.assertEqual(
+            1, len([startup["is_saved"] for startup in response.data["results"] if startup["is_saved"]]),
+            msg="Saved startups count doesn't match."
+        )
+
+    def test_get_profiles_filter_startups_authorized_content(self):
+        saved_startup = SavedStartupFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&page=1&page_size=12".format(company_type="startup"))
+
+        saved_from_response = [startup for startup in response.data["results"] if startup["is_saved"]]
+
+        all_startups_ids_from_db = [saved_startup.company.profile_id, self.startup.profile_id]
+        all_startups_ids_from_response = [startup["profile_id"] for startup in response.data["results"]]
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(saved_startup.company.profile_id, saved_from_response[0]["profile_id"],
+                         msg="Saved startups do not match")
+        self.assertCountEqual(all_startups_ids_from_db, all_startups_ids_from_response,
+                              msg="Startups do not match")
+
+    def test_get_profiles_filter_startups_saved_authorized_count(self):
+        SavedStartupFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&filters={filter}&page=1&page_size=12".format(
+                company_type="startup", filter="is_saved"))
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(1, response.data["total_items"])
+
+    def test_get_profiles_filter_startups_saved_authorized_content(self):
+        saved_startup = SavedStartupFactory(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.get(
+            path="/api/profiles/?company_type={company_type}&filters={filter}&page=1&page_size=12".format(
+                company_type="startup", filter="is_saved"))
+        saved_from_response = [startup for startup in response.data["results"] if startup["is_saved"]]
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(saved_startup.company.profile_id, saved_from_response[0]["profile_id"],
+                         msg="Saved startups do not match")
 
 
 class TestProfileFilterActivityType(APITestCase):
