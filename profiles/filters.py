e500e3d1ec4e44d02063648e48fc49b11dacb5bf
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-09-18 16:16:31 +0200

    redundant methods removed, filter names upd

--- profiles/filters.py
+++ profiles/filters.py
@@ -4,9 +4,9 @@ from django_filters.rest_framework import FilterSet
 
 class ProfileFilter(FilterSet):
     is_saved = filters.BooleanFilter(method="is_saved_filter")
-    company = filters.BooleanFilter(field_name="is_registered")
-    startup = filters.BooleanFilter(field_name="is_startup")
-    activity_type = filters.CharFilter(field_name="activities__name")
+    is_registered = filters.BooleanFilter()
+    is_startup = filters.BooleanFilter()
+    activities__name = filters.CharFilter()
 
     def is_saved_filter(self, queryset, name, value):
         if value:
@@ -14,19 +14,4 @@ class ProfileFilter(FilterSet):
                 queryset = queryset.filter(saved_list__user=self.request.user)
             else:
                 queryset = queryset.none()
-        return queryset
-
-    def company_filter(self, queryset, name, value):
-        if value:
-            return queryset.filter(is_registered=True)
-        return queryset
-    
-    def startup_filter(self, queryset, name, value):
-        if value:
-            return queryset.filter(is_startup=True)
-        return queryset
-
-    def activity_type_filter(self, queryset, name, value):
-        if value:
-            queryset = queryset.filter(activities__name=value)
         return queryset
