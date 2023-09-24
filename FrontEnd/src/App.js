131bb477193f9e718e86ea2558dc3f8c9450e3da
Sofia Bilous <EgikSonya@gmail.com>
2023-09-24 15:52:46 +0300

    fix .env in root directory

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AdminPage from "./components/adminPage/AdminPage";
-import BasicPage from "./components/basicPage/BasicPage";
-
+import AdminPage from './components/adminPage/AdminPage';
+import BasicPage from './components/basicPage/BasicPage';
 
 function App() {
+  console.log('************************', process.env.REACT_APP_API_URL);
   return (
     <BrowserRouter>
       <div className="App">
