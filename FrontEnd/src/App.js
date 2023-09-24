6376aa451aee0001daa54c9bdeb8961ef3d186aa
Sofia Bilous <EgikSonya@gmail.com>
2023-09-24 16:54:13 +0300

    delete console.log

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -5,7 +5,6 @@ import AdminPage from './components/adminPage/AdminPage';
 import BasicPage from './components/basicPage/BasicPage';
 
 function App() {
-  console.log('************************', process.env.REACT_APP_API_URL);
   return (
     <BrowserRouter>
       <div className="App">
