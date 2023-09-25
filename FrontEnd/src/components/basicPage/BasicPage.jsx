150f0feca3a6b64dedbb82facaa5ee2256b1f6b3
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-09-25 12:29:23 +0200

    app and basic page upd

--- FrontEnd/src/components/basicPage/BasicPage.jsx
+++ FrontEnd/src/components/basicPage/BasicPage.jsx
@@ -1,5 +1,5 @@
 import Header from "../HeaderFooter/header/Header";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import MainPage from "../landing-page/MainPage";
 import ProfilePage from "../ProfilePage/ProfilePage";
 import AuthorizationPage from "../authorization/AuthorizationPage";
@@ -10,24 +10,33 @@ import Footer from "../HeaderFooter/footer/Footer";
 import ScrollToTopButton from "../PrivacyPolicyPage/privacy/ScrollToTopButton";
 import React from "react";
 import CookiesPolicyComponent from "../CookiesPolicyPage/CookiesPolicyComponent";
+import { AuthContext } from "../../context";
+import { useProvideAuth } from "../../hooks";
 
 
 function BasicPage() {
+    const auth = useProvideAuth();
     return (
-        <div>
-            <Header />
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/profile/*" element={<ProfilePage />} />
-                <Route path="/authorization" element={<AuthorizationPage />} />
-                <Route path="/sign-up" element={<SignUpPage />} />
-                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-                <Route path="/cookies-policy" element={<CookiesPolicyComponent />} />
-            </Routes>
-            <Footer />
-            <ScrollToTopButton />
-        </div>
+        <AuthContext.Provider value={auth}>
+            <div>
+                <Header isAuthorized={ auth.isAuth }/>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/profile/*" element={<ProfilePage />} />
+                    { auth.isAuth ? (
+                    <Route path="/authorization" element={<Navigate to="/profile/user-info" />} />
+                    ) : (
+                    <Route path="/authorization" element={<AuthorizationPage />} />
+                    )}
+                    <Route path="/sign-up" element={<SignUpPage />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                    <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+                    <Route path="/cookies-policy" element={<CookiesPolicyComponent />} />
+                </Routes>
+                <Footer />
+                <ScrollToTopButton />
+            </div>
+        </AuthContext.Provider>
     );
 };
 
