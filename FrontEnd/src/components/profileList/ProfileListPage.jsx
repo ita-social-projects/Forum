0dff2b80cba327a230f51e3954c4d4db15484368
Oleksandra <mremarka314@gmail.com>
2023-09-25 23:29:15 +0300

    add all/saved switch

--- FrontEnd/src/components/profileList/ProfileListPage.jsx
+++ FrontEnd/src/components/profileList/ProfileListPage.jsx
@@ -1,5 +1,12 @@
+import css from "./ProfileListPage.module.css";
+import ProfileList from "./ProfileList";
+import AllSavedGroup from "./AllSavedGroup";
+
 export default function ProfileListPage() {
-    return <>
-        <div>Hello</div>
-    </>
+  return (
+    <div className={css["page-container"]}>
+      <AllSavedGroup />
+      <ProfileList />
+    </div>
+  );
 }
