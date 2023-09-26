53860ae698058db1dbb054cfa2ec9c91a2bf088f
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-09-26 19:52:40 +0200

    logout functionality added

--- FrontEnd/src/components/HeaderFooter/header/navbar/Profile.jsx
+++ FrontEnd/src/components/HeaderFooter/header/navbar/Profile.jsx
@@ -1,16 +1,57 @@
 import avatar_image from "./Avatar.png";
 import css from "./Profile.module.css";
-import { Link } from 'react-router-dom';
+import Logout from "./Logout";
+import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 function Profile(props) {
-    return (
-        <div className={css["header-profile-section"]}>
-            <img className={css["header-profile__avatar"]} src={avatar_image} alt=""/>
-            <div className={css["header-profile-tab"]}>
-                <Link to='/profile/user-info' className={css["header-profile-link__text"]}>Профіль</Link>
-            </div>
-        </div>
-    );
-};
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const onBodyClick = (e) => {
+      if (!ref.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.body.addEventListener("mousedown", onBodyClick);
+
+    return () => {
+      document.body.removeEventListener("mousedown", onBodyClick);
+    };
+  }, []);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={css["header-profile-section"]} ref={ref}>
+      <img
+        className={css["header-profile__avatar"]}
+        src={avatar_image}
+        alt=""
+      />
+      <div className={css["header-profile-tab"]}>
+        <span className={css["header-profile-tab__text"]} onClick={handleOpen}>
+          Профіль
+        </span>
+        {isOpen && (
+          <div className={css["header-profile-dropdown-menu"]}>
+            <Link
+              to="/profile/user-info"
+              className={css["header-profile-dropdown-menu__text"]}
+            >
+              Профіль
+            </Link>
+            <Logout className={css["header-profile-dropdown-menu__text"]}>
+              Вихід{" "}
+            </Logout>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
 export default Profile;
