604b240cc63d7de5f812d0b0e8632d5dc3b52f27
Oleksandra <mremarka314@gmail.com>
2023-09-26 16:12:41 +0300

    add star & badges

--- FrontEnd/src/components/profileList/ProfileCard.jsx
+++ FrontEnd/src/components/profileList/ProfileCard.jsx
@@ -1,16 +1,56 @@
-import { StarOutlined, StarFilled, StarTwoTone } from "@ant-design/icons";
 import css from "./ProfileCard.module.css";
-import { Badge, Space, Switch } from 'antd';
+import { Badge, Space, Switch, Typography } from "antd";
+import { useState } from "react";
+import { StarOutlined, StarFilled } from "@ant-design/icons";
+
+const { Paragraph } = Typography;
+
 export default function ProfileCard() {
+  const [isSaved, setIsSaved] = useState(false);
+  const [show, setShow] = useState(true);
+
   const profile1 = {
     companyName: "Ace&W by Stakhovsky",
     activities: "Виробник",
     region: "Львівська обл",
     address: "Львів",
-    catogories: ["cat1", "cat2", "cat3", "cat4"],
+    catogories: ["Сироваріння", "КАТЕГОРІЯ", "КАТЕГОРІЯ", "КАТЕГОРІЯ"],
+    isSaved: isSaved,
     commonInfo:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac condimentum nunc, eu bibendum odio. Donec porttitor tincidunt enim, at cursus diam efficitur sed. Cras sapien diam, efficitur in pretium sit amet, blandit vel nisi. Quisque facilisis sapien non mauris pharetra, sit amet tristique turpis placerat. Integer eleifend faucibus tristique. Etiam sed justo diam. Pellentesque vel elit at lectus elementum pellentesque quis vel erat.Proin laoreet, ipsum eget vestibulum ullamcorper, turpis nisl aliquam arcu, vitae dapibus ipsum nibh varius justo. Mauris dignissim iaculis libero non euismod. Maecenas massa purus, tincidunt sit amet enim et, scelerisque eleifend mauris. Aliquam euismod viverra mauris, ut interdum est venenatis nec. Mauris malesuada libero ut placerat semper. Cras sit amet vehicula metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam congue vestibulum neque, ac volutpat neque. Suspendisse semper turpis tincidunt elit pulvinar laoreet. Pellentesque convallis vitae risus at pellentesque. Ut eget viverra erat. Donec molestie mauris lacus, maximus sollicitudin nulla volutpat ullamcorper. Integer commodo cursus arcu ac ullamcorper. Donec vulputate eros est, at pretium neque faucibus eu.",
   };
+
+  const filledStar = (
+    <StarFilled
+      style={{ color: "#FFD800", fontSize: "24px" }}
+      onClick={onClick}
+    />
+  );
+  const outlinedStar = (
+    <StarOutlined
+      style={{ color: "#FFD800", fontSize: "24px" }}
+      onClick={onClick}
+    />
+  );
+  const badges = profile1.catogories.map((category) => (
+    <Badge
+      size="medium"
+      count={category.toUpperCase()}
+      style={{
+        backgroundColor: "#1F9A7C",
+        fontWeight: 600,
+        fontFamily: "Inter",
+        // lineHeight: 16
+      }}
+    />
+  ));
+
+  function onClick() {
+    // TODO: add request to add/remove to saved
+    setIsSaved(!isSaved);
+  }
+  // TODO: add getting info
+
   return (
     <div className={css["company-card"]}>
       <div className={css.logo}>logo</div>
@@ -29,20 +69,15 @@ export default function ProfileCard() {
             {profile1.region}
           </div>
         </div>
-        <div className={css["content__common-info"]}>{profile1.commonInfo}</div>
-        <div className={css["content__categories"]}>
-            <Badge color="#faad14">{profile1.catogories[0]}</Badge>
-            <Badge>{profile1.catogories[1]}</Badge>
-            <Badge>{profile1.catogories[2]}</Badge>
-            <Badge>{profile1.catogories[3]}</Badge>
-            {/* {profile1.catogories}</div> */}
-            </div>
+        <div className={css["content__common-info"]}>
+          <Paragraph ellipsis={{ rows: 3, expandable: false }}>
+            {profile1.commonInfo}
+          </Paragraph>
+        </div>
+        // TODO: add badges
+        <div className={css["content__categories"]}>{badges}</div>
       </div>
-
-      {/* {/* <StarOutlined /> */}
-      <StarFilled style={{ color: "#FFD800", fontSize: "24px" }} />
-      {/* <StarFilled color="#FFD800" /> */}
-      {/* <StarTwoTone twoToneColor="#eb2f96" /> */}
+      {isSaved ? filledStar : outlinedStar}
     </div>
   );
 }
