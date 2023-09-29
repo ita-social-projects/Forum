9ebb5d13e60a47ee473a5e1dc05d9d86bc4d3125
Oleksandra <mremarka314@gmail.com>
2023-09-29 12:58:08 +0300

    split urls & add loader

--- FrontEnd/src/components/profileList/ProfileListPage.jsx
+++ FrontEnd/src/components/profileList/ProfileListPage.jsx
@@ -3,7 +3,9 @@ import ProfileList from "./ProfileList";
 import useSWR from "swr";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
-import { ConfigProvider, Radio } from "antd";
+import { ConfigProvider, Radio, Skeleton, Spin } from "antd";
+import Loader from "./Loader";
+import ErrorPage from "./ErrorPage";
 
 const optionSet = [
   {
@@ -18,11 +20,11 @@ const optionSet = [
 
 export default function ProfileListPage(props) {
   let isAuthorized = props.isAuthorized;
-  
+
   const { filter } = useParams();
-  
+
   let profileTypeFilter = "";
-  
+
   let profileActivityFilter = "activities__name=";
   const emptyActivityFilter = profileActivityFilter.length;
 
@@ -52,72 +54,87 @@ export default function ProfileListPage(props) {
       break;
   }
   // isAuthorized = false;
-  const urlForAll = `${process.env.REACT_APP_BASE_API_URL}/api/profiles/?${profileTypeFilter}${
+  const urlForAll = `${
+    process.env.REACT_APP_BASE_API_URL
+  }/api/profiles/?${profileTypeFilter}${
     profileActivityFilter.length > emptyActivityFilter
       ? profileActivityFilter
       : ""
-  }&page=${currentPage}`
+  }&page=${currentPage}`;
 
-  const urlForSaved = `${process.env.REACT_APP_BASE_API_URL}/api/profiles/?${profileTypeFilter}${
+  const urlForSaved = `${
+    process.env.REACT_APP_BASE_API_URL
+  }/api/profiles/?${profileTypeFilter}${
     profileActivityFilter.length > emptyActivityFilter
       ? profileActivityFilter
       : ""
-  }${filterSaved ? "&is_saved=True" : ""}&page=${currentPage}`
+  }${filterSaved ? "&is_saved=True" : ""}&page=${currentPage}`;
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const {
     data: fetchedProfiles,
     error,
     isLoading,
-  } = useSWR(filterSaved ? urlForSaved : urlForAll,
-    fetcher
-  );
+  } = useSWR(filterSaved ? urlForSaved : urlForAll, fetcher);
 
-  // FIXME: fix pagination + saved filter
-
+  // FIXME: look up if useSWR is async
   const handleRadioSelect = () => {
+    if (!filterSaved) {
+      setCurrentPage(1);
+    }
     setFilterSaved(!filterSaved);
   };
+  // isLoading = true;
+  const loading = true;
+  const error404 = true;
 
   return (
-    <div className={css.page}>
-      <div className={css["page-content"]}>
-        {isAuthorized ? (
-          <div className={css.group}>
-            <ConfigProvider
-              theme={{
-                components: {
-                  Radio: {
-                    colorPrimary: "#1f9a7c",
-                    borderRadius: 2,
-                    colorBorder: "#DEE1E8",
-                    buttonColor: "#25292C",
-                    fontFamily: "Inter",
-                    fontSize: 16,
-                    algorithm: true, // Enable algorithm
+    <div className={isLoading || error404 ? css["loading"] : css.page}>
+      {error404 ? (
+        <ErrorPage />
+      ) : (
+        <div className={css["page-content"]}>
+          {isAuthorized ? (
+            <div className={css.group}>
+              <ConfigProvider
+                theme={{
+                  components: {
+                    Radio: {
+                      colorPrimary: "#1f9a7c",
+                      borderRadius: 2,
+                      colorBorder: "#DEE1E8",
+                      buttonColor: "#25292C",
+                      fontFamily: "Inter",
+                      fontSize: 16,
+                      algorithm: true, // Enable algorithm
+                    },
                   },
-                },
-              }}
-            >
-              <Radio.Group
-                options={optionSet}
-                onChange={handleRadioSelect}
-                value={filterSaved}
-                optionType="button"
-                buttonStyle="solid"
-                size="large"
-              />
-            </ConfigProvider>
-          </div>
-        ) : null}
-        {isLoading || error ? null : (
-          <ProfileList
-            isAuthorized={isAuthorized}
-            data={fetchedProfiles}
-            paginationFunc={setCurrentPage}
-          />
-        )}
-      </div>
+                }}
+              >
+                <Radio.Group
+                  options={optionSet}
+                  onChange={handleRadioSelect}
+                  value={filterSaved}
+                  optionType="button"
+                  buttonStyle="solid"
+                  size="large"
+                />
+              </ConfigProvider>
+            </div>
+          ) : null}
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <ProfileList
+              isAuthorized={isAuthorized}
+              isLoading={isLoading}
+              data={fetchedProfiles}
+              paginationFunc={setCurrentPage}
+              current={currentPage}
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 }
