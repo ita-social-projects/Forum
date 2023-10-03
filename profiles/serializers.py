e94ee8d25a3ddaa7fb7d849bd1e34e9efdcc6292
Oleksandra <mremarka314@gmail.com>
2023-10-03 14:52:58 +0300

    merge cleanup

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -13,6 +13,7 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = "__all__"
 
+
 class ProfileListSerializer(serializers.ModelSerializer):
     activities = ActivitySerializer(many=True, read_only=True)
     categories = CategorySerializer(many=True, read_only=True)
