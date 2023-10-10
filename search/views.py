b084e5d449059c0f8a94d850583e38c8db7666be
Sofia Bilous <EgikSonya@gmail.com>
2023-10-10 20:42:08 +0300

    ghggh

--- search/views.py
+++ search/views.py
@@ -1,11 +1,16 @@
 from rest_framework.generics import ListAPIView
+import django_filters
 
 from profiles.models import Profile
 from .serializers import CompanySerializers
+from search.filters import CompanyFilter
 
 
 class SearchCompanyView(ListAPIView):
+    queryset = Profile.objects.all()
     serializer_class = CompanySerializers
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    # filterset_class = CompanyFilter
 
     def get_queryset(self):
         search_field = self.request.query_params.get(
