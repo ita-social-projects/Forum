c4cd3f6e6468a0496d02f4d21ab46f8c888b52de
Sofia Bilous <EgikSonya@gmail.com>
2023-10-11 23:12:05 +0300

    my varient

--- search/views.py
+++ search/views.py
@@ -10,12 +10,4 @@ class SearchCompanyView(ListAPIView):
     queryset = Profile.objects.all()
     serializer_class = CompanySerializers
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    # filterset_class = CompanyFilter
-
-    def get_queryset(self):
-        search_field = self.request.query_params.get(
-            "search_field", ""
-        ).strip()
-        return Profile.objects.filter(name__icontains=search_field).order_by(
-            "name"
-        )
+    filterset_class = CompanyFilter
