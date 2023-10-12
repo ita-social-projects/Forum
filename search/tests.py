c8bdef2bae451d6e5423b9504b76440d69fc0b2d
Oleksandra <mremarka314@gmail.com>
2023-10-12 19:05:27 +0300

    discard changes (no checks have to be triggered)

--- search/tests.py
+++ search/tests.py
@@ -5,6 +5,7 @@ from authentication.factories import UserFactory
 from profiles.factories import (
     ProfileStartupFactory,
     ProfileCompanyFactory,
+    ActivityFactory, 
 )
 
 from utils.dump_response import dump  # noqa
