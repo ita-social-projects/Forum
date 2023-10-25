d9b48785a8dae9b325cbf38b370c82f6ad1621bc
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-10-25 19:27:37 +0200

    company description component added

---
+++ FrontEnd/src/components/ProfileDetail/DetailedInfo/CompanyDescription.jsx
@@ -0,0 +1,20 @@
+import classes from './CompanyDescription.module.css';
+import Company from './Company';
+import Startup from './Startup';
+import ProductsServices from './ProductsServices';
+import Logistics from './Logistics';
+import Cooperation from './Cooperation';
+
+function CompanyDescription ({ data }) {
+    return (
+        <div className={classes['company-description-block']}>
+            <Company data={data} />
+            <Startup data={data} />
+            <ProductsServices />
+            <Logistics />
+            <Cooperation />
+        </div>
+    );
+}
+
+export default CompanyDescription;
