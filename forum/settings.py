c16050c9f007d2af56f98eba0057a2d219685b24
Sofia Bilous <EgikSonya@gmail.com>
2023-10-27 17:08:03 +0300

    forgot 3000

--- forum/settings.py
+++ forum/settings.py
@@ -66,14 +66,14 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+    "http://localhost",
     "http://127.0.0.1",
     "http://0.0.0.0",
     config("CORS_ORIGIN_WHITELIST"),
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost",
     "http://127.0.0.1",
     "http://0.0.0.0",
 ]
