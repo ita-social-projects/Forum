89e2cfa21716b6aec8313292c43888c34d55982f
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-11-02 12:23:42 +0100

    navbar component updated

--- FrontEnd/src/components/ProfileDetail/MainInfo/ProfileDetailNavBar.jsx
+++ FrontEnd/src/components/ProfileDetail/MainInfo/ProfileDetailNavBar.jsx
@@ -1,46 +1,46 @@
 import { HashLink } from 'react-router-hash-link';
+import { useLocation  } from 'react-router-dom';
 import classes from './ProfileDetailNavBar.module.css';
 
+const MENU_LINKS = {
+    'about-company': 'Про компанію',
+    'startup': 'Стартап',
+    'products-services': 'Товари / послуги',
+    'logistics': 'Логістика товарів / послуг',
+    'cooperation': 'Формат співпраці',
+};
+
 function ProfileDetailNavBar() {
-  const menuItems = [
-    'Про компанію',
-    'Стартап',
-    'Товари / послуги',
-    'Логістика товарів / послуг',
-    'Формат співпраці',
-  ];
-  const menuLinks = [
-    'about-company',
-    'startup',
-    'products-services',
-    'logistics',
-    'cooperation',
-  ];
 
-  const activeHash = window.location.hash.substring(1);
+  const { hash } = useLocation ();
+  console.log('HASH', hash);
 
-  const handleItemClick = (link) => {
-    window.location.hash = link;
-  };
-
-  return (
+  return  (
     <div className={classes['navbar-menu']}>
-        {menuItems.map((item, index) => (
-        <div key={index} className={classes['navbar-menu__block']}>
-          <div
-            onClick={() => handleItemClick(menuLinks[index])}
-            className={classes['navbar-menu__item']}>
+      {Object.entries(MENU_LINKS).map(([link, label]) => (
+        <div key={link} className={classes['navbar-menu__block']}>
+          <div className={classes['navbar-menu__item']}>
             <HashLink
               smooth
-              to={`#${menuLinks[index]}`}
-              className={menuLinks[index] === activeHash ? `${classes['active-link']}` : `${classes['inactive-link']}`}
+              to={`#${link}`}
+              className={
+                `#${link}` === hash
+                  ? `${classes['active-link']}`
+                  : `${classes['inactive-link']}`
+              }
             >
-              {item}
+              {label}
             </HashLink>
           </div>
-          <div className={menuLinks[index] === activeHash ? `${classes['active-devider']}` : `${classes['inactive-devider']}`}/>
-          </div>
-        ))}
+          <div
+            className={
+              `#${link}` === hash
+                ? `${classes['active-devider']}`
+                : `${classes['inactive-devider']}`
+            }
+          />
+        </div>
+      ))}
     </div>
   );
 }
