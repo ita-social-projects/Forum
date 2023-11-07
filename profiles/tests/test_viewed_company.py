562924a3d783613a35fd1047e4ee07bec45178c8
Oleksandra <mremarka314@gmail.com>
2023-11-07 16:13:28 +0200

    adjust tests

--- profiles/tests/test_viewed_company.py
+++ profiles/tests/test_viewed_company.py
@@ -9,43 +9,35 @@ from utils.dump_response import dump  # noqa
 class TestViewedCompanyAPI(APITestCase):
     def setUp(self) -> None:
         self.user = UserFactory()
+        self.user_profile = ProfileStartupFactory(person=self.user)
 
     def test_create_viewed_company_unauthorized(self):
         profile = ProfileStartupFactory()
         response = self.client.post(
             path="/api/viewed-list/", data={"company": profile.id}
         )
-        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertIsNone(response.data["user"])
+        self.assertIsNone(response.data["user_profile_name"])
+        self.assertEqual(profile.id, response.data["company"])
+        self.assertEqual(profile.name, response.data["company_name"])
 
     def test_create_viewed_company_authorized(self):
         profile = ProfileStartupFactory()
         self.client.force_authenticate(self.user)
         response = self.client.post(
-            path="/api/viewed-list/",
-            data={"user": self.user.id, "company": profile.id},
+            path="/api/viewed-list/", data={"company": profile.id}
         )
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(self.user.id, response.data["user"])
+        self.assertEqual(self.user_profile.name, response.data["user_profile_name"])
         self.assertEqual(profile.id, response.data["company"])
+        self.assertEqual(profile.name, response.data["company_name"])
 
     def test_create_viewed_company_authorized_own_company(self):
-        own_profile = ProfileStartupFactory(person=self.user)
         self.client.force_authenticate(self.user)
         response = self.client.post(
             path="/api/viewed-list/",
-            data={"user": self.user.id, "company": own_profile.id},
+            data={"company": self.user_profile.id},
         )
-        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
-
-    def test_get_viewed_companies_unauthorized(self):
-        response = self.client.post(path="/api/viewed-list/")
-        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
-
-    def test_get_viewed_companies_authorized(self):
-        ViewedCompanyFactory.create_batch(3, user=self.user)
-        self.client.force_authenticate(self.user)
-
-        response = self.client.get(path="/api/viewed-list/")
-
-        self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(3, response.data["total_items"])
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
