080efde48624384c79eab94fa2d4c62b55e6848e
whooaami <yarkos03@gmail.com>
2023-11-10 23:52:05 +0200

    Fix formatting

--- FrontEnd/src/components/RestorePassword/components/restorepassword/restorepassword-form/RestorePasswordFormContent.js
+++ FrontEnd/src/components/RestorePassword/components/restorepassword/restorepassword-form/RestorePasswordFormContent.js
@@ -66,87 +66,94 @@ export function RestorePasswordFormContentComponent({ setIsValid }) {
 
   return (
     <div>
-
-    <div className={styles['reset-password-form']}>
-      <form
-        id="resetPasswordForm"
-        className={styles['reset-password-form__container']}
-        onSubmit={handleSubmit(onSubmit)}
-        autoComplete="off"
-        noValidate
-      >
-        <div className={styles['reset-password-form__row']}>
-          <div className={styles['reset-password-form__column']}>
-            <div className={styles['reset-password-form__label']}>
-              <label className={styles['reset-password-form__label--required']}>*</label>
-              <div className={styles['reset-password-form__label--password']}>
-                <label>Новий пароль</label>
-                <label className={styles['reset-password-form__label--hint']}>
-                  (Повинен містити A-Z, a-z, 0-9)
+      <div className={styles['reset-password-form']}>
+        <form
+          id="resetPasswordForm"
+          className={styles['reset-password-form__container']}
+          onSubmit={handleSubmit(onSubmit)}
+          autoComplete="off"
+          noValidate
+        >
+          <div className={styles['reset-password-form__row']}>
+            <div className={styles['reset-password-form__column']}>
+              <div className={styles['reset-password-form__label']}>
+                <label
+                  className={styles['reset-password-form__label--required']}
+                >
+                  *
                 </label>
+                <div className={styles['reset-password-form__label--password']}>
+                  <label>Новий пароль</label>
+                  <label className={styles['reset-password-form__label--hint']}>
+                    (Повинен містити A-Z, a-z, 0-9)
+                  </label>
+                </div>
+              </div>
+              <div className={styles['reset-password-form__field__password']}>
+                <input
+                  className={styles['reset-password-form__input__password']}
+                  placeholder="Новий пароль"
+                  type={showPassword ? 'text' : 'password'}
+                  {...register('new_password', {
+                    required: errorMessageTemplates.required,
+                    pattern: {
+                      value: passwordPattern,
+                      message: errorMessageTemplates.password,
+                    },
+                  })}
+                />
+                <span
+                  className={styles['password-visibility']}
+                  onClick={togglePassword}
+                >
+                  {!showPassword ? <EyeInvisible /> : <EyeVisible />}
+                </span>
+              </div>
+              <div className={styles['reset-password-form__error']}>
+                {errors.new_password && errors.new_password.message}
               </div>
             </div>
-            <div className={styles['reset-password-form__field__password']}>
-              <input
-                className={styles['reset-password-form__input__password']}
-                placeholder="Новий пароль"
-                type={showPassword ? 'text' : 'password'}
-                {...register('new_password', {
-                  required: errorMessageTemplates.required,
-                  pattern: {
-                    value: passwordPattern,
-                    message: errorMessageTemplates.password,
-                  },
-                })}
-              />
-              <span
-                className={styles['password-visibility']}
-                onClick={togglePassword}
-              >
-                {!showPassword ? <EyeInvisible /> : <EyeVisible />}
-              </span>
-            </div>
-            <div className={styles['reset-password-form__error']}>
-              {errors.new_password && errors.new_password.message}
-            </div>
           </div>
-        </div>
-        <div className={styles['reset-password-form__row']}>
-          <div className={styles['reset-password-form__column']}>
-            <div className={styles['reset-password-form__label']}>
-              <label className={styles['reset-password-form__label--required']}>*</label>
-              <div className={styles['reset-password-form__label--password']}>
-                <label className={styles['reset-password-form__label--text']}>
-                  Повторіть пароль
+          <div className={styles['reset-password-form__row']}>
+            <div className={styles['reset-password-form__column']}>
+              <div className={styles['reset-password-form__label']}>
+                <label
+                  className={styles['reset-password-form__label--required']}
+                >
+                  *
                 </label>
+                <div className={styles['reset-password-form__label--password']}>
+                  <label className={styles['reset-password-form__label--text']}>
+                    Повторіть пароль
+                  </label>
+                </div>
+              </div>
+              <div className={styles['reset-password-form__field__password']}>
+                <input
+                  className={styles['reset-password-form__input__password']}
+                  placeholder="Повторіть пароль"
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  {...register('confirmPassword', {
+                    required: errorMessageTemplates.required,
+                    validate: (value) =>
+                      value === getValues('new_password') ||
+                      errorMessageTemplates.confirmPassword,
+                  })}
+                />
+                <span
+                  className={styles['password-visibility']}
+                  onClick={toggleConfirmPassword}
+                >
+                  {!showConfirmPassword ? <EyeInvisible /> : <EyeVisible />}
+                </span>
+              </div>
+              <div className={styles['reset-password-form__error']}>
+                {errors.confirmPassword && errors.confirmPassword.message}
               </div>
             </div>
-            <div className={styles['reset-password-form__field__password']}>
-              <input
-                className={styles['reset-password-form__input__password']}
-                placeholder="Повторіть пароль"
-                type={showConfirmPassword ? 'text' : 'password'}
-                {...register('confirmPassword', {
-                  required: errorMessageTemplates.required,
-                  validate: (value) =>
-                    value === getValues('new_password') ||
-                    errorMessageTemplates.confirmPassword,
-                })}
-              />
-              <span
-                className={styles['password-visibility']}
-                onClick={toggleConfirmPassword}
-              >
-                {!showConfirmPassword ? <EyeInvisible /> : <EyeVisible />}
-              </span>
-            </div>
-            <div className={styles['reset-password-form__error']}>
-              {errors.confirmPassword && errors.confirmPassword.message}
-            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
     </div>
   );
 }
