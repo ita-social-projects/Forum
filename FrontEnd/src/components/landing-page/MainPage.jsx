e07520945f4056ed6837ee02b82852c92d6049ac
Heorhii Yatsenko <heorhii.yatsenko@x.studio>
2023-11-20 17:42:13 +0200

    added debug rows

--- FrontEnd/src/components/landing-page/MainPage.jsx
+++ FrontEnd/src/components/landing-page/MainPage.jsx
@@ -11,8 +11,8 @@ const MainPage = (props) => {
   const [modalActive, setModalActive] = useState(true);
   return (
     <div className={css['main-app']}>
-      <h1>{process.env.REACT_APP_BASE_API_URL}</h1>
-      <h1>{process.env.REACT_APP_PUBLIC_URL}</h1>
+      <h1>HELLO {process.env.REACT_APP_BASE_API_URL}</h1>
+      <h1>WORLD {process.env.REACT_APP_PUBLIC_URL}</h1>
       <div className={css['main-app-header']}>
         <MainBanner isAuthorized={props.isAuthorized}/>
         <div className={css['main-app-body']}>
