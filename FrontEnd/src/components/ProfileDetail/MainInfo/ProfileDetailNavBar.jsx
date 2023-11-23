a02036d506952224cbc35a48919efbc2d3905a4c
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-11-23 23:35:34 +0100

    update navigation

--- FrontEnd/src/components/ProfileDetail/MainInfo/ProfileDetailNavBar.jsx
+++ FrontEnd/src/components/ProfileDetail/MainInfo/ProfileDetailNavBar.jsx
@@ -1,5 +1,7 @@
 import { HashLink } from 'react-router-hash-link';
-import { useLocation  } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { PropTypes } from 'prop-types';
 import classes from './ProfileDetailNavBar.module.css';
 
 const MENU_LINKS = {
@@ -10,38 +12,65 @@ const MENU_LINKS = {
     'cooperation': 'Формат співпраці',
 };
 
-function ProfileDetailNavBar() {
-
+function ProfileDetailNavBar({ data }) {
   const { hash } = useLocation ();
+  const navigate = useNavigate ();
+  const [activeLink, setActiveLink] = useState('');
 
-  return  (
-    <div className={classes['navbar-menu']}>
-      {Object.entries(MENU_LINKS).map(([link, label]) => (
-        <div key={link} className={classes['navbar-menu__block']}>
-          <div className={classes['navbar-menu__item']}>
-            <HashLink
-              smooth
-              to={`#${link}`}
-              className={
-                `#${link}` === hash
-                  ? `${classes['active-link']}`
-                  : `${classes['inactive-link']}`
-              }
-            >
-              {label}
-            </HashLink>
-          </div>
-          <div
-            className={
-              `#${link}` === hash
-                ? `${classes['active-devider']}`
-                : `${classes['inactive-devider']}`
-            }
-          />
-        </div>
-      ))}
+  useEffect(() => {
+    if (hash) {
+      setActiveLink(hash.substring(1));
+    } else {
+      setActiveLink(
+        data.is_registered && document.getElementById('about-company') ? 'about-company' :
+        data.is_startup && document.getElementById('startup') ? 'startup' : ''
+      );
+    }
+  }, [hash, data.is_registered, data.is_startup]);
+
+  useEffect(() => {
+    navigate(hash.pathname, {replace: true});
+  }, [navigate, hash.pathname]);
+
+  return (
+    <div>
+      <div className={classes['navbar-menu']}>
+        {Object.entries(MENU_LINKS).map(
+          ([link, label]) =>
+            document.getElementById(link) && (
+              <div key={link} className={classes['navbar-menu__block']}>
+                <div className={classes['navbar-menu__item']}>
+                  <HashLink
+                    smooth
+                    to={`#${link}`}
+                    className={
+                      link === activeLink
+                        ? `${classes['active-link']}`
+                        : `${classes['inactive-link']}`
+                    }
+                  >
+                    {label}
+                  </HashLink>
+                </div>
+                <div
+                  className={
+                    link === activeLink && `${classes['active-devider']}`
+                  }
+                />
+              </div>
+            )
+        )}
+      </div>
+      <div className={classes['devider']}></div>
     </div>
   );
 }
 
 export default ProfileDetailNavBar;
+
+ProfileDetailNavBar.propTypes = {
+  data: PropTypes.shape({
+    is_registered: PropTypes.bool.isRequired,
+    is_startup: PropTypes.bool.isRequired,
+  }).isRequired,
+};
