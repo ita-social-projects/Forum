44935edebff7e1f215ef97e542af62e5912927e5
Oleksandra <mremarka314@gmail.com>
2023-12-02 03:00:14 +0200

    adjust test for both options chosen

--- authentication/tests/test_user_registration.py
+++ authentication/tests/test_user_registration.py
@@ -154,8 +154,11 @@ class UserRegistrationAPITests(APITestCase):
             },
             format="json",
         )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(
-            {"comp_status": ["Please choose either registered or startup."]},
+            {
+                "name": "Jane",
+                "surname": "Smith",
+            },
             response.json(),
         )
