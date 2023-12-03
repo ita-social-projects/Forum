af83a601c6ad6fe73736c153db573652fc095dce
Lvyshnevska <lilia.vyshnevska@gmail.com>
2023-12-03 13:48:02 +0100

    disabled menu items and tooltips

--- FrontEnd/src/components/ProfilePage/ProfilePageComponents/ProfileContent.js
+++ FrontEnd/src/components/ProfilePage/ProfilePageComponents/ProfileContent.js
@@ -1,3 +1,4 @@
+import { Tooltip } from 'antd';
 import css from './ProfileContent.module.css';
 import { Link, NavLink, Route, Routes } from 'react-router-dom';
 import AdditionalInfo from '../FormComponents/AdditionalInfo';
@@ -26,14 +27,17 @@ const INFOLINKS = [
     {
         title: 'Інформація про товари/ послуги',
         link: '/products-service-info',
+        tooltipText: 'Цей розділ доступний, коли обрано опцію "Зареєстрована компанія" у розділі Загальна інформація'
     },
     {
         title: 'Додаткова інформація',
         link: '/additional-info',
+        tooltipText: 'Цей розділ доступний, коли обрано опцію "Зареєстрована компанія" у розділі Загальна інформація'
     },
     {
         title: 'Стартап',
         link: '/startup',
+        tooltipText: 'Цей розділ доступний, коли обрано опцію "Стартап проект" у розділі Загальна інформація'
     },
 ];
 
@@ -52,13 +56,43 @@ const ProfileContent = (props) => {
         <div className={css['content__container']}>
             <div className={css['content']}>
                 <div className={css['info-links-profile']}>
-                    {INFOLINKS.map((element) => (
+                {INFOLINKS.map((element) => {
+                    const isLinkEnabled =
+                        (props.profile.is_registered && props.profile.is_startup) ||
+                        (props.profile.is_registered && element.title !== 'Стартап') ||
+                        (props.profile.is_startup && element.title !== 'Інформація про товари/ послуги' && element.title !== 'Додаткова інформація');
+                    return isLinkEnabled ? (
                         <NavLink
                             className={({ isActive }) => (`${css['infolink']} ${isActive && css['infolink__active']}`)}
                             to={`/profile${element.link}`}
                             key={element.title}
-                        >{element.title}</NavLink>
-                    ))}
+                        >
+                            {element.title}
+                        </NavLink>
+                    ) : (
+                        <Tooltip
+                            placement="bottomLeft"
+                            title={element.tooltipText}
+                            overlayInnerStyle={{
+                                display: 'flex',
+                                borderRadius: '2px',
+                                background: 'var(--main-grey-90, #25292C)',
+                                color: 'var(--main-white, #FFF)',
+                                fontFeatureSettings: 'calt',
+                                fontFamily: 'Inter',
+                                letterSpacing: '-0.14px',
+                            }}
+                        >
+                                <NavLink
+                                    className={`${css['infolink']} ${css['infolink__disabled']}`}
+                                    key={element.title}
+                                    disabled
+                                >
+                                    {element.title}
+                                </NavLink>
+                        </Tooltip>
+                    );
+                })}
                     <div className={css['divider']}></div>
                     <Link
                         to="/profile/delete"
