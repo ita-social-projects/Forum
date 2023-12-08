01c197fb86f9d5d40d8a5cd4d4ed204bee542d03
Sofia Bilous <EgikSonya@gmail.com>
2023-12-08 20:55:03 +0200

    change permission, add tests

---
+++ images/tests/test_banner.py
@@ -0,0 +1,110 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+import os
+from io import BytesIO
+from PIL import Image
+
+from authentication.factories import UserFactory
+from profiles.factories import (
+    ProfileStartupFactory,
+    ProfileCompanyFactory,
+)
+
+from utils.dump_response import dump  # noqa
+
+
+class TestBannerChange(APITestCase):
+    @staticmethod
+    def _generate_image(ext, size=(100, 100)):
+        """for mocking png and jpeg files"""
+        file = BytesIO()
+        image = Image.new("RGB", size=size)
+        formatext = ext.upper()
+        image.save(file, formatext)
+        file.name = f"test.{formatext}"
+        file.seek(0)
+        return file
+
+    def setUp(self) -> None:
+        self.right_image = self._generate_image("jpeg", (100, 100))
+        self.wrong_image = self._generate_image("png", (7000, 7000))
+
+        self.user = UserFactory(email="test1@test.com")
+        self.company_dnipro = ProfileStartupFactory.create(
+            person=self.user,
+            name="Dnipro",
+            banner_image=f"banners/{self.right_image.name}",
+        )
+
+        self.company_kiev = ProfileCompanyFactory(name="Kyivbud")
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.right_image.name):
+            os.remove(self.right_image.name)
+        if os.path.exists(self.wrong_image.name):
+            os.remove(self.wrong_image.name)
+
+    def test_get_empty_banner_unauthorized(self):
+        response = self.client.get(path=f"/api/banner/{self.company_kiev.id}/")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual({
+           "banner_image": None
+            }, response.json())
+
+    def test_get_banner_unauthorized(self):
+        response = self.client.get(path=f"/api/banner/{self.company_dnipro.id}/")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual({
+           "banner_image": f'http://testserver/media/banners/{self.right_image.name}'
+            }, response.json())
+
+    def test_get_banner_authorized(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.get(path=f"/api/banner/{self.company_dnipro.id}/")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual({
+            "banner_image": f'http://testserver/media/banners/{self.right_image.name}'
+        }, response.json())
+
+    def test_get_empty_banner_authorized(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.get(path=f"/api/banner/{self.company_kiev.id}/")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual({
+           "banner_image": None
+            }, response.json())
+
+    def test_put_banner_unauthorized(self):
+        response = self.client.put(path=f"/api/banner/{self.company_dnipro.id}/",
+                                   data={"banner_image": self.right_image})
+        self.assertEqual(401, response.status_code)
+        self.assertEqual({
+            "detail": "Authentication credentials were not provided."
+            }, response.json())
+
+    def test_put_banner_authorized_not_owner(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.put(path=f"/api/banner/{self.company_kiev.id}/",
+                                   data={"banner_image": self.right_image})
+        self.assertEqual(403, response.status_code)
+        self.assertEqual({
+            "detail": "You do not have permission to perform this action."
+            }, response.json())
+
+    def test_put_banner_authorized_owner_right_image(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.put(path=f"/api/banner/{self.company_dnipro.id}/",
+                                   data={"banner_image": self.right_image})
+        self.assertEqual(200, response.status_code)
+
+    def test_put_banner_authorized_owner_wrong_image(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.put(path=f"/api/banner/{self.company_dnipro.id}/",
+                                   data={"banner_image": self.wrong_image})
+        self.assertEqual(400, response.status_code)
+        self.assertEqual({
+            "banner_image": ["Image size exceeds the maximum allowed (50MB)."]
+        }, response.json())
+
+
--- images/views.py
+++ images/views.py
@@ -2,15 +2,13 @@ from rest_framework.generics import (
     RetrieveUpdateAPIView,
 )
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.permissions import (
-    IsAuthenticatedOrReadOnly,
-)
 
+from profiles.permissions import UserIsProfileOwnerOrReadOnly
 from profiles.models import Profile
 from .serializers import BannerSerializer
 
 class BannerChangeAPIView(RetrieveUpdateAPIView):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (UserIsProfileOwnerOrReadOnly,)
     serializer_class = BannerSerializer
     parser_classes = (MultiPartParser, FormParser)
     queryset = Profile.objects.all()
