b2345b0e5f1796853e23b2c4b85dc0718fec927e
Sofia Bilous <EgikSonya@gmail.com>
2023-12-11 15:05:59 +0200

    add functionality to logo

--- images/serializers.py
+++ images/serializers.py
@@ -6,3 +6,9 @@ class BannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ("banner_image",)
+
+
+class LogoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("logo_image",)
--- images/views.py
+++ images/views.py
@@ -5,11 +5,18 @@ from rest_framework.parsers import MultiPartParser, FormParser
 
 from profiles.permissions import UserIsProfileOwnerOrReadOnly
 from profiles.models import Profile
-from .serializers import BannerSerializer
+from .serializers import BannerSerializer, LogoSerializer
 
 
 class BannerRetrieveUpdate(RetrieveUpdateAPIView):
     permission_classes = (UserIsProfileOwnerOrReadOnly,)
     serializer_class = BannerSerializer
+    parser_classes = (MultiPartParser, FormParser)
+    queryset = Profile.objects.all()
+
+
+class LogoRetrieveUpdate(RetrieveUpdateAPIView):
+    permission_classes = (UserIsProfileOwnerOrReadOnly,)
+    serializer_class = LogoSerializer
     parser_classes = (MultiPartParser, FormParser)
     queryset = Profile.objects.all()
