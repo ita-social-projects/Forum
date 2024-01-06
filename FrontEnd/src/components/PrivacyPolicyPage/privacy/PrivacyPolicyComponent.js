66b5a1cddb23e68122eb7b18b32fd90ee8d23e0c
romanmyko <romanklapek@gmail.com>
2024-01-06 11:39:37 +0200

    PrivacyPolicy

--- FrontEnd/src/components/PrivacyPolicyPage/privacy/PrivacyPolicyComponent.js
+++ FrontEnd/src/components/PrivacyPolicyPage/privacy/PrivacyPolicyComponent.js
@@ -1,23 +1,31 @@
 import './Text.css';
-import {privacyPolicy} from './Text';
+import './PrivacyPolicyComponent.css';
+import { privacyPolicy } from './Text';
+import { useEffect } from 'react';
+
 export function PrivacyPolicy() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div>
-      <div className="divider"/>
+    <div className="block-privacypolicy block-size">
+      <div className="divider" />
       <div className="privacy-text">
-        <h1>Privacy Policy</h1>
+        <h2>Privacy Policy</h2>
         <p>Updated: {privacyPolicy.updated}</p>
         <p>{privacyPolicy.intro}</p>
         <ul>
           {privacyPolicy.sections.map((section, index) => (
-            <p key={index}>
-              <h2>{section.title}</h2>
+            <li key={index}>
+              <h3>{section.title}</h3>
               <p>{section.content}</p>
-            </p>
+            </li>
           ))}
         </ul>
       </div>
     </div>
   );
 }
+
 export default PrivacyPolicy;
