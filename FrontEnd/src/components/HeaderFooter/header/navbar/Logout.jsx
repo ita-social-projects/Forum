4248619124dcc97ae3a1600dd3833d8e49ef927a
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-01-16 16:44:55 +0100

    logout updated as a link

--- FrontEnd/src/components/HeaderFooter/header/navbar/Logout.jsx
+++ FrontEnd/src/components/HeaderFooter/header/navbar/Logout.jsx
@@ -1,23 +1,27 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useAuth } from '../../../../hooks';
 
 function Logout() {
   const auth = useAuth();
   const navigate = useNavigate();
 
-  const onClick = async () => {
-    await axios.post(
-      `${process.env.REACT_APP_BASE_API_URL}/api/auth/token/logout`
-    );
-    auth.logout();
-    navigate('/');
-  };
-  return (
-    <button onClick={onClick} type="submit">
-      Вихід
-    </button>
-  );
+  useEffect(() => {
+    const performLogout = async () => {
+      if (auth.isAuth) {
+        try {
+          await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/auth/token/logout`);
+          await auth.logout();
+        } catch (error) {
+          console.error('Error during logout', error);
+        }
+      }
+      navigate('/');
+    };
+
+    performLogout();
+  }, [auth, navigate]);
 }
 
 export default Logout;
