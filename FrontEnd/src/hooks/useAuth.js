5d0076b341fafe4c1f59ccd1dd6861f9e5bba49b
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-01-24 12:17:09 +0100

    authprovider in useauth hook

--- FrontEnd/src/hooks/useAuth.js
+++ FrontEnd/src/hooks/useAuth.js
@@ -1,6 +1,78 @@
 import { useContext } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { AuthContext } from '../context';
 
-export default function useAuth() {
+export function AuthProvider ({ children }) {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const validateToken = async (authToken) => {
+    try {
+      await axios.get(
+        `${process.env.REACT_APP_BASE_API_URL}/api/auth/users/me/`,
+        {
+          headers: {
+            Authorization: `Token ${authToken}`,
+          },
+        }
+      );
+      return true;
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        return false;
+      }
+    }
+  };
+
+  const login = (authToken) => {
+    localStorage.setItem('Token', authToken);
+    axios.defaults.headers.common['Authorization'] = `Token ${authToken}`;
+    setIsAuth(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('Token');
+    delete axios.defaults.headers.common['Authorization'];
+    setIsAuth(false);
+    navigate('/', { replace: true });
+  };
+
+  useEffect(() => {
+    const authToken = localStorage.getItem('Token');
+    axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          logout();
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    if (authToken && validateToken(authToken)) {
+      login(authToken);
+    } else {
+      logout();
+    }
+
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('storage', (e) => {
+      if (e.key === 'Token') {
+        setIsAuth(e.newValue);
+      }
+    });
+  });
+
+  const value =  { login, logout, isAuth, isLoading };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
   return useContext(AuthContext);
 }
