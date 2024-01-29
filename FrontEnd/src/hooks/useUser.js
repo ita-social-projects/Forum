0cb67bf3b6c3ce69cccf60770c8b6be7c6981537
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-01-29 19:13:23 +0100

    useuser formatted

--- FrontEnd/src/hooks/useUser.js
+++ FrontEnd/src/hooks/useUser.js
@@ -8,33 +8,38 @@ export default function useUser() {
     const { logout } = useAuth();
 
     const { data, error, mutate } = useSWR(
-        authToken ? `${process.env.REACT_APP_BASE_API_URL}/api/auth/users/me/` : null,
-        url =>
-            fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Token ${authToken}`,
-                },
-            }).then((res) => {
-                if (!res.ok && res.status === 401) {
-                    logout();
-                  const error = new Error('Unauthorized user.');
-                        error.info = res.json();
-                        error.status = res.status;
-                        throw error;
-                }
-                if (!res.ok) {
-                    const error = new Error('An error occurred while fetching the data.');
-                          error.info = res.json();
-                          error.status = res.status;
-                          throw error;
-                  }
-                return res.json();
-              })
-            .catch(error => {
-                console.error(error);
-            }),
-        { revalidateOnFocus: false }
+      authToken
+        ? `${process.env.REACT_APP_BASE_API_URL}/api/auth/users/me/`
+        : null,
+      (url) =>
+        fetch(url, {
+          method: 'GET',
+          headers: {
+            Authorization: `Token ${authToken}`,
+          },
+        })
+          .then((res) => {
+            if (!res.ok && res.status === 401) {
+              logout();
+              const error = new Error('Unauthorized user.');
+              error.info = res.json();
+              error.status = res.status;
+              throw error;
+            }
+            if (!res.ok) {
+              const error = new Error(
+                'An error occurred while fetching the data.'
+              );
+              error.info = res.json();
+              error.status = res.status;
+              throw error;
+            }
+            return res.json();
+          })
+          .catch((error) => {
+            console.error(error);
+          }),
+      { revalidateOnFocus: false }
     );
 
     useEffect(() => {
