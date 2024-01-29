2d54a92d49cf9417e976ca859db854a2b38bcd0d
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-01-29 21:04:23 +0100

    authprovider moved in router

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import './App.css';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AdminPage from './components/adminPage/AdminPage';
 import BasicPage from './components/basicPage/BasicPage';
 import { AuthProvider } from './hooks';
 
 function App() {
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/*" element={<BasicPage />} />
-        <Route path="/customadmin/*" element={<AdminPage />} />
-      </>
-    )
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/*',
+      element: (
+        <AuthProvider>
+          <BasicPage />
+        </AuthProvider>
+      )
+    },
+    {
+      path: '/customadmin/*',
+      element: (
+          <AdminPage />
+      )
+    },
+  ]);
 
   return (
-    <AuthProvider>
-        <div className="App">
-          <RouterProvider router={router} />
-        </div>
-    </AuthProvider>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
