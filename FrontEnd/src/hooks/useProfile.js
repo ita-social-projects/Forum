2c002af35ec0d967215a6322dcf4de543cd975bb
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-01-29 19:12:29 +0100

    logout to useprofile hook

--- FrontEnd/src/hooks/useProfile.js
+++ FrontEnd/src/hooks/useProfile.js
@@ -1,23 +1,47 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import useUser from './useUser';
+import { useAuth } from './useAuth';
 
 export default function useProfile() {
     const token = localStorage.getItem('Token');
     const { user } = useUser();
-
+    const { logout } = useAuth();
     const [profile, setProfile] = useState(null);
 
     const { data, error, mutate } = useSWR(
-        (token && user) ? `${process.env.REACT_APP_BASE_API_URL}/api/profiles/${user.profile_id}` : null,
-        url =>
-            fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Token ${token}`,
-                },
-            }).then(res => res.json()),
-        { revalidateOnFocus: false }
+      token && user
+        ? `${process.env.REACT_APP_BASE_API_URL}/api/profiles/${user.profile_id}`
+        : null,
+      (url) =>
+        fetch(url, {
+          method: 'GET',
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        })
+          .then((res) => {
+            if (!res.ok && res.status === 401) {
+              logout();
+              const error = new Error('Unauthorized user.');
+              error.info = res.json();
+              error.status = res.status;
+              throw error;
+            }
+            if (!res.ok) {
+              const error = new Error(
+                'An error occurred while fetching the data.'
+              );
+              error.info = res.json();
+              error.status = res.status;
+              throw error;
+            }
+            return res.json();
+          })
+          .catch((error) => {
+            console.error(error);
+          }),
+      { revalidateOnFocus: false }
     );
 
     useEffect(() => {
