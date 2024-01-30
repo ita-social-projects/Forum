849501e8fd675fe12cb7292bc2f0b99d5a504bd2
romanmyko <romanklapek@gmail.com>
2024-01-30 22:14:59 +0200

    start rebuild admin panel

--- FrontEnd/src/components/adminPage/AdminPage.jsx
+++ FrontEnd/src/components/adminPage/AdminPage.jsx
@@ -1,5 +1,5 @@
-import Footer from '../HeaderFooter/footer/Footer';
 import Header from './header/Header';
+// import Header_main from '../HeaderFooter/header/Header';
 import Menu from './menu/Menu';
 import UserDetail from './detail-view/UserDetail';
 import UserTable from './table/UserTable';
@@ -8,24 +8,34 @@ import CompanyDetail from './detail-view/CompanyDetail';
 import css from './AdminPage.module.css';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './mainPage/MainPage';
+import { useAuth } from '../../hooks';
 
 function AdminPage() {
+    const auth = useAuth();
+    const authRoutes = auth.isAuth ? (
+        <>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/users" element={<UserTable />} />
+            <Route path="/users/:id" element={<UserDetail />} />
+            <Route path="/companies" element={<CompanyTable />} />
+            <Route path="/company/:id" element={<CompanyDetail />} />
+        </>
+    ) : (
+        <Route path="/login" />
+    );
+
     return (
         <div>
-            <Header />
-                <div className={css['content']}>
-                        <Menu />
-                        <Routes>
-                            <Route path="/" element={<MainPage />} />
-                            <Route path="/users" element={<UserTable />} />
-                            <Route path="/users/:id" element={<UserDetail />} />
-                            <Route path="/companies" element={<CompanyTable />} />
-                            <Route path="/company/:id" element={<CompanyDetail />} />
-                        </Routes>
-                </div>
-            <Footer page="admin"></Footer>
+            {/* <Header_main isAuthorized={auth.isAuth} /> */}
+            <Header isAuthorized={auth.isAuth} />
+            <div className={css['content']}>
+                <Menu isAuthorized={auth.isAuth} />
+                <Routes >
+                    {authRoutes}
+                </Routes>
+            </div>
+            {/* <Footer page="admin"></Footer> */}
         </div>
-
     );
 }
 
--- FrontEnd/src/components/adminPage/menu/Menu.jsx
+++ FrontEnd/src/components/adminPage/menu/Menu.jsx
@@ -1,5 +1,8 @@
 import css from './Menu.module.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../hooks';
 
 const MENU = [
     {
@@ -15,14 +18,23 @@ const MENU = [
 ];
 
 function Menu() {
+    const auth = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/auth/token/logout`);
+        auth.logout();
+        navigate('/');
+    };
+
     return (
-      <div className={css['menu-section']}>
-          {MENU.map((element) =>(
-              <Link className={css['menu-section-element']} key={element.id} to={element.link}>{element.title}</Link>
-          ))}
-          <div className={css['menu-section-divider']}></div>
-          <Link className={css['menu-section-logout']} to={'#'}>Вихід</Link>
-      </div>
+        <div className={css['menu-section']}>
+            {MENU.map((element) => (
+                <Link className={css['menu-section-element']} key={element.id} to={element.link}>{element.title}</Link>
+            ))}
+            <div className={css['menu-section-divider']}></div>
+            <button className={css['menu-section-logout']} onClick={handleLogout}>Вихід</button>
+        </div>
     );
 }
 
