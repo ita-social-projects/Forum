2d94657e40ff657536425d2e72b06d876f9d0695
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-01-31 12:00:22 +0100

    swr request, update hook

--- FrontEnd/src/hooks/useAuth.js
+++ FrontEnd/src/hooks/useAuth.js
@@ -1,57 +1,72 @@
 import { useContext } from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useSWR from 'swr';
 import axios from 'axios';
 import { AuthContext } from '../context';
 
 export function AuthProvider ({ children }) {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(!!JSON.parse(localStorage.getItem('isAuth')));
+  const [user, setUser] = useState(null);
   const [isLoading, setLoading] = useState(true);
+  const authToken = localStorage.getItem('Token');
   const navigate = useNavigate();
-  const validateToken = async (authToken) => {
-    try {
-      await axios.get(
-        `${process.env.REACT_APP_BASE_API_URL}/api/auth/users/me/`,
-        {
-          headers: {
-            Authorization: `Token ${authToken}`,
-          },
-        }
-      );
-      return true;
-    } catch (error) {
-      if (error.response && error.response.status === 401) {
-        return false;
-      }
-    }
-  };
+
+  const { data, error, mutate } = useSWR(
+    authToken
+      ? [`${process.env.REACT_APP_BASE_API_URL}/api/auth/users/me/`, authToken]
+      : null,
+    ([url, authToken]) =>
+      fetch(url, {
+        method: 'GET',
+        headers: {
+          Authorization: `Token ${authToken}`,
+        },
+      })
+        .then((res) => {
+          if (!res.ok) {
+            const error = new Error(
+              'An error occurred while fetching the data.'
+            );
+            error.info = res.json();
+            error.status = res.status;
+            throw error;
+          }
+          return res.json();
+        })
+        .catch((error) => {
+          console.error(error);
+        }),
+    { revalidateOnFocus: false }
+  );
 
   const login = (authToken) => {
     localStorage.setItem('Token', authToken);
+    localStorage.setItem('isAuth', true);
     axios.defaults.headers.common['Authorization'] = `Token ${authToken}`;
     setIsAuth(true);
   };
 
   const logout = () => {
     localStorage.removeItem('Token');
+    localStorage.removeItem('isAuth');
     delete axios.defaults.headers.common['Authorization'];
     setIsAuth(false);
     navigate('/', { replace: true });
   };
 
   useEffect(() => {
-    const authToken = localStorage.getItem('Token');
     axios.interceptors.response.use(
-      (response) => response,
-      (error) => {
-        if (error.response.status === 401) {
+      response => response,
+      error => {
+        if (error.response && error.response.status === 401) {
           logout();
         }
         return Promise.reject(error);
       }
     );
 
-    if (authToken && validateToken(authToken)) {
+    if (authToken) {
       login(authToken);
     } else {
       logout();
@@ -61,6 +76,15 @@ export function AuthProvider ({ children }) {
   }, []);
 
   useEffect(() => {
+    if (data) {
+        setUser(data);
+    }
+    if (error) {
+        setUser(null);
+    }
+  }, [data, error]);
+
+  useEffect(() => {
     window.addEventListener('storage', (e) => {
       if (e.key === 'Token') {
         setIsAuth(e.newValue);
@@ -68,7 +92,7 @@ export function AuthProvider ({ children }) {
     });
   });
 
-  const value =  { login, logout, isAuth, isLoading };
+  const value =  { login, logout, isAuth, isLoading, user, error, mutate };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
