a4e051c246acc68851328aa41a215e1d46cb8ec1
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-01-31 12:36:15 +0100

    useuser hook removed from index

--- FrontEnd/src/hooks/index.js
+++ FrontEnd/src/hooks/index.js
@@ -1,5 +1,4 @@
 import { useAuth, AuthProvider } from './useAuth';
-import useUser from './useUser';
 import useProfile from './useProfile';
 
-export { useAuth, AuthProvider, useUser, useProfile };
+export { useAuth, AuthProvider, useProfile };
