e7fe64afcff3b225fcb281d55da97035b23e12e8
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-02-06 22:14:51 +0100

    edrpou validator updated

--- validation/validate_edrpou.py
+++ validation/validate_edrpou.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 
 def validate_edrpou(edrpou_value: str):
-    if len(edrpou_value) != 8:
+    if len(edrpou_value) != 8 or not edrpou_value.isdecimal():
         raise ValidationError("EDRPOU must be exactly 8 digits long.")
