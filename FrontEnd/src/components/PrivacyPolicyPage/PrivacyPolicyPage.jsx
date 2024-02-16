a22507c6416e277c3440dd87ea6a952b828097d9
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-02-16 18:17:06 +0200

    make Privacy Policy page

--- FrontEnd/src/components/PrivacyPolicyPage/PrivacyPolicyPage.jsx
+++ FrontEnd/src/components/PrivacyPolicyPage/PrivacyPolicyPage.jsx
@@ -25,14 +25,16 @@ const PrivacyPolicy = () => {
         <h2 className={styles['privacy_policy__title']}>{privacyPolicyText.title} </h2>
         {rulesText.content.map((item) => {
             if (item.type === 'paragraph') {
-            return <p  className={styles['text_contaent']} key={item.id || uuidv4()}>{item.text}</p>;
+            return <p className={styles['text_content']} key={item.id || uuidv4()}>{item.text}</p>;
             } else if (item.type === 'heading') {
-            const HeadingTag = `h${item.level}`;
-            return <HeadingTag key={item.id || uuidv4()}>{item.text}</HeadingTag>;
+            const HeadingTag =`h${item.level}`;
+            return <HeadingTag className={styles['text_content__heading']} key={item.id || uuidv4()}>{item.text}</HeadingTag>;
+            }else if (item.type === 'paragraph__margin_bottom') {
+            return <p  className={styles['text_content__margin_bottom']} key={item.id || uuidv4()}>{item.text}</p>;
             } else if (item.type === 'list-item') {
             return (
                 <ul key={item.id || uuidv4()} className={styles['custom-list']}>
-                <li>{item.text}</li>
+                <li className={styles['custom-list__item']}>{item.text}</li>
                 </ul>
             );
             }
