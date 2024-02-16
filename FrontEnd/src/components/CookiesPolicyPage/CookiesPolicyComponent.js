b71b87dc8964d9ac1c9ba5e87811e3d8c11bd151
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-02-16 23:46:26 +0200

    created a Cookie Policy page

--- FrontEnd/src/components/CookiesPolicyPage/CookiesPolicyComponent.js
+++ FrontEnd/src/components/CookiesPolicyPage/CookiesPolicyComponent.js
@@ -1,35 +1,48 @@
-import css from './CookiesPolicyComponent.module.css';
+import styles from './CookiesPolicyComponent.module.css';
+import CookiesPolicyText from './text';
+import Text from './text';
+import { v4 as uuidv4 } from 'uuid';
+import reactLogo from './img/dots.png';
 import { useEffect } from 'react';
-import { cookiesPolicy } from './Text';
 
-function CookiesPolicyComponent() {
+const CookiesPolicy = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-
-    return (
-        <div className="block-cookies_policy block-size">
-            <div className={css['root-container']}>
-                <div className={css['cookies-main']}>
-                    <div className={css['title-container']}>
-                        <h2 className={css['title']}>Cookies Policy</h2>
-                        <p className={css['description']}>{cookiesPolicy.intro}</p>
-                    </div>
-                </div>
+  return (
+    <div className={styles['cookies_policy']}>
+        <div className={styles['cookies_policy__link_container']}>
+            <div className={styles['link']}>
+                <a className={styles['link_text']} href="/privacy-policy/">Privacy Policy<br /></a>
+                <a className={styles['link_text']} href="/terms-and-conditions/">Terms & Conditions<br /></a>
+                <a className={styles['link_text__active']} href="/cookies-policy/">Cookie Policy</a>
+                <a className={styles['link_text']} href="/Contact/">Contact</a>
             </div>
-            <div className={css['cookies-section']}>
-                <ul>
-                    <h4>Updated: {cookiesPolicy.updated}</h4>
-                    {cookiesPolicy.sections.map((section) => (
-                        <li key={section.id}>
-                            <h3>{section.title}</h3>
-                            <p>{section.content}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <img className={styles['cookies_policy__img1']} src={reactLogo} alt="Image dots" />
+            <img className={styles['cookies_policy__img2']} src={reactLogo} alt="Image dots" />
         </div>
-    );
-}
+        <div className={styles['cookies_policy__text_container']}>
+        <h2 className={styles['cookies_policy__title']}>{CookiesPolicyText.title} </h2>
+        {Text.content.map((item) => {
+            if (item.type === 'paragraph') {
+            return <p className={styles['text_content']} key={item.id || uuidv4()}>{item.text}</p>;
+            } else if (item.type === 'heading') {
+            const HeadingTag =`h${item.level}`;
+            return <HeadingTag className={styles['text_content__heading']} key={item.id || uuidv4()}>{item.text}</HeadingTag>;
+            }else if (item.type === 'paragraph__margin_bottom') {
+            return <p  className={styles['text_content__margin_bottom']} key={item.id || uuidv4()}>{item.text}</p>;
+            } else if (item.type === 'list-item') {
+            return (
+                <ul key={item.id || uuidv4()} className={styles['custom-list']}>
+                <li className={styles['custom-list__item']}>{item.text}</li>
+                </ul>
+            );
+            }
+            return null;
+        })}
+        </div>
+    </div>
+  );
+};
 
-export default CookiesPolicyComponent;
+export default CookiesPolicy;
---
+++ FrontEnd/src/components/CookiesPolicyPage/text.js
@@ -0,0 +1,137 @@
+const privacyPolicyText = {
+    id: 1,
+    title: 'Cookie Policy',
+    level: 2,
+    content: [
+        {
+            id: 2,
+            type: 'paragraph__margin_bottom',
+            text: 'Effective Date:November 2, 2022'
+        },
+        {
+            id: 3,
+            type: 'paragraph__margin_bottom',
+            text: 'This Cookie and Other Technologies policy ("Cookie Policy") explains our use of cookies and other technologies to understand, improve, and protect Kickstarter and the Kickstarter Community. Kickstarter allows certain third parties to use cookies and other automated data collection tools to understand your use of our services that help people bring creative projects to life (our "Services").'
+        },
+        {
+            id: 4,
+            type: 'paragraph',
+            text: 'This Cookie Policy should be read along with our Terms of Use and our Privacy Policy. Kickstarter’s Terms of Use will always control in case of any conflict. Kickstarter may amend this Cookie Policy at any time and all changes will be made on this page. The changes will take effect once they have been posted on this website'
+        },
+        {
+            id: 5,
+            type: 'heading',
+            level: 4,
+            text: 'What they are:'
+        },
+        {
+            id: 6,
+            type: 'paragraph',
+            text: 'Cookies are small data files that websites place on your device when you visit a website. These are then processed and store information about how you use Kickstarter. Pixel tags (which are also called clear GIFs, web beacons, or pixels) are snippets of code or tiny images embedded on websites and in email that help us learn how you interact with our site and emails.'
+        },
+        {
+            id: 7,
+            type: 'heading',
+            level: 4,
+            text: 'Why we use them'
+        },
+        {
+            id: 8,
+            type: 'paragraph',
+            text: 'Kickstarter and select third parties, such as our creators, use cookies and these other technologies to help recognize you as a repeat visitor and to improve the quality of our Services. Overall, these tools are used to analyze trends in site traffic, market and advertise to you, obtain demographic information about Kickstarter’s Community as a whole, and to optimize third parties to assist us in responding to specific user interests.'
+        },
+        {
+            id: 9,
+            type: 'heading',
+            level: 4,
+            text: 'How they work'
+        },
+        {
+            id: 10,
+            type: 'paragraph__margin_bottom',
+            text: 'When you visit Kickstarter, our web server sends a cookie to your device, which allows us to recognize your device (but not the specific person using it). By associating the identification numbers in the cookies with other account information when, for example, you log in to our Services, we know that the cookie information relates to your user account. Similarly, pixel tags on Kickstarter and in email communications help us identify how your device is being used to interact with Kickstarter content. Some of the tags and cookies used by our Services are served by us, and some are served by trusted third parties who are delivering services on our behalf.'
+        },
+        {
+            id: 11,
+            type: 'paragraph',
+            text: 'Some examples of the types of cookies and other technologies Kickstarter uses:'
+        },
+        {
+            id: 12,
+            type: 'heading',
+            level: 4,
+            text: 'Essential'
+        },
+        {
+            id: 13,
+            type: 'paragraph__margin_bottom',
+            text: 'Essential cookies are strictly necessary for you to browse the website and use its features, such as accessing secure areas of the site. These cookies also help keep Kickstarter secure by facilitating security features and allowing us to detect activity that might violate our rules and Terms of Use.'
+        },
+        {
+            id: 14,
+            type: 'paragraph',
+            text: 'Essential cookies help protect your account from being accessed by anyone other than you, alert you and us when your account is accessed, and provide capabilities that allow us to disable any active sessions you have (for example, when you log out or change your password).'
+        },
+        {
+            id: 15,
+            type: 'heading',
+            level: 4,
+            text: 'Functional'
+        },
+        {
+            id: 16,
+            type: 'paragraph',
+            text: 'Functional or preferences cookies provide functionality and help deliver our products and services — for instance, by storing your language preferences or by pre-filling the username field when you log in.'
+        },
+        {
+            id: 17,
+            type: 'heading',
+            level: 4,
+            text: 'Analytics'
+        },
+        {
+            id: 18,
+            type: 'paragraph',
+            text: 'Analytics and statistics cookies are sometimes known as “performance cookies,” and relate to information collected about how you use a website, like which pages you visit. This data is used to improve website functionality.'
+        },
+        {
+            id: 19,
+            type: 'heading',
+            level: 4,
+            text: 'Advertising'
+        },
+        {
+            id: 20,
+            type: 'paragraph',
+            text: 'These cookies are used by Kickstarter and creators to collect information about how you use our site. Creators use this information to show you projects they believe will be interesting to you within our platform and elsewhere, and to measure how the ads perform.'
+        },
+        {
+            id: 21,
+            type: 'heading',
+            level: 4,
+            text: 'Your Choices'
+        },
+        {
+            id: 22,
+            type: 'paragraph__margin_bottom',
+            text: 'Kickstarter provides several ways for you to manage your cookies and other tracking technologies. How you do so depends on the type of cookie. If you do not accept our cookies, you may experience some inconvenience in your use of Kickstarter. For example, we may not be able to recognize your computer or mobile device and you may need to log in every time you visit our Sites.'
+        },
+        {
+            id: 23,
+            type: 'paragraph__margin_bottom',
+            text: 'Kickstarter offers consent management tooling to enable users to manage cookies and other tracking technologies. For Preferences, Analytics & Statistics, and Advertising cookies, Kickstarter provides a consent management window to enable you to manage cookies and other technology tracking. You will not be able to opt out of Essential cookies, as they are necessary for the operation of the Kickstarter website. To update your previously set cookie preferences, please visit your consent tooling, here.'
+        },
+        {
+            id: 24,
+            type: 'paragraph__margin_bottom',
+            text: 'Using your browser setting, you can accept or decline cookies. Some browsers also allow you to enable the browser to prompt you before accepting cookies from websites you visit. To do this, follow the instructions in your browser settings. Many browsers accept cookies by default until you change your settings.'
+        },
+        {
+            id: 25,
+            type: 'paragraph__margin_bottom',
+            text: 'In addition, many browsers and devices provide specific consent management options. For example, some browsers have "do not track" features that allow you to tell a website not to track you. These features are not all uniform. If you block cookies, certain features on our sites may not work. If you block or reject cookies, not all of the tracking described here will stop.'
+        },
+    ],
+};
+
+export default privacyPolicyText;
