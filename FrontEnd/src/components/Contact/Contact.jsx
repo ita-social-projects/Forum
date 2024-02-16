1a4230c95f238161b4c3cb37314c43157cf29376
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-02-16 13:05:53 +0200

    created the contacts app

--- FrontEnd/src/components/Contact/Contact.jsx
+++ FrontEnd/src/components/Contact/Contact.jsx
@@ -1,17 +1,46 @@
-
+import styles from './Contact.module.css';
+import privacyPolicyText from './text';
+import rulesText from './text';
+import { v4 as uuidv4 } from 'uuid';
+import reactLogo from './img/dots.png';
 import { useEffect } from 'react';
 
-
-function Contact() {
+const Contact = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-
-    return (
-        <div className="block-cookies_policy block-size">
-            <p>Hello world</p>
+  return (
+    <div className={styles['privacy_policy']}>
+        <div className={styles['privacy_policy__link_container']}>
+            <div className={styles['link']}>
+                <a className={styles['link_text']} href="/privacy-policy/">Privacy Policy<br /></a>
+                <a className={styles['link_text']} href="/terms-and-conditions/">Terms & Conditions<br /></a>
+                <a className={styles['link_text']} href="/cookies-policy/">Cookie Policy</a>
+                <a className={styles['link_text']} href="/Contact/">Contact</a>
+            </div>
+            <img className={styles['privacy_policy__img1']} src={reactLogo} alt="Image dots" />
+            <img className={styles['privacy_policy__img2']} src={reactLogo} alt="Image dots" />
         </div>
-    );
-}
+        <div className={styles['privacy_policy__text_container']}>
+        <h2 className={styles['privacy_policy__title']}>{privacyPolicyText.title} </h2>
+        {rulesText.content.map((item) => {
+            if (item.type === 'paragraph') {
+            return <p  className={styles['text_contaent']} key={item.id || uuidv4()}>{item.text}</p>;
+            } else if (item.type === 'heading') {
+            const HeadingTag = `h${item.level}`;
+            return <HeadingTag key={item.id || uuidv4()}>{item.text}</HeadingTag>;
+            } else if (item.type === 'list-item') {
+            return (
+                <ul key={item.id || uuidv4()} className={styles['custom-list']}>
+                <li>{item.text}</li>
+                </ul>
+            );
+            }
+            return null;
+        })}
+        </div>
+    </div>
+  );
+};
 
 export default Contact;
