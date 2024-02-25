67d105ceaead7add332db55472d99b6ae64e8913
romanmyko <romanklapek@gmail.com>
2024-02-25 21:28:24 +0200

    logout v3

--- FrontEnd/src/components/adminPage/menu/Menu.jsx
+++ FrontEnd/src/components/adminPage/menu/Menu.jsx
@@ -1,6 +1,5 @@
 import css from './Menu.module.css';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../hooks';
 import axios from 'axios';
 
@@ -20,13 +19,11 @@ const MENU = [
 
 function Menu() {
     const auth = useAuth();
-    const navigate = useNavigate();
     const handleLogout = async () => {
         if (auth.isAuth) {
             await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/auth/token/logout`);
         }
         auth.logout();
-        navigate('/');
     };
 
     return (
