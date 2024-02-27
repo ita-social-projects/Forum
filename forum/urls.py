0240294b02b3b68a6aae3dffbbf16525f7f61c5a
romanmyko <romanklapek@gmail.com>
2024-02-28 00:35:58 +0200

    fornm/urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
