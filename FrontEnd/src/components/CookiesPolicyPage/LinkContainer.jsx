40291e07eb2e5c82d334e753a3d0cab16047d546
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-03-01 15:51:58 +0200

    create getClassName function

--- FrontEnd/src/components/CookiesPolicyPage/LinkContainer.jsx
+++ FrontEnd/src/components/CookiesPolicyPage/LinkContainer.jsx
@@ -1,42 +1,26 @@
 import { NavLink } from 'react-router-dom';
 import styles from './cookiesPolicyComponent.module.css';
 
-const LinkContainer = () => (
-  <div className={styles['link']}>
-          <NavLink
-            to="/privacy-policy/"
-            className={({ isActive }) =>
-              isActive ? styles['link_text__active'] : styles['link_text']
-            }
-          >
-            Privacy Policy<br />
-          </NavLink>
-          <NavLink
-            to="/terms-and-conditions/"
-            className={({ isActive }) =>
-              isActive ? styles['link_text__active'] : styles['link_text']
-            }
-          >
-            Terms & Conditions<br />
-          </NavLink>
-          <NavLink
-            to="/cookies-policy/"
-            className={({ isActive }) =>
-              isActive ? styles['link_text__active'] : styles['link_text']
-            }
-          >
-            Cookie Policy
-          </NavLink>
-          <NavLink
-            to="/contact/"
-            className={({ isActive }) =>
-              isActive ? styles['link_text__active'] : styles['link_text']
-            }
-          >
-            Contact
-          </NavLink>
-        </div>
-);
+const LinkContainer = () => {
+  const getClassName = ({ isActive }) =>
+    isActive ? styles['link_text__active'] : styles['link_text'];
+
+  return (
+    <div className={styles['link']}>
+      <NavLink to="/privacy-policy/" className={getClassName}>
+        Privacy Policy
+      </NavLink>
+      <NavLink to="/terms-and-conditions/" className={getClassName}>
+        Terms & Conditions
+      </NavLink>
+      <NavLink to="/cookies-policy/" className={getClassName}>
+        Cookie Policy
+      </NavLink>
+      <NavLink to="/contact/" className={getClassName}>
+        Contact
+      </NavLink>
+    </div>
+  );
+};
 
 export default LinkContainer;
-
