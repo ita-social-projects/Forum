0f5733d911ed6e4818b5b11587ed05d60060e41d
romanmyko <romanklapek@gmail.com>
2024-03-06 10:20:49 +0200

    profiles table SWR v0

--- FrontEnd/src/components/adminPage/table/ProfilesTable.jsx
+++ FrontEnd/src/components/adminPage/table/ProfilesTable.jsx
@@ -1,55 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import css from './ProfilesTable.module.css';
 import { useNavigate } from 'react-router-dom';
 import PaginationButtons from './PaginationButtons';
 import axios from 'axios';
+import useSWR from 'swr';
 
 const COLUMN_NAMES = [
     'ID', 'Person', 'Position', 'Company', 'Region', 'Phone', 'EDRPOU', 'Adress', 'IsDeleted', 'IsApproved'
 ];
 
 function ProfilesTable() {
-    const [profiles, setProfiles] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
     const navigate = useNavigate();
     const routeChange = (id) => {
         let path = `../../customadmin/profile/${id}`;
         navigate(path);
     };
+    const [currentPage, setCurrentPage] = useState(1);
     const [pageSize, setPageSize] = useState(10);
     const handlePageSizeChange = (size) => {
         setPageSize(size);
         setCurrentPage(1);
     };
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(
-                    `${process.env.REACT_APP_BASE_API_URL}/api/admin/profiles/?page=${currentPage}&page_size=${pageSize}`
-                );
-                if (response.status !== 200) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                setProfiles(response.data.results);
-                setTotalPages(response.data.total_pages);
-                setLoading(false);
-            } catch (error) {
-                setError(error.message);
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, [currentPage, pageSize]);
-
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
+
+    const url = `${process.env.REACT_APP_BASE_API_URL}/api/admin/profiles/?page=${currentPage}&page_size=${pageSize}`;
+
+    async function fetcher(url) {
+        const response = await axios.get(url);
+        return response.data;
+    }
+    const { data, error, isValidating: loading } = useSWR(url, fetcher);
 
     if (loading) {
         return <p>Loading...</p>;
@@ -59,9 +41,13 @@ function ProfilesTable() {
         return <p>Error: {error}</p>;
     }
 
+    const profiles = data.results;
+
     return (
         <div className={css['table-profiles']}>
-            <PaginationButtons totalPages={totalPages} currentPage={currentPage} onPageChange={handlePageChange}
+            <PaginationButtons
+                totalPages={data ? data.total_pages : 1}
+                currentPage={currentPage} onPageChange={handlePageChange}
                 pageSize={pageSize} onPageSizeChange={handlePageSizeChange}
             />
             <table className={css['table-section']}>
