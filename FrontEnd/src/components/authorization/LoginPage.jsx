6f7648ed09b51733d32954e4ea752686a2d259b8
Sofia Bilous <EgikSonya@gmail.com>
2024-03-18 20:51:52 +0200

    timer, ratelimit message

--- FrontEnd/src/components/authorization/LoginPage.jsx
+++ FrontEnd/src/components/authorization/LoginPage.jsx
@@ -1,13 +1,14 @@
 import { useForm } from 'react-hook-form';
 import { useState, useEffect } from 'react';
-import { useNavigate, Link  } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import { useStopwatch } from 'react-timer-hook';
+
 import validator from 'validator';
 import EyeVisible from './EyeVisible';
 import EyeInvisible from './EyeInvisible';
 import classes from './LoginPage.module.css';
 import { useAuth } from '../../hooks/';
-
 
 const LoginContent = (props) => {
   const auth = useAuth();
@@ -18,10 +19,20 @@ const LoginContent = (props) => {
     setShowPassword(!showPassword);
   };
 
+  // function RateLimitStopwatch() {
+  const { minutes, isRunning, start, reset } = useStopwatch({
+    autoStart: false,
+  });
+  // return minutes, isRunning, start, pause
+  // }
+
+  // const { minutes, isRunning, start, pause } = RateLimitStopwatch()
+
   const errorMessageTemplates = {
     required: 'Обов’язкове поле',
     email: 'Формат електронної пошти некоректний',
     unspecifiedError: 'Електронна пошта чи пароль вказані некоректно',
+    rateError: 'Небезпечні дії на сторінці. Сторінка заблокована на 10 хвилин',
   };
 
   const {
@@ -32,7 +43,7 @@ const LoginContent = (props) => {
     clearErrors,
     formState: { errors, isValid },
   } = useForm({
-    mode: 'all'
+    mode: 'all',
   });
 
   const { setErrorMessage } = props;
@@ -44,7 +55,9 @@ const LoginContent = (props) => {
       if (errors.email.message === errors.password.message) {
         errorMessage = errors.email.message;
       } else {
-        errorMessage = `${errors.email?.message || ''}\n${errors.password?.message || ''}`;
+        errorMessage = `${errors.email?.message || ''}\n${
+          errors.password?.message || ''
+        }`;
       }
     } else if (errors.email?.message) {
       errorMessage = errors.email.message;
@@ -52,34 +65,59 @@ const LoginContent = (props) => {
       errorMessage = errors.password.message;
     } else if (errors.unspecifiedError?.message) {
       errorMessage = errors.unspecifiedError.message;
+    } else if (errors.rateError?.message) {
+      errorMessage = errors.rateError.message;
     }
 
     setErrorMessage(errorMessage);
-  }, [errors.email?.message, errors.password?.message, errors.unspecifiedError?.message, setErrorMessage]);
+  }, [
+    errors.email?.message,
+    errors.password?.message,
+    errors.unspecifiedError?.message,
+    errors.rateError?.message,
+    setErrorMessage,
+  ]);
 
   useEffect(() => {
     clearErrors('unspecifiedError');
+    clearErrors('rateError');
   }, [getValues('email'), getValues('password'), clearErrors]);
+
+  const disabled = !isValid || (isRunning && minutes < 2);
 
   const onSubmit = async (value) => {
     try {
-      const response = await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/auth/token/login/`, {
-        email: value.email,
-        password: value.password,
-      });
+      const response = await axios.post(
+        `${process.env.REACT_APP_BASE_API_URL}/api/auth/token/login/`,
+        {
+          email: value.email,
+          password: value.password,
+        }
+      );
       const authToken = response.data.auth_token;
       auth.login(authToken);
       navigate('/profile/user-info');
-    }
-    catch (error) {
+    } catch (error) {
       console.error('ERROR', error);
       if (error.response.status === 400) {
-      setError('unspecifiedError', {
-        type: 'manual',
-        message: errorMessageTemplates.unspecifiedError
-      });
-    }}
+        const resp = error.response.data.non_field_errors[0];
+        if (resp == 'Unable to log in with provided credentials.') {
+          setError('unspecifiedError', {
+            type: 'manual',
+            message: errorMessageTemplates.unspecifiedError,
+          });
+        } else if (resp == 'User account is disabled.') {
+          isRunning ? reset() : start();
+          setError('rateError', {
+            type: 'manual',
+            message: errorMessageTemplates.rateError,
+          });
+        }
+      }
+    }
   };
+
+  useEffect(() => {}, [disabled]);
 
   return (
     <div className={classes['login-basic']}>
@@ -108,14 +146,15 @@ const LoginContent = (props) => {
                   placeholder="Електронна пошта"
                   {...register('email', {
                     required: errorMessageTemplates.required,
-                    validate: (value) => validator.isEmail(value) || errorMessageTemplates.email,
+                    validate: (value) =>
+                      validator.isEmail(value) || errorMessageTemplates.email,
                   })}
                 />
               </div>
               <span className={classes['error-message']}>
                 {errors.email && errors.email.message}
                 {errors.required && errors.required.message}
-                </span>
+              </span>
             </div>
             <div className={classes['login-content__item']}>
               <label
@@ -139,20 +178,24 @@ const LoginContent = (props) => {
                       required: errorMessageTemplates.required,
                     })}
                   />
-                  <span className={classes['password-visibility']} onClick={togglePassword}>
+                  <span
+                    className={classes['password-visibility']}
+                    onClick={togglePassword}
+                  >
                     {!showPassword ? <EyeInvisible /> : <EyeVisible />}
                   </span>
                 </div>
               </div>
               <span className={classes['error-message']}>
-                    {errors.password && errors.password.message}
-                    {errors.required && errors.required.message}
-                    {errors.unspecifiedError && errors.unspecifiedError.message}
+                {errors.password && errors.password.message}
+                {errors.required && errors.required.message}
+                {errors.unspecifiedError && errors.unspecifiedError.message}
               </span>
             </div>
-            <Link to="/reset-password" className={classes['forget-password']}>Забули пароль?</Link>
+            <Link to="/reset-password" className={classes['forget-password']}>
+              Забули пароль?
+            </Link>
           </div>
-
         </div>
         <div className={classes['login-footer']}>
           <div className={classes['login-footer-buttons']}>
@@ -165,9 +208,13 @@ const LoginContent = (props) => {
               </button>
             </Link>
             <button
-              disabled={!isValid}
+              disabled={disabled}
               type="submit"
-              className={isValid ? classes['login-footer-buttons__signin'] : classes['login-footer-buttons__signin__disabled']}
+              className={
+                disabled
+                  ? classes['login-footer-buttons__signin__disabled']
+                  : classes['login-footer-buttons__signin']
+              }
             >
               Увійти
             </button>
