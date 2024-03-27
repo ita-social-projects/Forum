736a60e51b9dd5e81b0de38213aa509644ef5aba
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-03-27 21:35:30 +0100

    ipn and is_fop added

--- FrontEnd/src/components/ProfilePage/FormComponents/FormFields/CheckBoxField.js
+++ FrontEnd/src/components/ProfilePage/FormComponents/FormFields/CheckBoxField.js
@@ -2,49 +2,66 @@ import { PropTypes } from 'prop-types';
 import css from './CheckBoxField.module.css';
 
 const CheckBoxField = (props) => {
-    return (<div className={css['representative']}>
-        <div className={css['representative__label']}>
-            {props.requredField && <label className={css['representative__label--required']}>*</label>}
-            <label
-                className={`${css['representative__label--text']} ${!props.requredField && css['fields__field--notrequired']}`}
-            >
-                Кого ви представляєте?
+    return (props.fop_field ? (
+        <div className={`${css['representative__checkboxes']} ${css['fop-field']}`}>
+            <input
+                className={css['checkbox__input']}
+                name={props.name}
+                type="checkbox"
+                value={props.name}
+                onChange={props.updateHandler}
+                checked={props.value}
+            />
+            <label className={css['form-control']}>
+                ФОП
             </label>
         </div>
-        <div className={css['representative__checkboxes']}>
-            <div className={css['representative__checkboxes--company-type']}>
-                <label className={css['form-control']}>
-                    <input
-                        className={css['checkbox__input']}
-                        name={props.nameRegister}
-                        type="checkbox"
-                        value={props.nameRegister}
-                        onChange={props.updateHandler}
-                        checked={props.valueRegister}
-                    />
-                    Зареєстрована компанія
+        ) : (
+        <div className={css['representative']}>
+            <div className={css['representative__label']}>
+                {props.requredField && <label className={css['representative__label--required']}>*</label>}
+                <label
+                    className={`${css['representative__label--text']} ${!props.requredField && css['fields__field--notrequired']}`}
+                >
+                    Кого ви представляєте?
                 </label>
             </div>
-            <div className={css['representative__checkboxes--company-type']}>
-                <label className={css['form-control']}>
-                    <input
-                        className={css['checkbox__input']}
-                        name={props.nameStartup}
-                        type="checkbox"
-                        value={props.nameStartup}
-                        onChange={props.updateHandler}
-                        checked={props.valueStartup}
-                    />
-                    Стартап проект, який шукає інвестиції
-                </label>
+            <div className={css['representative__checkboxes']}>
+                <div className={css['representative__checkboxes--company-type']}>
+                    <label className={css['form-control']}>
+                        <input
+                            className={css['checkbox__input']}
+                            name={props.nameRegister}
+                            type="checkbox"
+                            value={props.nameRegister}
+                            onChange={props.updateHandler}
+                            checked={props.valueRegister}
+                        />
+                        Зареєстрована компанія
+                    </label>
+                </div>
+                <div className={css['representative__checkboxes--company-type']}>
+                    <label className={css['form-control']}>
+                        <input
+                            className={css['checkbox__input']}
+                            name={props.nameStartup}
+                            type="checkbox"
+                            value={props.nameStartup}
+                            onChange={props.updateHandler}
+                            checked={props.valueStartup}
+                        />
+                        Стартап проект, який шукає інвестиції
+                    </label>
+                </div>
             </div>
+            {(props.requredField || props.error) &&
+                <span className={css['error-message']}>
+                    {props.error}
+                </span>
+                }
         </div>
-        {(props.requredField || props.error) &&
-            <span className={css['error-message']}>
-                {props.error}
-            </span>
-            }
-    </div>);
+        )
+    );
 };
 
 export default CheckBoxField;
