153cf9d3ec6881d685a24c13e80ac1af8bb2417c
romanmyko <romanklapek@gmail.com>
2024-03-28 10:36:58 +0200

    checkIsStaff.js rebuild

--- FrontEnd/src/components/adminPage/checkIfStaff.js
+++ FrontEnd/src/components/adminPage/checkIfStaff.js
@@ -3,11 +3,7 @@ import axios from 'axios';
 async function checkIfStaff() {
     const url = `${process.env.REACT_APP_BASE_API_URL}/api/auth/users/me/`;
     const userDataResponse = await axios.get(url);
-    if (userDataResponse.data.is_staff === true ) {
-        return true;
-    } else {
-        return false;
-    }
+    return userDataResponse.data.is_staff === true;
 }
 export default checkIfStaff;
 
