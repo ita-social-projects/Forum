f8f1b0839a934047207b8699e16ce1cce5410981
Sofia Bilous <EgikSonya@gmail.com>
2024-04-09 23:32:44 +0300

    delete imports

--- search/tests/test_advanced_search.py
+++ search/tests/test_advanced_search.py
@@ -1,12 +1,9 @@
-from rest_framework import status
 from rest_framework.test import APITestCase
 
 from authentication.factories import UserFactory
 from profiles.factories import (
     ProfileStartupFactory,
     ProfileCompanyFactory,
-    ActivityFactory,
-    RegionFactory,
 )
 
 from utils.dump_response import dump  # noqa
