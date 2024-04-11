b34b51ad759b38eb002dc73f06a40f884a371585
romanmyko <romanklapek@gmail.com>
2024-04-11 14:51:41 +0300

    rename index.css => index.module.css

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.module.css';
 import './global.css';
 import reportWebVitals from './reportWebVitals';
 
