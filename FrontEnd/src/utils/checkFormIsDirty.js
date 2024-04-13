cfe747535c12aa51e1b19ac5438c1d70f0abd27c
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-04-13 20:52:34 +0200

    condition fixed

--- FrontEnd/src/utils/checkFormIsDirty.js
+++ FrontEnd/src/utils/checkFormIsDirty.js
@@ -20,12 +20,12 @@ const checkFormIsDirty = (fields, userData, profileData) => {
     return Object.keys(fields).some(key => {
       const { defaultValue, type, context } = fields[key];
       const currentValue = context === 'user' ? userData?.[key] : profileData?.[key];
-
       if (type === 'array') {
         return !compareArrays(defaultValue, currentValue);
       }
       if (key === 'founded') {
-        if (((defaultValue === null && currentValue !== '')) || (defaultValue.toString() !== currentValue.toString())) {
+        if ((defaultValue !== null && currentValue !== null && defaultValue.toString() !== currentValue.toString()) ||
+            (defaultValue === null && (currentValue !== null && currentValue !== ''))) {
           return true;
         }
         return false;
