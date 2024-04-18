c3158a3ef3b62d7ffc494694757c0356de2505f7
romanmyko <romanklapek@gmail.com>
2024-04-18 15:22:24 +0300

    rename def

--- administration/tests/test_admin_user.py
+++ administration/tests/test_admin_user.py
@@ -168,7 +168,7 @@ class TestPatchUser(APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_patch_reverese_bool_user(self):
+    def test_patch_change_role_user(self):
         self.client.force_authenticate(self.user)
         data = {
             "name": "string",
