53cbf7322fb1675d707a74737c73a839f7d5b41b
romanmyko <romanklapek@gmail.com>
2024-04-21 14:31:33 +0300

    region v0

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -22,10 +22,35 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class RegionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Region
-        fields = "__all__"
+class RegionSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name_eng = serializers.CharField()
+    name_ukr = serializers.CharField()
+
+    def update(self, instance, validated_data):
+        instance.name_eng = validated_data.get("name_eng", instance.name_eng)
+        instance.name_ukr = validated_data.get("name_ukr", instance.name_ukr)
+        instance.save()
+        return instance
+
+    def create(self, validated_data):
+        name_eng = validated_data.get("name_eng")
+        name_ukr = validated_data.get("name_ukr")
+        if (
+            name_eng
+            and Region.objects.filter(name_eng__iexact=name_eng).exists()
+        ):
+            raise serializers.ValidationError(
+                "Region with this name already exists."
+            )
+        if (
+            name_ukr
+            and Region.objects.filter(name_ukr__iexact=name_ukr).exists()
+        ):
+            raise serializers.ValidationError(
+                "Region with this name already exists."
+            )
+        return Region.objects.create(**validated_data)
 
 
 class ProfileListSerializer(serializers.ModelSerializer):
