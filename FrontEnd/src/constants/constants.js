279f89c0178e31cc76d9427b1a8c90d5cff3cd37
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-05-15 13:31:11 +0300

    fixed the maximum length of the password field according to the new requirements. 100 characters

--- FrontEnd/src/constants/constants.js
+++ FrontEnd/src/constants/constants.js
@@ -1,4 +1,4 @@
 export const EMAIL_PATTERN = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-export const PASSWORD_PATTERN = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,128}$/;
+export const PASSWORD_PATTERN = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,100}$/;
 export const NAME_SURNAME_PATTERN = /^(?=.{2,50}$)[a-zA-Zа-щюяьА-ЩЮЯЬїЇіІєЄґҐ']+(\s[a-zA-Zа-щюяьА-ЩЮЯЬїЇіІєЄґҐ']+)*$/;
 export const COMPANY_NAME_PATTERN = /^.{2,100}$/;
