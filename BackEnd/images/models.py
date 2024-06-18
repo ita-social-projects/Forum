7bc72e59a825777a45072d98d9df377616be39ba
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-06-18 19:04:25 +0200

    extension validation to model

--- BackEnd/images/models.py
+++ BackEnd/images/models.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from django.db import models
+from django.core.validators import FileExtensionValidator
 from authentication.models import CustomUser
 
 
@@ -24,7 +25,7 @@ class ProfileImage(models.Model):
     )
     image_type = models.CharField(max_length=10, choices=IMAGE_TYPES)
     content_type = models.CharField(max_length=5, blank=True)
-    image_path = models.ImageField(upload_to=image_directory_path, blank=True)
+    image_path = models.ImageField(upload_to=image_directory_path, validators=[FileExtensionValidator(allowed_extensions=["jpeg", "png", "jpg"])], blank=True)
     image_size = models.PositiveIntegerField(null=True)
     hash_md5 = models.CharField(max_length=32, blank=True)
     is_approved = models.BooleanField(default=False)
