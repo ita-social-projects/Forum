7dd93a00c1af62b1636c26ba8fc1a7b384c18bc3
Yan Zhylavy <yanzhylavy@gmail.com>
2024-07-03 12:25:30 +0300

    Created model for storing autoapprove hours, migration for default setting

---
+++ BackEnd/administration/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_auto_moderation_hours(value: int):
+    if value < 1 or value > 48:
+        raise ValidationError("Кількість годин має бути в діапазоні 1-48 години")    
+
+
+class AutoModeration(models.Model):
+    auto_moderation_hours = models.PositiveSmallIntegerField(default=12, validators=[validate_auto_moderation_hours])
+
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super().save(*args, **kwargs)    
+
+    @classmethod
+    def get_auto_moderation_hours(cls):
+        obj, created = cls.objects.get_or_create(pk=1, defaults={"auto_moderation_hours": 12})
+        return obj        
