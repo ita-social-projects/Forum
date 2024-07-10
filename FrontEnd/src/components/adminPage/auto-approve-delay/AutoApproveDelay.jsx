ec414bbb2b258fd3bdaccb9a0c93b59b366984ba
Yan Zhylavy <yanzhylavy@gmail.com>
2024-07-10 15:58:07 +0300

    Added useSWR into AutoApproveDelay

--- FrontEnd/src/components/adminPage/auto-approve-delay/AutoApproveDelay.jsx
+++ FrontEnd/src/components/adminPage/auto-approve-delay/AutoApproveDelay.jsx
@@ -1,43 +1,35 @@
 import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { Tooltip } from 'antd';
+import useSWR from 'swr';
 import axios from 'axios';
 import css from './AutoApproveDelay.module.css';
 
 const AutoApproveDelay = () => {
-
-    const [delay, setDelay] = useState('');
+    const fetcher = url => axios.get(url).then(res => res.data);
+    const url = `${process.env.REACT_APP_BASE_API_URL}/api/admin/automoderation/`;
+    const { data, mutate } = useSWR(url, fetcher);
+    const [delay, setDelay] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/admin/automoderation/`);
-                if (response.data && response.data.auto_moderation_hours) {
-                    setDelay(response.data.auto_moderation_hours);
-                } else {
-                    throw new Error('Помилка з`єднання з сервером');
-                }
-            } catch (err) {
-                toast.error(err.message);
-            }
-        };
-
-        fetchData();
-    }, []);
+        if (data && data.auto_moderation_hours) {
+            setDelay(data.auto_moderation_hours);
+        }
+    }, [data]);
 
     const handleInputChange = (e) => {
-        let value = e.target.value;
+        let value = Number(e.target.value);
         setError(null);
         setDelay(value);
-        if (!(1 <= value) || !(value <= 48) || !Number.isInteger(Number(value))) {
+        if (!(1 <= value && value <= 48) || !Number.isInteger(value)) {
             setError('Кількість годин має бути в діапазоні 1-48 години');
         }
     };
 
     const handleSubmit = () => {
         !error && axios.put(`${process.env.REACT_APP_BASE_API_URL}/api/admin/automoderation/`, { 'auto_moderation_hours': delay })
-            .then(() => toast.success('Зміни успішно застосовано.'))
+            .then(() => { toast.success('Зміни успішно застосовано.'); mutate({ ...data, auto_moderation_hours: delay }); })
             .catch((e) => toast.error(e.message));
     };
     return (
@@ -46,7 +38,7 @@ const AutoApproveDelay = () => {
                 title={'Введіть значення 1-48'}
                 placement="top"
                 pointAtCenter={true}>
-            <input className={css['autoapprove-input']} type="number" step={1} onChange={handleInputChange} value={delay} />
+                <input className={css['autoapprove-input']} type="number" step={1} onChange={handleInputChange} value={delay} />
             </Tooltip>
             {error &&
                 <p className={css['error-message']}>{error}</p>}
