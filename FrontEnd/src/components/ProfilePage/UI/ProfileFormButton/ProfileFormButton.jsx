bb674e74416ddac14391c71368e1a4e4023ed94d
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-07-12 19:01:39 +0200

    disable submit button when there are no changes

--- FrontEnd/src/components/ProfilePage/UI/ProfileFormButton/ProfileFormButton.jsx
+++ FrontEnd/src/components/ProfilePage/UI/ProfileFormButton/ProfileFormButton.jsx
@@ -1,12 +1,14 @@
 import css from './ProfileFormButton.module.css';
 
 const ProfileFormButton = (props) => {
+  console.log('PROPS', props.formState);
   return (
     <div className={css['sign-up-footer__buttons']}>
       <button
         form={props.formName}
         className={css['sign-up__button']}
         type="submit"
+        disabled={props.formState === false}
       >
         Зберегти зміни
       </button>
