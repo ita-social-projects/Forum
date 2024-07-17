350167d02d6d91be72c3df5c05fc581701554eb8
KyzukY <kyzuk.yr@gmail.com>
2024-07-17 13:12:50 +0300

    add BanerModeration.css

--- FrontEnd/src/components/ProfilePage/FormComponents/BanerModeration.jsx
+++ FrontEnd/src/components/ProfilePage/FormComponents/BanerModeration.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Tooltip } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import './BanerModeration.css';
 
 const BanerModeration = () => {
-  const [moderationTime, setModerationTime] = useState(0);
+  const [moderationTime, setModerationTime] = useState(12);
 
   useEffect(() => {
     const fetchModerationTime = async () => {
       try {
-        const response = await axios.get('/api/automoderation/');
-        console.log('Response data:', response.data); // Додаємо логування для перевірки
+        const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/admin/automoderation/`);
         setModerationTime(response.data.auto_moderation_hours);
       } catch (error) {
         console.error('Error fetching moderation time:', error);
@@ -19,18 +21,23 @@ const BanerModeration = () => {
   }, []);
 
   return (
-    <div>
-      <form>
-            Контент доданий до Банеру буде підлягати модерації.
-            Після затвердження модератором контент стане доступним усім користувачам сайту.
-            Час модерації - до {moderationTime} годин. Якщо запит не буде розглянуто впродовж цього часу то зміни автоматично будуть вважатись затвердженими.
-            УВАГА: Профіль буде заблоковано якщо доданий контент має непристойний зміст.
-        {/* Інші поля */}
-      </form>
+    <div className="moderation-container">
+      <div className="moderation-warning">
+        <Tooltip>
+          <div className="warning-icon">
+            <ExclamationCircleOutlined />
+          </div>
+        </Tooltip>
+        <div className="moderation-text">
+          Контент доданий до Банеру, Логотипу буде підлягати модерації.
+          Після затвердження модератором контент стане доступним усім користувачам сайту.
+          Час модерації - до {moderationTime} годин. Якщо запит не буде розглянуто впродовж цього часу, то зміни автоматично будуть вважатись затвердженими.
+          <br /><br />
+          <span className="attention">Увага:</span> Профіль буде заблоковано, якщо доданий контент має непристойний зміст.
+        </div>
+      </div>
     </div>
   );
 };
 
 export default BanerModeration;
-
-
