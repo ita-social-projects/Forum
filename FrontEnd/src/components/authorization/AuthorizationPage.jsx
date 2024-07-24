501048d4c4c50fbaf80f6eb5a39c5a1950b0ad4b
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-07-24 18:05:35 +0200

    toastify errors

--- FrontEnd/src/components/authorization/AuthorizationPage.jsx
+++ FrontEnd/src/components/authorization/AuthorizationPage.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react';
 import Login from '../authorization/Login';
-import AlertMessage from '../authorization/AlertMessage';
 import classes from './AuthorizationPage.module.css';
 import dots_decor from './dots_decor.svg';
 
 const AuthorizationPage = () => {
-  const [errorText, setErrorText] = useState('');
 
   return (
     <div className={classes['auth-page']}>
-      {errorText && <AlertMessage errorMessage={errorText} setErrorMessage={setErrorText} />}
       <img className={classes['frame-img-left']} src={dots_decor} alt="frame" />
       <img className={classes['frame-img-right']} src={dots_decor} alt="frame" />
-      <Login setErrorMessage={setErrorText} />
+      <Login />
     </div>
   );
 };
