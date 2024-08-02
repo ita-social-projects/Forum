63549303c5e947da9990ef4999e732c648aa1462
romanmyko <romanklapek@gmail.com>
2024-08-02 15:47:58 +0300

    About Cards map text.js

---
+++ FrontEnd/src/components/landing-page/about-section/cards/text.js
@@ -0,0 +1,57 @@
+const AboutUsText = {
+    id: 1,
+    title: 'ЧОМУ ВАРТО',
+    level: 2,
+    content: [
+        {
+            id: 2,
+            type: 'paragraph__margin_bottom',
+            level: 2,
+            logo: 'expert.svg',
+            title: 'Прямий зв\'язок з виробниками',
+            text: 'Знайомтеся з історією та цінностями брендів.'
+        },
+        {
+            id: 3,
+            type: 'paragraph__margin_bottom',
+            level: 2,
+            logo: 'info.svg',
+            title: 'Ексклюзивні пропозиції',
+            text: 'Знаходьте унікальні продукти, недоступні в масовому продажі.'
+        },
+        {
+            id: 4,
+            type: 'paragraph__margin_bottom',
+            level: 2,
+            logo: 'contacts.svg',
+            title: 'Інновації та тренди',
+            text: 'Будьте в курсі останніх новинок та технологій галузі.'
+        },
+        {
+            id: 5,
+            type: 'paragraph__margin_bottom',
+            level: 2,
+            logo: 'contacts.svg',
+            title: 'Співпраця та синергія',
+            text: 'Об\'єднуйтесь, щоб творити нове та ділитися досвідом.'
+        },
+        {
+            id: 6,
+            type: 'paragraph__margin_bottom',
+            level: 2,
+            logo: 'contacts.svg',
+            title: 'Розвиток та масштабування',
+            text: 'Знаходьте нових партнерів, клієнтів та ринки збуту.'
+        },
+        {
+            id: 7,
+            type: 'paragraph__margin_bottom',
+            level: 2,
+            logo: 'exchange.svg',
+            title: 'Підтримка та знання',
+            text: 'Отримуйте консультації, експертну допомогу та доступ до освітніх ресурсів.'
+        },
+    ],
+};
+
+export default AboutUsText;
