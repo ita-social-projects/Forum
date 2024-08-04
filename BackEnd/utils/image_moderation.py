5f0a7508ada9756df9e1e20d86db8ce68dd7ac4b
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-08-04 13:24:13 +0200

    approve image added to moderation manager

--- BackEnd/utils/image_moderation.py
+++ BackEnd/utils/image_moderation.py
@@ -1,4 +1,5 @@
 from django.utils.timezone import now
+from utils.completeness_counter import completeness_count
 
 
 class ModerationManager:
@@ -24,3 +25,17 @@ class ModerationManager:
             self.update_status()
             self.banner_logo["logo"] = self.profile.logo
         return self.moderation_is_needed
+
+    def approve_image(self):
+        if self.needs_moderation(self.profile.banner):
+            self.profile.banner.is_approved = True
+            self.profile.banner.save()
+            self.profile.banner_approved = self.profile.banner
+        if self.needs_moderation(self.profile.logo):
+            self.profile.logo.is_approved = True
+            self.profile.logo.save()
+            self.profile.logo_approved = self.profile.logo
+        self.profile.status = "approved"
+        self.profile.status_updated_at = now()
+        self.profile.save()
+        completeness_count(self.profile)
