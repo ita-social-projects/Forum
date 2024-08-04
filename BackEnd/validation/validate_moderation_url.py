fa072ea3062d7cd8e026562ba7cb96b30821b8bd
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-08-04 13:23:41 +0200

    moderation ulr validation added

---
+++ BackEnd/validation/validate_moderation_url.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_url(profile, timestamp):
+    url_timestamp = int(timestamp)
+    if url_timestamp < int(profile.status_updated_at.timestamp()):
+         if profile.status == "pending":
+            raise ValidationError("There is a new request for moderation. URL is outdated")
+         else:
+            raise ValidationError("The change approval request has been processed. URL is outdated")
