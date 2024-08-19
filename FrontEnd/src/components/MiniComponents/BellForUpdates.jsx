add747fbfc6f3c61f5ca5e7e792d8f1eefcf126a
superolegatron <olegtsirkun@gmail.com>
2024-08-19 17:02:01 +0300

    Added bell component

---
+++ FrontEnd/src/components/MiniComponents/BellForUpdates.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './BellForUpdates.module.css';
+
+export default function BellForUpdates({ savedIsUpdated }){
+    return (
+        <img
+            src={`${process.env.PUBLIC_URL}/svg/bell-icon.svg`}
+            className={`${styles['bell']} ${!savedIsUpdated ? styles['hidden'] : ''}`}
+            alt="Bell Icon"
+        />
+
+    );
+}
+
+BellForUpdates.propTypes={
+    savedIsUpdated: PropTypes.bool.isRequired,
+};
