307ee4e60521d04f2ed96c8459d48cc17e84005b
superolegatron <olegtsirkun@gmail.com>
2024-09-03 18:03:29 +0300

    Bug fix

--- BackEnd/forum/urls.py
+++ BackEnd/forum/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     ),
     path("api/", include("search.urls", namespace="search")),
     path("api/", include("images.urls", namespace="images")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
@@ -41,5 +42,4 @@ if settings.DEBUG:
             SpectacularSwaggerView.as_view(url_name="schema"),
             name="schema_docs",
         ),
-        path("__debug__/", include("debug_toolbar.urls")),
     ]
