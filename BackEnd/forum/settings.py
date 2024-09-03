ef38ab78fd475c85a9267272cfa213bc171512fb
superolegatron <olegtsirkun@gmail.com>
2024-09-04 00:22:58 +0300

    minor fixes

--- BackEnd/forum/settings.py
+++ BackEnd/forum/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [
     "localhost",
