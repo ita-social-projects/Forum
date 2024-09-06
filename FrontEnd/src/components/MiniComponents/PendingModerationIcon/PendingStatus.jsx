436b7beaaa64186b184272a2747a7b2de398eb8b
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-09-06 12:32:15 +0300

    added elementType for the banner and logo and checking whether there is approval

--- FrontEnd/src/components/MiniComponents/PendingModerationIcon/PendingStatus.jsx
+++ FrontEnd/src/components/MiniComponents/PendingModerationIcon/PendingStatus.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { Tooltip } from 'antd';
 import css from './PendingStatus.css';
 
-const PendingStatus = ({ profile }) => {
+const PendingStatus = ({ profile, elementType }) => {
   if (!profile || profile.status !== 'pending') return null;
+
+  const bannerApproved = profile.banner_approved;
+  const logoApproved = profile.logo_approved;
+
+  const isBanner = elementType === 'banner';
+  const isLogo = elementType === 'logo';
+  const shouldShowTooltip = (isBanner && !bannerApproved) || (isLogo && !logoApproved);
+
+  console.log('banner_approved:', profile?.banner_approved);
+  console.log('logo_approved:', profile.logo_approved);
+
+  if (!shouldShowTooltip) return null;
 
   const formattedDate = new Date(profile.status_updated_at).toLocaleString('uk-UA', {
     year: 'numeric',
