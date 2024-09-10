24a74774843d515b1a728cf98f567511c66712fd
Alex <alexandersychev2005@gmail.com>
2024-09-10 18:24:42 +0300

    Added the functionality when email with info about logos and banners sends to admin email on our table, or email in settings

--- BackEnd/utils/images/send_email.py
+++ BackEnd/utils/images/send_email.py
@@ -3,9 +3,18 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+from administration.models import ModerationEmail
+
 EMAIL_CONTENT_SUBTYPE = "html"
 PROTOCOL = "http"
 DOMAIN = config("ALLOWED_ENV_HOST")
+
+
+def set_admin_email():
+    email = ModerationEmail.objects.first()
+    if email is None:
+        email = settings.EMAIL_HOST_USER
+    return email
 
 
 def send_email_about_banners_and_logos(
@@ -19,13 +28,14 @@ def send_email_about_banners_and_logos(
         "size_of_logos": size_of_logos_kb,
     }
 
+    recipient = set_admin_email()
     email_body = render_to_string("images/email_template.html", context)
     email = EmailMultiAlternatives(
         subject="Information about number and size of banners and logos",
         body=email_body,
         from_email=settings.EMAIL_HOST_USER,
         to=[
-            settings.EMAIL_HOST_USER,
+            recipient,
         ],
     )
 
