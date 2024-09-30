42024ddd1aacb94d96b174fa9c44916e7f2f0865
OlesiaBarannik <olesyabarannik23@gmail.com>
2024-09-30 12:41:16 +0300

    Used Ant Design for categories and added loader on message submission

--- FrontEnd/src/components/Contact/Contact.jsx
+++ FrontEnd/src/components/Contact/Contact.jsx
@@ -3,161 +3,170 @@ import axios from 'axios';
 import LinkContainer from '../../pages/CookiesPolicyPage/LinkContainer.jsx';
 import styles from './Contact.module.css';
 import contactText from './text';
-import useScrollToTop from '../../hooks/useScrollToTop';
+import useScrollToTop from '../../hooks/useScrollToTop'; // Ensure this is used
 import {
     EMAIL_PATTERN,
     MESSAGE_PATTERN
 } from '../../constants/constants';
+import PropTypes from 'prop-types';
+import MultipleSelectChip from '../../pages/ProfilePage/FormComponents/FormFields/MultipleSelectChip.jsx';
+import Loader from '../Loader/Loader.jsx';
 
 const Contact = () => {
-  useScrollToTop();
+    useScrollToTop(); // Call the hook to scroll to top on component mount
 
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('Привіт, хочу повідомити...');
-  const [category, setCategory] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [messageError, setMessageError] = useState('');
-  const [showModal, setShowModal] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('Привіт, хочу повідомити...');
+    const [category, setCategory] = useState(null);
+    const [emailError, setEmailError] = useState('');
+    const [messageError, setMessageError] = useState('');
+    const [showModal, setShowModal] = useState(false);
 
-  const handleEmailChange = (e) => {
-    const { value } = e.target;
-    setEmail(value);
+    const categoryOptions = [
+        { id: 1, name: 'Технічне питання' },
+        { id: 2, name: 'Рекомендації' },
+        { id: 3, name: 'Питання' },
+        { id: 4, name: 'Інше' },
+    ];
 
-    // Validate email using EMAIL_PATTERN
-    if (!EMAIL_PATTERN.test(value)) {
-      setEmailError('Електронна пошта не відповідає вимогам');
-    } else {
-      setEmailError('');
-    }
-  };
+    const handleEmailChange = (e) => {
+        const { value } = e.target;
+        setEmail(value);
 
-  const handleMessageChange = (e) => {
-    const { value } = e.target;
-    setMessage(value);
+        if (!EMAIL_PATTERN.test(value)) {
+            setEmailError('Електронна пошта не відповідає вимогам');
+        } else {
+            setEmailError('');
+        }
+    };
 
-    // Validate message length using MESSAGE_PATTERN
-    if (!MESSAGE_PATTERN.test(value)) {
-      setMessageError('Повідомлення не може бути коротшим за 10 символів');
-    } else {
-      setMessageError('');
-    }
-  };
+    const handleMessageChange = (e) => {
+        const { value } = e.target;
+        setMessage(value);
 
-  const handleCategoryChange = (e) => setCategory(e.target.value);
+        if (!MESSAGE_PATTERN.test(value)) {
+            setMessageError('Повідомлення не може бути коротшим за 10 символів');
+        } else {
+            setMessageError('');
+        }
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    if (!EMAIL_PATTERN.test(email)) {
-      setEmailError('Електронна пошта не відповідає вимогам');
-      return;
-    }
+        // Валідація перед відправкою
+        if (!EMAIL_PATTERN.test(email)) {
+            setEmailError('Електронна пошта не відповідає вимогам');
+            return;
+        }
 
-    if (!MESSAGE_PATTERN.test(message)) {
-      setMessageError('Повідомлення не може бути коротшим за 10 символів');
-      return;
-    }
+        if (!MESSAGE_PATTERN.test(message)) {
+            setMessageError('Повідомлення не може бути коротшим за 10 символів');
+            return;
+        }
 
-    setShowModal(true);
-  };
+        setLoading(true);
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/admin/feedback/`, {
+                email: email,
+                message: message,
+                category: category
+            });
 
-  const handleRedirect = () => {
-    window.location.href = '/';
-  };
+            if (response.status === 200) {
+                setShowModal(true);
+                setEmail('');
+                setMessage('Привіт, хочу повідомити...');
+                setCategory(null);
+            }
+        } catch (error) {
+            setShowModal(true);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    try {
-      const response = await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/admin/feedback/`, {
-        email: email,
-        message: message,
-        category: category
-      });
+    const handleRedirect = () => {
+        window.location.href = '/';
+    };
 
-      if (response.status === 200) {
-        alert('Ваше повідомлення надіслано успішно!');
-        setEmail('');
-        setMessage('Привіт, хочу повідомити...');
-        setCategory('');
-      }
-    } catch (error) {
-      if (error.response && error.response.data) {
-        alert('Сталася помилка: ' + JSON.stringify(error.response.data));
-      } else {
-        alert('Виникла проблема з підключенням до сервера: ' + error.message);
-      }
-    }
-  };
+    const closeModal = () => {
+        setShowModal(false);
+    };
 
-  return (
-    <div className={styles['contact_container']}>
-      <div className={styles['contact__link_container']}>
-        <LinkContainer />
-        <img
-          className={styles['contact__img1']}
-          src={`${process.env.REACT_APP_PUBLIC_URL}/img/dots_12x10.png`}
-          alt="dots_12x10.png"
-        />
-      </div>
-      <div className={styles['contact__text_container']}>
-        <h2 className={styles['contact__title']}>{contactText.title}</h2>
-        <form onSubmit={handleSubmit} className={styles['contact__form']}>
-          <label className={styles['contact__label']} htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            className={styles['contact__input']}
-            placeholder="Ваша пошта"
-            required
-          />
-          {/* Show email validation error */}
-          {emailError && <p className={styles['contact__error']}>{emailError}</p>}
+    return (
+        <div className={styles['contact_container']}>
+            <div className={styles['contact__link_container']}>
+                <LinkContainer />
+                <img
+                    className={styles['contact__img1']}
+                    src={`${process.env.REACT_APP_PUBLIC_URL}/img/dots_12x10.png`}
+                    alt="dots_12x10.png"
+                />
+            </div>
+            <div className={styles['contact__text_container']}>
+                <h2 className={styles['contact__title']}>{contactText.title}</h2>
+                <form onSubmit={handleSubmit} className={styles['contact__form']}>
+                    <label className={styles['contact__label']} htmlFor="email">Email:</label>
+                    <input
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        className={styles['contact__input']}
+                        placeholder="Ваша пошта"
+                        required
+                    />
+                    {emailError && <p className={styles['contact__error']}>{emailError}</p>}
 
-          <label className={styles['contact__label']} htmlFor="category">Категорія:</label>
-          <select
-            id="category"
-            value={category}
-            onChange={handleCategoryChange}
-            className={`${styles['contact__select']} ${category === '' ? styles['placeholder'] : ''}`}
-            required
-          >
-            <option value="" disabled>Будь ласка, оберіть тип повідомлення</option>
-            <option value="Технічне питання">Технічне питання</option>
-            <option value="Рекомендації">Рекомендації</option>
-            <option value="Питання">Питання</option>
-            <option value="Інше">Інше</option>
-          </select>
+                    <MultipleSelectChip
+                        value={category ? [category] : []}
+                        onChange={(value) => setCategory(value[0] || null)}
+                        options={categoryOptions}
+                        updateHandler={(value) => setCategory(value[0] || null)}
+                        className={styles['contact__select']}
+                        placeholder="Оберіть категорію"
+                        name="category"
+                        label="Категорія:"
+                    />
 
-          <label className={styles['contact__label']} htmlFor="message">Повідомлення:</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={handleMessageChange}
-            className={styles['contact__textarea']}
-            placeholder="Ваше повідомлення"
-            required
-          />
-          {/* Show message validation error */}
-          {messageError && <p className={styles['contact__error']}>{messageError}</p>}
-
-          <button type="submit" className={styles['contact__button_send']}>Надіслати</button>
-          <button type="button" className={styles['contact__button_cancel']}>Відмінити</button>
-        </form>
-      </div>
-            {/* Modal */}
-      {showModal && (
-        <div className={styles['modal_feedback']}>
-          <div className={styles['modal_feedback_content']}>
-            <h2>Повідомлення успішно надіслано!</h2>
-            <button type="button" onClick={handleRedirect} className={styles['contact__button_send']}>
-              На головну
-            </button>
-          </div>
+                    <label className={styles['contact__label']} htmlFor="message">Повідомлення:</label>
+                    <textarea
+                        id="message"
+                        value={message}
+                        onChange={handleMessageChange}
+                        className={styles['contact__textarea']}
+                        placeholder="Ваше повідомлення"
+                        required
+                    />
+                    {messageError && <p className={styles['contact__error']}>{messageError}</p>}
+                    <button type="submit" className={styles['contact__button_send']}>Надіслати</button>
+                    <button type="button" className={styles['contact__button_cancel']}>Відмінити</button>
+                </form>
+                {loading ? (
+                    <Loader />
+                ) : null}
+            </div>
+            {showModal && (
+                <div className={styles['modal_feedback']}>
+                    <div className={styles['modal_feedback_content']}>
+                        <h2>Повідомлення успішно надіслано!</h2>
+                        <button type="button" onClick={handleRedirect} className={styles['contact__button_send']}>
+                            На головну
+                        </button>
+                        <button type="button" onClick={closeModal} className={styles['contact__button_cancel']}>
+                            Закрити
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-
-    </div>
-  );
+    );
 };
 
 export default Contact;
+
+Contact.propTypes = {
+    currentFormNameHandler: PropTypes.func,
+};
