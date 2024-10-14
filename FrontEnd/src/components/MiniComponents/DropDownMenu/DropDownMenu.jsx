461ee51a398a453634c2d1372f8e167508c94025
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-10-14 14:09:03 +0300

    created a drop down menu

---
+++ FrontEnd/src/components/MiniComponents/DropDownMenu/DropDownMenu.jsx
@@ -0,0 +1,59 @@
+import { Select, Space } from 'antd';
+import PropTypes from 'prop-types';
+import preventEnterSubmit from '../../../utils/preventEnterSubmit';
+import css from './DropDownMenu.module.css';
+
+export default function DropDownMenu(props){
+  return (
+    <div className={css['dropdown__container']}>
+      <div className={css['dropdown__label']}>
+        {props.requiredField && (
+          <label className={css['dropdown__label--required']}>*</label>
+        )}
+        <label
+          className={`${css['dropdown__label--text']} ${
+            !props.requiredField  ? css['dropdown__label--notrequired'] : ''
+          }`}
+        >
+            {props.label}
+        </label>
+      </div>
+      <Space>
+        <Select
+           mode="multiple"
+           name={props.name}
+           value={props.value}
+           placeholder={props.placeholder || 'Оберіть'}
+           variant="borderless"
+           className={css['dropdown__select-container']}
+           dropdownStyle={{
+             width: '400px',
+             paddingLeft: '0px',
+             paddingRight: '0px',
+             borderRadius: '2px',
+           }}
+           onChange={props.updateHandler}
+           onKeyDown={preventEnterSubmit}
+           options={props.options.map((option) => ({
+             value: option.name ?? option.name_ukr,
+           }))}
+        />
+      </Space>
+      {props.requiredField && (
+        <div className={css['error-message']}>{props.error}</div>
+      )}
+    </div>
+  );
+}
+
+DropDownMenu.propTypes = {
+    requiredField: PropTypes.bool,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.array,
+    options: PropTypes.array,
+    updateHandler: PropTypes.func.isRequired,
+    error: PropTypes.string,
+    placeholder: PropTypes.string,
+  };
+
