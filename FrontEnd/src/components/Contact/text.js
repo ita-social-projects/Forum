97bc3de2a73cc8ddefe5db496f0627ddd05e11f8
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-10-18 13:48:53 +0300

    deleted redundant data

--- FrontEnd/src/components/Contact/text.js
+++ FrontEnd/src/components/Contact/text.js
@@ -3,17 +3,6 @@ const contactText = {
     title: 'Зворотній зв\'язок',
     level: 2,
     content: [
-        {
-            id: 2,
-            type: 'paragraph',
-            text: 'Effective Date:November 2, 2022'
-        },
-        {
-            id: 3,
-            type: 'heading',
-            level: 4,
-            text: 'What is Kickstarter\'s copyright policy?'
-        },
     ],
 };
 
