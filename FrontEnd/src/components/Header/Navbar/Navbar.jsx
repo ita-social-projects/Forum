51570772f66b862946460b02954231ac5456b38b
superolegatron <olegtsirkun@gmail.com>
2024-10-24 21:39:58 +0300

    small styling fixes and for now navbar exists during auth too

--- FrontEnd/src/components/Header/Navbar/Navbar.jsx
+++ FrontEnd/src/components/Header/Navbar/Navbar.jsx
@@ -11,14 +11,12 @@ import css from './Navbar.module.css';
 
 function Navbar (props) {
   const { isStaff} = useAuth();
-  const hideMenu = props.page === '/login' || props.page === '/sign-up' || props.page.includes('/customadmin');
   return (
     <div className={css['navbar-content']}>
       <div className={css['navbar-logo__text']}>
         <Link to="/" target={isStaff ? '_blank' : null}>
           <div className={css['navbar-main-logo']}>
             <img
-                // className={css['navbar-main-logo']}
                 src={`${process.env.REACT_APP_PUBLIC_URL}/craftMerge-logo.svg`}
                 alt="CraftMerge logo"
             ></img>
@@ -27,12 +25,10 @@ function Navbar (props) {
         </Link>
       </div>
       <div className={css['navbar-utility-bar']}>
-        {hideMenu ? null : (
-            <>
-              <Menu/>
-              <SearchBox></SearchBox>
-            </>
-        )}
+        <>
+          <Menu/>
+          <SearchBox></SearchBox>
+        </>
         {props.isAuthorized === true ? <Profile /> : <Buttons adminPage={props.page.includes('/customadmin')} />}
       </div>
     </div>
