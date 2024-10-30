34b4d457463c04ee7935b2ebaea37ea0014ecb66
OlesiaBarannik <olesiabarannik@outlook.com>
2024-10-30 14:12:19 +0200

    Added protocol variable to context in send_email_feedback function

--- BackEnd/utils/administration/send_email_feedback.py
+++ BackEnd/utils/administration/send_email_feedback.py
@@ -5,11 +5,12 @@ from django.conf import settings
 from utils.images.send_email import set_admin_email
 
 EMAIL_CONTENT_SUBTYPE = "html"
-
+PROTOCOL = "http"
 
 def send_email_feedback(user_email, message, category):
     """Function for sending feedback letters to the administrator and the user."""
     context = {
+        "protocol": PROTOCOL,
         "category": category,
         "message": message,
         "user_email": user_email,
