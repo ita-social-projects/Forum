0185abd880248b1d2ec560874e1e2b734be3e737
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-10-30 13:53:51 +0200

    created the banner section structure according to the new design

--- FrontEnd/src/pages/LandingPage/Banner/Banner.jsx
+++ FrontEnd/src/pages/LandingPage/Banner/Banner.jsx
@@ -1,33 +1,28 @@
-import styles from './Banner.css';
-//import { Link } from 'react-router-dom';
+import styles from './Banner.module.css';
+import { Link } from 'react-router-dom';
 //import { PropTypes } from 'prop-types';
 
 const MainBanner = () => {
   return (
     <div  className={styles['main-container']}>
-      <img src="" alt="" /><img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-
-      <img src="" alt="" />
-
-      <img src="" alt="" />
-
-      <img src="" alt="" />
-
-      <img src="" alt="" />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in voluptatum ipsa nobis odio corporis sunt sit aliquam quod placeat, nam quidem optio, error dignissimos beatae perferendis debitis sapiente! Voluptates.</p>
+      <div className={styles['main-baner-text']}>
+        <h2 className={styles['main-baner-text__logo']}>CRAFTMERGE</h2>
+        <h3 className={styles['main-baner-text__explanation']}>Обʼєднуємо крафтових виробників та інноваторів</h3>
+        <div className={styles['main-banner-link__container']}>
+        <Link
+                className={styles['main-banner-link']}
+                to="/#"
+              >
+                Детальніше про нас
+        </Link>
+        </div>
+      </div>
+      <div>
+        <img className={styles['main-banner__img']}
+          src={`${process.env.REACT_APP_PUBLIC_URL}/img/main-baner.png`}
+          alt="Banner img"
+        />
+      </div>
     </div>
   );
 };
