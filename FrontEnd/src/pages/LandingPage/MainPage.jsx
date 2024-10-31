3a81c3c327182f831ae5ba11c92686e3206e7ad4
romanmyko <romanklapek@gmail.com>
2024-10-31 15:14:47 +0200

    rename main baner to JoinUs block

--- FrontEnd/src/pages/LandingPage/MainPage.jsx
+++ FrontEnd/src/pages/LandingPage/MainPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import MainBanner from './Banner/Banner';
 import MainCompanies from './Companies/Companies';
-import MainLoginBanner from './LoginBanner/LoginBanner';
+import JoinUs from './JoinUs/JoinUs';
 import MainAboutSection from './AboutSection/About';
 import CookieMod from '../../components/CookieAcception/CookieMod';
 import css from './MainPage.module.css';
@@ -14,20 +14,14 @@ const MainPage = ({ isAuthorized }) => {
   const [modalActive, setModalActive] = useState(true);
   return (
     <div className={css['main-app']}>
-      <div className={css['main-app-header']}>
-        <MainBanner isAuthorized={isAuthorized} />
-        <div className={css['main-app-body']}>
-          <MainCompanies isAuthorized={isAuthorized} />
-          {!isAuthorized ? <MainLoginBanner /> : null}
-          <MainAboutSection />
-          <div>
-            <CookieMod
-              active={modalActive}
-              setActive={setModalActive}
-            ></CookieMod>
-          </div>
-        </div>
-      </div>
+      <MainBanner isAuthorized={isAuthorized} />
+      <MainCompanies isAuthorized={isAuthorized} />
+      {!isAuthorized ? <JoinUs /> : null}
+      <MainAboutSection />
+      <CookieMod
+        active={modalActive}
+        setActive={setModalActive}
+      ></CookieMod>
     </div>
   );
 };
