2dbb1dc1faa3ee2764f5ebe776d9590f3f2143f1
Yan Zhylavy <yanzhylavy@gmail.com>
2024-11-01 18:11:09 +0200

    Add custom reusable hook useWindowWidth

--- FrontEnd/src/pages/LandingPage/Companies/Companies.jsx
+++ FrontEnd/src/pages/LandingPage/Companies/Companies.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import useWindowWidth from '../../../hooks/useWindowWidth';
 import { Link } from 'react-router-dom';
 import styles from './Companies.module.css';
 import CompanyCard from '../../../components/CompanyCard/CompanyCard';
@@ -11,17 +12,7 @@ const MainCompanies = ({ isAuthorized }) => {
   const baseUrl = process.env.REACT_APP_BASE_API_URL;
   const [searchResults, setSearchResults] = useState([]);
   const [newMembers, setNewMembers] = useState(true);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  const windowWidth = useWindowWidth();
 
   const fetcher = async (url) => {
     const data = await axios.get(url);
@@ -62,11 +53,15 @@ const MainCompanies = ({ isAuthorized }) => {
           <Link to="profiles/companies">
             <p>{linkText}
               <svg width="22" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0.768024 7.90895L12.5223 7.90895L5.487 14.0161C5.3745 14.1145 5.4428 14.2973 5.59146 14.2973L7.36936 14.2973C7.44771 14.2973 7.52204 14.2692 7.5803 14.219L15.172 7.63172C15.2416 7.57145 15.2973 7.49694 15.3356 7.41323C15.3738 7.32953 15.3936 7.23858 15.3936 7.14657C15.3936 7.05455 15.3738
-                6.96361 15.3356 6.8799C15.2973 6.79619 15.2416 6.72168 15.172 6.66141L7.53611 0.0339549C7.50597 0.00783851 7.46981 -0.00622437 7.43164 -0.00622437L5.59347 -0.00622454C5.44481 -0.00622455 5.37651 0.178598 5.48901 0.275025L12.5223 6.38217L0.768024 6.38217C0.679632 6.38217 0.60731 6.45449 0.60731 6.54288L0.60731
+                <path d="M0.768024 7.90895L12.5223 7.90895L5.487 14.0161C5.3745 14.1145 5.4428 14.2973 5.59146 14.2973L7.36936
+                 14.2973C7.44771 14.2973 7.52204 14.2692 7.5803 14.219L15.172 7.63172C15.2416 7.57145 15.2973 7.49694 15.3356 7.41323C15.3738
+                 7.32953 15.3936 7.23858 15.3936 7.14657C15.3936 7.05455 15.3738
+                 6.96361 15.3356 6.8799C15.2973 6.79619 15.2416 6.72168 15.172 6.66141L7.53611 0.0339549C7.50597 0.00783851 7.46981 -0.00622437
+                 7.43164 -0.00622437L5.59347 -0.00622454C5.44481 -0.00622455 5.37651 0.178598 5.48901 0.275025L12.5223 6.38217L0.768024 6.38217C0.679632
+                 6.38217 0.60731 6.45449 0.60731 6.54288L0.60731
                  7.74824C0.60731 7.83663 0.679632 7.90895 0.768024 7.90895Z"
-                 fill="black"
-                 fillOpacity="0.85" />
+                  fill="black"
+                  fillOpacity="0.85" />
               </svg>
             </p>
           </Link>
