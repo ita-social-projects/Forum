5496208772dce33065f398c5c3f5beb31026c82b
superolegatron <olegtsirkun@gmail.com>
2024-11-03 21:47:46 +0200

    adjusted styles for responsive design

--- FrontEnd/src/components/Header/Navbar/Navbar.jsx
+++ FrontEnd/src/components/Header/Navbar/Navbar.jsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
-
 import { useAuth } from '../../../hooks';
-
 import Menu from './Menu';
 import SearchBox from './SearchBox';
 import Profile from './Profile';
 import Buttons from './Buttons';
-
+import BurgerMenu from './BurgerMenu';
 import css from './Navbar.module.css';
 
-function Navbar (props) {
-  const { isStaff} = useAuth();
+function Navbar(props) {
+  const { isStaff } = useAuth();
+
   return (
     <div className={css['navbar-wrapper']}>
       <div className={css['navbar-content']}>
@@ -18,19 +17,22 @@ function Navbar (props) {
           <Link to="/" target={isStaff ? '_blank' : null}>
             <div className={css['navbar-main-logo']}>
               <img
-                  src={`${process.env.REACT_APP_PUBLIC_URL}/craftMerge-logo.svg`}
-                  alt="CraftMerge logo"
-              ></img>
+                src={`${process.env.REACT_APP_PUBLIC_URL}/craftMerge-logo.svg`}
+                alt="CraftMerge logo"
+              />
               <span>CRAFTMERGE</span>
             </div>
           </Link>
         </div>
         <div className={css['navbar-utility-bar']}>
-          <>
-            <Menu/>
-            <SearchBox></SearchBox>
-          </>
-          {props.isAuthorized === true ? <Profile /> : <Buttons adminPage={props.page.includes('/customadmin')} />}
+          <div className={css['menu-search-wrapper']}>
+              <Menu />
+              <SearchBox />
+          </div>
+          {props.isAuthorized ? <Profile /> : <Buttons adminPage={props.page.includes('/customadmin')} />}
+          <div className={css['burger-menu-wrapper']}>
+            <BurgerMenu/>
+          </div>
         </div>
       </div>
     </div>
