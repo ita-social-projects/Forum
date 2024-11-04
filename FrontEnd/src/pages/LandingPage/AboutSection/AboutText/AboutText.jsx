fe413bef1b835c4c5801f2e412408534062b43a2
KyzukY <kyzuk.yr@gmail.com>
2024-11-04 14:35:07 +0200

    add new text

--- FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
@@ -2,33 +2,23 @@ import './AboutText.css';
 import AboutText from './text.js';
 
 const AboutSectionText = () => {
-  const renderParagraphs = (paragraphNumber) => {
-    return AboutText.content
-      .filter(item => item.paragraph === paragraphNumber)
-      .map((item, index) => (
-        <li key={index}>{item.text}</li>
-      ));
-  };
-
   return (
     <div className="about-us-text-section">
-      <div className="about-us-text-section__text-field">
-        <div className="about-us-text-section__header-text">{AboutText.title}</div>
-        <div className="about-us-text-section__paragraphs">
-          <ul className="about-us-text-section__paragraph-one">
-            {renderParagraphs(1)}
-          </ul>
-          <ul className="about-us-text-section__paragraph-two">
-            {renderParagraphs(2)}
-          </ul>
-        </div>
+      <h2 className="about-us-text-section__header-text">{AboutText.title}</h2>
+      <div className="about-us-text-section__grid">
+        {AboutText.content.map((item) => (
+          <div key={item.id} className="about-us-text-section__card">
+            <img
+              src={`${process.env.REACT_APP_PUBLIC_URL}/icons/${item.icon}`}
+              alt={item.text}
+              className="about-us-text-section__icon"
+            />
+            <p>{item.text}</p>
+          </div>
+        ))}
       </div>
-      <img className="about-us-text-section__dots"
-        src={`${process.env.REACT_APP_PUBLIC_URL}/img/dots_12x10.png`}
-        alt="dots_12x10.png" />
     </div>
   );
 };
 
 export default AboutSectionText;
-
