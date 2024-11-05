e0c8cba0dca3ab04b017984a0792d60b691f6f7e
superolegatron <olegtsirkun@gmail.com>
2024-11-05 14:09:30 +0200

    deleted redundant code

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,22 +1,16 @@
 import { useLocation } from 'react-router-dom';
 
-// import HeaderMenu from './Menu/HeaderMenu';
 import Navbar from './Navbar/Navbar';
 
 import css from './Header.module.css';
 
 function Header(props) {
   const { pathname } = useLocation();
-  // const hideMenu = pathname === '/login' || pathname === '/sign-up' || pathname.includes('/customadmin');
 
   return (
     <header>
       <div className={css['header-content']}>
         <Navbar isAuthorized={props.isAuthorized} page={pathname}></Navbar>
-        {/*<div className={css['header-divider']}></div>*/}
-        {/*  {hideMenu ? null : (*/}
-        {/*    <HeaderMenu />*/}
-        {/*  )}*/}
         <div className={css['header-divider']}></div>
       </div>
     </header>
