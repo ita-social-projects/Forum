f8f97bc23faad5634a3a602decdda28688604a2d
KyzukY <kyzuk.yr@gmail.com>
2024-11-07 13:42:11 +0200

    change css

--- FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Row, Col } from 'antd';
 import './AboutText.css';
 import AboutText from './text.js';
 
@@ -5,18 +7,25 @@ const AboutSectionText = () => {
   return (
     <div className="about-us-text-section">
       <h2 className="about-us-text-section__header-text">{AboutText.title}</h2>
-      <div className="about-us-text-section__grid">
+      <Row justify="start" gutter={[32, 24]}>
         {AboutText.content.map((item) => (
-          <div key={item.id} className="about-us-text-section__card">
-            <img
-              src={`${process.env.PUBLIC_URL}/svg/${item.icon}`}
-              alt={item.text}
-              className="about-us-text-section__icon"
-            />
-            <p>{item.text}</p>
-          </div>
+          <Col
+            key={item.id}
+            xs={12}   // Одна колонка на мобільних (<=576px)
+            md={6}   // Дві колонки на планшетах (>=768px)
+            xl={6}    // Чотири колонки на десктопах (>=1200px)
+          >
+            <div className="about-us-text-section__card">
+              <img
+                src={`${process.env.REACT_APP_PUBLIC_URL}/svg/${item.icon}`}
+                alt={item.text}
+                className="about-us-text-section__icon"
+              />
+              <p>{item.text}</p>
+            </div>
+          </Col>
         ))}
-      </div>
+      </Row>
     </div>
   );
 };
