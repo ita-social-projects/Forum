bc3963a4f86771560eaf35bbf6a5888740eca01a
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-11-07 22:21:48 +0200

    fix css styles display on large screens

--- FrontEnd/src/pages/LandingPage/Banner/Banner.jsx
+++ FrontEnd/src/pages/LandingPage/Banner/Banner.jsx
@@ -3,28 +3,29 @@ import { Link } from 'react-router-dom';
 
 const MainBanner = () => {
   return (
-    <div  className={styles['main-container']}>
-      <div className={styles['main-baner-text']}>
-        <h2 className={styles['main-baner-text__logo']}>CRAFTMERGE</h2>
-        <h3 className={styles['main-baner-text__explanation']}>Обʼєднуємо крафтових виробників та інноваторів</h3>
-        <div className={styles['main-banner-link__container']}>
-        <Link
-                className={styles['main-banner-link']}
-                to="/#"
-              >
-                Детальніше про нас
-        </Link>
+    <div className={styles['main-baner__container']}>
+      <div  className={styles['main-container']}>
+        <div className={styles['main-baner-text']}>
+          <h2 className={styles['main-baner-text__logo']}>CRAFTMERGE</h2>
+          <h3 className={styles['main-baner-text__explanation']}>Обʼєднуємо крафтових виробників та інноваторів</h3>
+          <div className={styles['main-banner-link__container']}>
+          <Link
+                  className={styles['main-banner-link']}
+                  to="/#"
+                >
+                  Детальніше про нас
+          </Link>
+          </div>
         </div>
-      </div>
-      <div>
-        <img className={styles['main-banner__img']}
-          src={`${process.env.REACT_APP_PUBLIC_URL}/img/main-baner.png`}
-          alt="Banner img"
-        />
+        <div>
+          <img className={styles['main-banner__img']}
+            src={`${process.env.REACT_APP_PUBLIC_URL}/img/main-baner.png`}
+            alt="Banner img"
+          />
+        </div>
       </div>
     </div>
   );
 };
 
 export default MainBanner;
-
