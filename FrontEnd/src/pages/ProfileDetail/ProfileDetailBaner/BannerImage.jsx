32cd9916f96ac7867c769d3576aa797eb35d7154
romanmyko <romanklapek@gmail.com>
2024-11-10 16:07:39 +0200

    default baner

--- FrontEnd/src/pages/ProfileDetail/ProfileDetailBaner/BannerImage.jsx
+++ FrontEnd/src/pages/ProfileDetail/ProfileDetailBaner/BannerImage.jsx
@@ -2,19 +2,17 @@ import { PropTypes } from 'prop-types';
 import classes from './BannerImage.module.css';
 
 function BannerImage({ data }) {
+    const backgroundImage = data?.banner?.path
+        ? `url(${data.banner.path}) lightgray center / cover no-repeat`
+        : `url(${process.env.REACT_APP_PUBLIC_URL}/svg/empty-baner-31x29.svg) #e0e0e0 left top / 100px 68px repeat `;
+
     return (
         <div
             className={classes['banner-image__block']}
-            style={{
-                background: data.banner?.path
-                    ? `url(${data.banner.path}) lightgray center / cover no-repeat`
-                    : `url(${process.env.REACT_APP_PUBLIC_URL}/svg/image-for-empty-banner.svg`,
-            }}
+            style={{ background: backgroundImage }}
         />
     );
 }
-
-export default BannerImage;
 
 BannerImage.propTypes = {
     data: PropTypes.shape({
@@ -23,3 +21,5 @@ BannerImage.propTypes = {
         }),
     }),
 };
+
+export default BannerImage;
