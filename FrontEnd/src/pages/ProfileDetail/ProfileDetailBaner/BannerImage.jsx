822af58c11f504b27485875f3e51c84e8e6dd5ee
romanmyko <romanklapek@gmail.com>
2024-11-12 18:03:50 +0200

    decore baner

--- FrontEnd/src/pages/ProfileDetail/ProfileDetailBaner/BannerImage.jsx
+++ FrontEnd/src/pages/ProfileDetail/ProfileDetailBaner/BannerImage.jsx
@@ -4,7 +4,7 @@ import classes from './BannerImage.module.css';
 function BannerImage({ data }) {
     const backgroundImage = data?.banner?.path
         ? `url(${data.banner.path}) lightgray center / cover no-repeat`
-        : `url(${process.env.REACT_APP_PUBLIC_URL}/svg/empty-baner-31x29.svg) #e0e0e0 left top / 100px 68px repeat `;
+        : `url(${process.env.REACT_APP_PUBLIC_URL}/svg/empty-decore-50x46.svg) #e0e0e0 left top / 50px 46px repeat `;
 
     return (
         <div
