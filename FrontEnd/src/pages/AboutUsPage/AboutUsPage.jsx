e03480671cc047a7d74c9a20622037a885a9b8a6
Parcival <ipavlenko239@gmail.com>
2024-11-13 11:58:57 +0200

    third commit

--- FrontEnd/src/pages/AboutUsPage/AboutUsPage.jsx
+++ FrontEnd/src/pages/AboutUsPage/AboutUsPage.jsx
@@ -11,30 +11,34 @@ const AboutUs = () => {
       </React.Fragment>
     ));
 
-
   return (
     <div className={css['about-us-section']}>
-      <div className={css['about-us-section-content__header']}>
-        <h2>{AboutTextContent.header}</h2>
+      <div className={css['about-us-section-main']}>
+        <div className={css['about-us-section-content__header']}>
+          <h2>{AboutTextContent.header}</h2>
         </div>
-      <div className={css['about-us-section-content']} >
-        <div id={'about-us'} className={css['about-us-rectangle']} >
-          <div className={css['about-us-section-content__text']} >
-            {AboutTextContent.contents.map(({ id, text }, i) => (
-              <p className={css['about-us-section-content__smart-text']} key={id}>
-                {i === 0 ?
-                TextStrong(text, 'CraftMerge')
-                : text}
-              </p>
-            ))}
+        <div className={css['about-us-section-content']}>
+          <div id={'about-us'} className={css['about-us-rectangle']}>
+            <div className={css['about-us-section-content__text']}>
+              {AboutTextContent.contents.map(({ id, text }, i) => (
+                <p
+                  className={css['about-us-section-content__smart-text']}
+                  key={id}
+                >
+                  {i === 0 ? TextStrong(text, 'CraftMerge') : text}
+                </p>
+              ))}
+            </div>
+            <div className={css['about-us-section-content__image-container']}>
+              <img
+                className={css['about-us-section-content__image']}
+                src={`${process.env.REACT_APP_PUBLIC_URL}/img/about.jpeg`}
+                alt="about img"
+                loading="lazy"
+                title="About CarftMerge"
+              />
+            </div>
           </div>
-          <img
-            className={css['about-us-section-content__image']}
-            src={`${process.env.REACT_APP_PUBLIC_URL}/img/about.jpeg`}
-            alt="about img"
-            loading="lazy"
-            title="About CarftMerge"
-          />
         </div>
       </div>
     </div>
