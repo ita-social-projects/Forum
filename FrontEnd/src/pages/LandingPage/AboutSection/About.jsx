779a4a8395eaa5f320629e5658cc17b1fc8ef5d1
Parcival <ipavlenko239@gmail.com>
2024-11-14 17:33:02 +0200

    eslint fix

--- FrontEnd/src/pages/LandingPage/AboutSection/About.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/About.jsx
@@ -1,19 +1,9 @@
-import './About.css';
-import React from 'react';
-import AboutSectionCards from './Cards/AboutCards';
-import AboutSectionText from './AboutText/AboutText.jsx';
-
+import "./About.css";
+import React from "react";
+import AboutSectionCards from "./Cards/AboutCards";
+import AboutSectionText from "./AboutText/AboutText.jsx";
 
 const MainAboutSection = () => {
-  const TextStrong = (text, keyword) =>
-    text.split(keyword).map((part, i, arr) => (
-      <React.Fragment key={i}>
-        {part}
-        {i < arr.length - 1 && <strong>{keyword}</strong>}
-      </React.Fragment>
-    ));
-
-
   return (
     <div className="about-us-section">
       <AboutSectionText />
--- FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
@@ -1,19 +1,19 @@
-import './AboutText.css';
-import AboutText from './text.js';
+import "./AboutText.css";
+import AboutText from "./text.js";
 
 const AboutSectionText = () => {
   const renderParagraphs = (paragraphNumber) => {
     return AboutText.content
-      .filter(item => item.paragraph === paragraphNumber)
-      .map((item, index) => (
-        <li key={index}>{item.text}</li>
-      ));
+      .filter((item) => item.paragraph === paragraphNumber)
+      .map((item, index) => <li key={index}>{item.text}</li>);
   };
 
   return (
     <div className="about-us-text-section">
       <div className="about-us-text-section__text-field">
-        <div className="about-us-text-section__header-text">{AboutText.title}</div>
+        <div className="about-us-text-section__header-text">
+          {AboutText.title}
+        </div>
         <div className="about-us-text-section__paragraphs">
           <ul className="about-us-text-section__paragraph-one">
             {renderParagraphs(1)}
@@ -23,9 +23,11 @@ const AboutSectionText = () => {
           </ul>
         </div>
       </div>
-      <img className="about-us-text-section__dots"
+      <img
+        className="about-us-text-section__dots"
         src={`${process.env.REACT_APP_PUBLIC_URL}/img/dots_12x10.png`}
-        alt="dots_12x10.png" />
+        alt="dots_12x10.png"
+      />
     </div>
   );
 };
