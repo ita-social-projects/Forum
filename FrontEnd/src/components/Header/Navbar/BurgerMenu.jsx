4129dca1111387868150d29e16a68d65229d1738
Leonid Chelakhov <leonidchelahov@gmail.com>
2024-11-17 15:02:04 +0300

    Improved burger menu behavior: smooth transition and no overlap with page content

--- FrontEnd/src/components/Header/Navbar/BurgerMenu.jsx
+++ FrontEnd/src/components/Header/Navbar/BurgerMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Hamburger from 'hamburger-react';
 import Menu from './Menu';
 import SearchBox from './SearchBox';
@@ -7,20 +7,29 @@ import css from './BurgerMenu.module.css';
 const BurgerMenu = () => {
   const [isOpen, setOpen] = useState(false);
 
+  useEffect(() => {
+    const pageContent = document.getElementById('page-content');
+    if (pageContent) {
+      pageContent.style.marginTop = isOpen ? '180px' : '0';
+    }
+  }, [isOpen]);
+
   return (
     <div className={css.burgerMenuContainer}>
       <Hamburger
-          toggled={isOpen}
-          toggle={setOpen}
-          size={30}
-          className={css.hamburgerIcon}
+        toggled={isOpen}
+        toggle={setOpen}
+        size={30}
+        className={css.hamburgerIcon}
       />
-      {isOpen && (
-        <div className={css.menu}>
-          <SearchBox className={css.searchBar} />
-          <Menu />
-        </div>
-      )}
+      <div className={`${css.menu} ${isOpen ? css.open : ''}`}>
+        {isOpen && (
+          <>
+            <SearchBox className={css.searchBar} />
+            <Menu />
+          </>
+        )}
+      </div>
     </div>
   );
 };
