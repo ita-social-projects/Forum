d17e00e902e410fcb81d42ff1dd8929247b59905
KyzukY <kyzuk.yr@gmail.com>
2024-11-18 21:12:27 +0200

    add about-us-section-background

--- FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/AboutText/AboutText.jsx
@@ -5,29 +5,32 @@ import AboutText from './text.js';
 
 const AboutSectionText = () => {
   return (
-    <div className="about-us-text-section">
-      <h2 className="about-us-text-section__header-text">{AboutText.title}</h2>
-      <Row justify="start" gutter={[32, 24]}>
-        {AboutText.content.map((item) => (
-          <Col
-            key={item.id}
-            xs={12}
-            md={6}
-            xl={6}
-          >
-            <div className="about-us-text-section__card">
-              <img
-                src={`${process.env.REACT_APP_PUBLIC_URL}/svg/${item.icon}`}
-                alt={item.text}
-                className="about-us-text-section__icon"
-              />
-              <p>{item.text}</p>
-            </div>
-          </Col>
-        ))}
-      </Row>
+    <div className="about-us-section-background">
+      <div className="about-us-text-section">
+        <h2 className="about-us-text-section__header-text">{AboutText.title}</h2>
+        <Row justify="start" gutter={[32, 24]}>
+          {AboutText.content.map((item) => (
+            <Col
+              key={item.id}
+              xs={12}
+              md={12}
+              xl={6}
+            >
+              <div className="about-us-text-section__card">
+                <img
+                  src={`${process.env.REACT_APP_PUBLIC_URL}/svg/${item.icon}`}
+                  alt={item.text}
+                  className="about-us-text-section__icon"
+                />
+                <p>{item.text}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 };
 
 export default AboutSectionText;
+
