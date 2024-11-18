56ca451cefc09c5300e980a22b008ad8c6e12d5f
Yan Zhylavy <yanzhylavy@gmail.com>
2024-11-18 09:36:03 +0200

    Submit button is disabled when the form is clean

--- FrontEnd/src/pages/ProfilePage/UI/ProfileFormButton/ProfileFormButton.jsx
+++ FrontEnd/src/pages/ProfilePage/UI/ProfileFormButton/ProfileFormButton.jsx
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
+import { DirtyFormContext } from '../../../../context/DirtyFormContext';
+
 import css from './ProfileFormButton.module.css';
 
 const ProfileFormButton = () => {
+  const { formIsDirty } = useContext(DirtyFormContext);
   return (
     <div className={css['submit-button__conteiner']}>
       <button
         className={css['submit-button']}
         type="submit"
+        disabled={!formIsDirty}
       >
         Зберегти
       </button>
