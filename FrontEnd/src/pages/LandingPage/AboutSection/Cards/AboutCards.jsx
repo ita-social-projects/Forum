aaf5b27abbee0e4114cb7d87cb7e6c2124fe693a
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-11-24 18:52:39 +0200

    changed the logic of unpacking content into a card

--- FrontEnd/src/pages/LandingPage/AboutSection/Cards/AboutCards.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/Cards/AboutCards.jsx
@@ -3,19 +3,19 @@ import AboutUsText from './text.js';
 
 const AboutSectionCards = () => {
   return (
-    <div className="about-us-cards-section">
-      <h2 className="about-us-cards-section__header-text">{AboutUsText.title}</h2>
-      <div className="about-us-cards-section__cards">
-        {AboutUsText.content.map((card, index) => (
-          <div key={index} className={`about-us-cards-section__card 
-          ${index === 0 ? 'about-us-cards-section__first-card' : 'about-us-cards-section__next-card'}`}
-          >
-            <div className="about-us-cards-section__cards-content">
-              <h3 className="about-us-cards-section__cards-content__text-header">{card.title}</h3>
-              <p className="about-us-cards-section__cards-content__text">{card.text}</p>
+    <div className="about-us-cards-main-container">
+      <div className="about-us-cards-section">
+        <h2 className="about-us-cards-section__header-text">{AboutUsText.title}</h2>
+        <div className="about-us-cards-section__cards">
+          {AboutUsText.content.map((card, index) => (
+            <div key={index} className="about-us-cards-section__card">
+              <div className="about-us-cards-section__cards-content">
+                <h3 className="about-us-cards-section__cards-content__text-header">{card.title}</h3>
+                <p className="about-us-cards-section__cards-content__text">{card.text}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
