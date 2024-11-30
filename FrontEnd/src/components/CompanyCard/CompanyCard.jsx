88b11209afb0da6783553658999bf89ea7e932fb
Lvyshnevska <lilia.vyshnevska@gmail.com>
2024-11-30 13:30:44 +0100

    fixed styles in company card

--- FrontEnd/src/components/CompanyCard/CompanyCard.jsx
+++ FrontEnd/src/components/CompanyCard/CompanyCard.jsx
@@ -98,33 +98,34 @@ export default function CompanyCard({
             )}
           </div>
           <div className={styles['company-card__text-block']}>
-            <Tooltip
-              title={activitiesString}
-              placement="bottom"
-              pointAtCenter={true}
-            >
-              <p className={styles['company-card__category-text']}>
-                {activitiesSliceString}
-              </p>
-            </Tooltip>
             <div className={styles['company-card__text-block__header']}>
+              <Tooltip
+                title={activitiesString}
+                placement="bottom"
+                pointAtCenter={true}
+              >
+                <p className={styles['company-card__category-text']}>
+                  {activitiesSliceString}
+                </p>
+              </Tooltip>
+
               <h3 className={styles['company-card__name-text']}>
                 {profile.name}
               </h3>
+              <Tooltip
+                title={profile.regions_ukr_display}
+                placement="bottom"
+                pointAtCenter={true}
+              >
+                <p className={styles['company-card__region-text']}>
+                  {regions
+                    ? regions.length < lengthOfRegion
+                      ? regions
+                      : `${regions.substring(0, 35)}...`
+                    : ''}
+                </p>
+              </Tooltip>
             </div>
-            <Tooltip
-              title={profile.regions_ukr_display}
-              placement="bottom"
-              pointAtCenter={true}
-            >
-              <p className={styles['company-card__region-text']}>
-                {regions
-                  ? regions.length < lengthOfRegion
-                    ? regions
-                    : `${regions.substring(0, 35)}...`
-                  : ''}
-              </p>
-            </Tooltip>
             <div className={styles['company-card__footer_content']}>
               <Tooltip
                 title={
@@ -138,6 +139,12 @@ export default function CompanyCard({
                   <CategoryBadges categories={profile.categories.slice(0, 3)} />
                 </div>
               </Tooltip>
+                <StarForLike
+                  isSaved={profile.is_saved}
+                  isAuthorized={isAuthorized}
+                  ownProfile={ownProfile}
+                  handleClick={profile.is_saved ? handleDeleteSaved : handleSave}
+                />
             </div>
           </div>
         </div>
@@ -157,14 +164,6 @@ export default function CompanyCard({
           )}
         </div>
       </Link>
-      <div className={styles['company-card__star']}>
-        <StarForLike
-          isSaved={profile.is_saved}
-          isAuthorized={isAuthorized}
-          ownProfile={ownProfile}
-          handleClick={profile.is_saved ? handleDeleteSaved : handleSave}
-        />
-      </div>
     </div>
   );
 }
