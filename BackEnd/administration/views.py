436574671c44bfdc42ef8ed743a00ef86b6487db
Andrewakiv <danchyshy.andriy@gmail.com>
2024-12-08 13:10:56 +0200

    changed permission class

--- BackEnd/administration/views.py
+++ BackEnd/administration/views.py
@@ -35,7 +35,7 @@ from utils.administration.send_email_feedback import send_email_feedback
 
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import UsersFilter
-from rest_framework.permissions import AllowAny
+
 
 class UsersListView(ListAPIView):
     """
@@ -111,7 +111,7 @@ class ProfileStatisticsView(RetrieveAPIView):
     Count of companies
     """
 
-    permission_classes = [AllowAny]
+    permission_classes = [IsStaffUser]
     serializer_class = StatisticsSerializer
 
     def get_object(self):
