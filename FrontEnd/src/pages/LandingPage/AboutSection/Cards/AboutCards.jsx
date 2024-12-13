9a511cdf1980872e652678a510843b4fc6e8eb8f
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-12-13 13:08:25 +0200

    Used ant design elements to display cards

--- FrontEnd/src/pages/LandingPage/AboutSection/Cards/AboutCards.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/Cards/AboutCards.jsx
@@ -1,21 +1,29 @@
 import './AboutCards.css';
 import AboutUsText from './text.js';
+import { Row, Col } from 'antd';
 
 const AboutSectionCards = () => {
   return (
     <div className="about-us-cards-main-container">
       <div className="about-us-cards-section">
         <h2 className="about-us-cards-section__header-text">{AboutUsText.title}</h2>
-        <div className="about-us-cards-section__cards">
+        <Row gutter={[32, 32]} className="about-us-cards-section__cards">
           {AboutUsText.content.map((card, index) => (
-            <div key={index} className="about-us-cards-section__card">
+            <Col
+              key={index}
+              xs={24}  // 1 колонки на мобільних пристроях
+              md={8}  // 2 колонки на планшетах
+              xl={4}   // 3 колонки на ноутбуках
+              xxl={4}  // 4 колонки на великих екранах
+              className="about-us-cards-section__card"
+            >
               <div className="about-us-cards-section__cards-content">
                 <h3 className="about-us-cards-section__cards-content__text-header">{card.title}</h3>
                 <p className="about-us-cards-section__cards-content__text">{card.text}</p>
               </div>
-            </div>
+            </Col>
           ))}
-        </div>
+        </Row>
       </div>
     </div>
   );
