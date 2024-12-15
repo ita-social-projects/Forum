791dcfabd59383d89141adb7a2f63ed43f6a3045
romanmyko <romanklapek@gmail.com>
2024-12-15 20:10:14 +0200

    correct test

--- BackEnd/administration/tests/test_admin_category.py
+++ BackEnd/administration/tests/test_admin_category.py
@@ -4,27 +4,21 @@ from rest_framework.test import APITestCase
 from administration.factories import (
     AdminCategoryFactory,
     AdminUserFactory,
-    AdminProfileFactory,
 )
-from profiles.models import Profile, Category, CustomUser
+
+from utils.dump_response import dump  # noqa
+from utils.unittest_helper import AnyInt, AnyStr
 
 
 class TestAdminCategoryAPIUserNotStaff(APITestCase):
+    maxDiff = None
+
     def setUp(self):
         self.user = AdminUserFactory(
             is_staff=False,
             is_active=True,
         )
-        self.name = AdminCategoryFactory()
-
-    def tearDown(self):
-        super().tearDown()
-        Category.objects.all().delete()
-        Profile.objects.all().delete()
-        CustomUser.objects.all().delete()
-        AdminUserFactory.reset_sequence(0)
-        AdminCategoryFactory.reset_sequence(0)
-        AdminProfileFactory.reset_sequence(0)
+        self.category = AdminCategoryFactory()
 
     def test_get_category_users_not_staff(self):
         self.client.force_authenticate(self.user)
@@ -38,74 +32,73 @@ class TestAdminCategoryAPIUserNotStaff(APITestCase):
 
 
 class TestAdminCategoryAPIUserStaff(APITestCase):
+    maxDiff = None
+
     def setUp(self):
         self.user = AdminUserFactory(
             is_staff=True,
             is_active=True,
         )
-        self.name = AdminCategoryFactory.create_batch(2)
-
-    def tearDown(self):
-        super().tearDown()
-        Category.objects.all().delete()
-        Profile.objects.all().delete()
-        CustomUser.objects.all().delete()
-        AdminUserFactory.reset_sequence(0)
-        AdminCategoryFactory.reset_sequence(0)
-        AdminProfileFactory.reset_sequence(0)
+        self.categories = AdminCategoryFactory.create_batch(2)
 
     def test_get_categories_users_staff(self):
         self.client.force_authenticate(self.user)
         response = self.client.get(path="/api/admin/categories/")
         data = [
             {
-                "id": 5,
-                "name": "category 1",
+                "id": AnyInt(),
+                "name": AnyStr(),
             },
             {
-                "id": 6,
-                "name": "category 2",
+                "id": AnyInt(),
+                "name": AnyStr(),
             },
         ]
-
         self.assertEqual(data, response.json()["results"])
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_get_categories_id_users_staff(self):
         self.client.force_authenticate(self.user)
         response = self.client.get(path="/api/admin/categories/3/")
-        data = {"name": "category 1"}
+        data = {"name": AnyStr()}
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(data, response.json())
 
     def test_post_new_categories_users_staff(self):
         self.client.force_authenticate(self.user)
-        data = {"name": "category 11"}
+        data = {"name": "some category"}
         response = self.client.post(path="/api/admin/categories/", data=data)
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(response.json()["name"], data["name"])
         self.assertIn("id", response.json())
 
     def test_post_unique_categories_users_staff(self):
+        self.existing_name = AdminCategoryFactory.create(
+            name="existing category"
+        )
         self.client.force_authenticate(self.user)
-        data = {"name": "category 1"}
+        data = {"name": "existing category"}
         message = "Category with this name already exists."
         response = self.client.post(path="/api/admin/categories/", data=data)
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
         self.assertEqual(response.json()["name"][0], message)
 
     def test_put_categories_id_users_staff(self):
+        self.category = AdminCategoryFactory.create()
         self.client.force_authenticate(self.user)
         data = {"name": "category 1212"}
-        response = self.client.put(path="/api/admin/categories/14/", data=data)
+        response = self.client.put(
+            path=f"/api/admin/categories/{self.category.id}/", data=data
+        )
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(response.json()["name"], data["name"])
 
     def test_patch_categories_id_users_staff(self):
+        self.category = AdminCategoryFactory.create()
         self.client.force_authenticate(self.user)
         data = {"name": "category 77"}
         response = self.client.patch(
-            path="/api/admin/categories/7/", data=data
+            path=f"/api/admin/categories/{self.category.id}/", data=data
         )
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(response.json()["name"], data["name"])
