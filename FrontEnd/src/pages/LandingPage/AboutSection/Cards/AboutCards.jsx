10ef42698dba12f12041cea80bad7daf3d172b0d
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2024-12-15 14:12:59 +0200

    fixed import formatting and optimized code

--- FrontEnd/src/pages/LandingPage/AboutSection/Cards/AboutCards.jsx
+++ FrontEnd/src/pages/LandingPage/AboutSection/Cards/AboutCards.jsx
@@ -1,23 +1,22 @@
-import './AboutCards.css';
-import AboutUsText from './text.js';
 import { Row, Col } from 'antd';
+import AboutUsText from './text.js';
+import './AboutCards.css';
 
 const AboutSectionCards = () => {
   return (
     <div className="about-us-cards-main-container">
       <div className="about-us-cards-section">
         <h2 className="about-us-cards-section__header-text">{AboutUsText.title}</h2>
-        <Row gutter={[32, 32]} className="about-us-cards-section__cards">
+        <Row gutter={[32, 32]} >
           {AboutUsText.content.map((card, index) => (
             <Col
               key={index}
               xs={24}
-              md={8}
-              xl={4}
-              xxl={4}
-              className="about-us-cards-section__card"
+              md={12}
+              xl={8}
+              xxl={8}
             >
-              <div className="about-us-cards-section__cards-content">
+              <div className="about-us-cards-section__card">
                 <h3 className="about-us-cards-section__cards-content__text-header">{card.title}</h3>
                 <p className="about-us-cards-section__cards-content__text">{card.text}</p>
               </div>
